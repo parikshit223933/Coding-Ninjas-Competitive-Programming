8f13cc4440e6f7c323c8f7b0ebfc18cafdc67371
parikshit singh <pk223933@gmail.com>
2020-03-15 08:37:19 +0530

    implemented myself

--- Advanced Graphs/connected_horses_2.cpp
+++ Advanced Graphs/connected_horses_2.cpp
@@ -1,81 +1,99 @@
 #include <iostream>
 using namespace std;
-#define p int(1e9 + 7)
 #define ll long long int
+#define mod int(1e9 + 7)
 int mat[1001][1001], n, m;
-ll fac(ll t)
+ll fact(ll t)
 {
-    ll ans = 1;
-    for (ll i = 1; i <= t; i++)
-    {
-        ans=(ans*i)%p;
-    }
-    return ans;
+	int ans = 1;
+	for (ll i = 1; i <= t; i++)
+	{
+		ans = (ans * i) % mod;
+	}
+	return ans;
 }
-ll bfs(ll i, ll j)
+int bfs(ll i, ll j)
 {
-    ll t=1;
-    mat[i][j]=2;
-    // the eight conditions
-    if(i-2>=1 && j+1<=m && mat[i-2][j+1]==1)
-        t+=bfs(i-2,j+1);
-    if(i-1>=1 && j+2<=m && mat[i-1][j+2]==1)
-        t+=bfs(i-1,j+2);
-    if(i+1<=n && j+2<=m && mat[i+1][j+2]==1)
-        t+=bfs(i+1,j+2);
-    if(i+2<=n && j+1<=m && mat[i+2][j+1]==1)
-        t+=bfs(i+2,j+1);
-    if(i-2>=1 && j-1>=1 && mat[i-2][j-1]==1)
-        t+=bfs(i-2,j-1);
-    if(i-1>=1 && j-2>=1 && mat[i-1][j-2]==1)
-        t+=bfs(i-1,j-2);
-    if(i+1<=n && j-2>=1 && mat[i+1][j-2]==1)
-        t+=bfs(i+1,j-2);
-    if(i+2<=n && j-1>=1 && mat[i+2][j-1]==1)
-        t+=bfs(i+2,j-1);
-    return t;
+	ll total = 1;
+	mat[i][j] = 2;
+	// now we have to write the conditions where the horse moves
+	if (i - 2 >= 1 && j + 1 <= m && mat[i - 2][j + 1] == 1)
+	{
+		total += bfs(i - 2, j + 1);
+	}
+	if (i - 2 >= 1 && j - 1 >= 1 && mat[i - 2][j - 1] == 1)
+	{
+		total += bfs(i - 2, j - 1);
+	}
+	if (i + 2 <= n && j + 1 <= m && mat[i + 2][j + 1] == 1)
+	{
+		total += bfs(i + 2, j + 1);
+	}
+	if (i + 2 <= n && j - 1 >= 1 && mat[i + 2][j - 1] == 1)
+	{
+		total += bfs(i + 2, j - 1);
+	}
+	////////////////////////////////////////////////////////
+	if (i - 1 >= 1 && j + 2 <= m && mat[i - 1][j + 2] == 1)
+	{
+		total += bfs(i - 1, j + 2);
+	}
+	if (i - 1 >= 1 && j - 2 >= 1 && mat[i - 1][j - 2] == 1)
+	{
+		total += bfs(i - 1, j - 2);
+	}
+	if (i + 1 <= n && j + 2 <= m && mat[i + 1][j + 2] == 1)
+	{
+		total += bfs(i + 1, j + 2);
+	}
+	if (i + 1 <= n && j - 2 >= 1 && mat[i + 1][j - 2] == 1)
+	{
+		total += bfs(i + 1, j - 2);
+	}
+	return total;
 }
 void func()
 {
-    long long ans = 1, t;
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j <= m; j++)
-        {
-            if (mat[i][j] == 1)
-            {
-                t = bfs(i, j);
-                ans = (ans * fac(t)) % p;
-            }
-        }
-    }
-    cout << ans << endl;
+	ll ans = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			if (mat[i][j] == 1)
+			{
+				ll t = bfs(i, j);
+				ans = (ans * fact(t)) % mod;
+			}
+		}
+	}
+	cout << ans << endl;
 }
 void input()
 {
-    int q;
-    cin >> n >> m >> q;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            mat[i][j] = 0;
-        }
-    }
-    int x, y;
-    while (q--)
-    {
-        cin >> x >> y;
-        mat[x][y] = 1;
-    }
-    func();
+	int q;
+	cin >> n >> m >> q;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= m; j++)
+		{
+			mat[i][j] = 0;
+		}
+	}
+	while (q--)
+	{
+		int x, y;
+		cin >> x >> y;
+		mat[x][y] = 1;
+	}
+	func();
 }
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        input();
-    }
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		input();
+	}
+	return 0;
 }
