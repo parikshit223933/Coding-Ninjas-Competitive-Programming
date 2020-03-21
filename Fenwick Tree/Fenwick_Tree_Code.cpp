15a3834d4fb69780f41e0ef40fba10065b926990
parikshit singh <pk223933@gmail.com>
2020-03-21 16:25:22 +0530

    Implemented

---
+++ Fenwick Tree/Fenwick_Tree_Code.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+void update(int index, int value, int *bit, int n)
+{
+    for(; index<=n; index+=index&(-index))
+    {
+        bit[index]+=value;
+    }
+}
+int query(int index, int *bit)
+{
+    int ans=0;
+    for(; index>0; index-=index&(-index))
+    {
+        ans+=bit[index];
+    }
+    return ans;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr=new int [n+1];
+    int *bit/*binary index tree*/=new int [n+1];
+    for(int i=0; i<=n; i++)
+    {
+        bit[i]=0;
+        /*all the elements of binary index tree should be zero initially otherwise
+        it will cause problems in update function while incrementing a value at an index.*/
+    }
+    //we dont care about arr[0].
+    for(int i=1; i<=n; i++)
+    {
+        cin>>arr[i];
+        update(i, arr[i], bit, n);
+    }
+    cout<<query(5, bit)<<endl;
+    cout<<query(6, bit)-query(1, bit)<<endl;
+    
+}
