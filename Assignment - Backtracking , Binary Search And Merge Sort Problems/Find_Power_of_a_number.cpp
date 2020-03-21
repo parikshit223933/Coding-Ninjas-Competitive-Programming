983c6b8368e1aae866b794074742d56e9baa114b
parikshit singh <pk223933@gmail.com>
2020-03-21 12:36:33 +0530

    Implemented

---
+++ Assignment - Backtracking , Binary Search And Merge Sort Problems/Find_Power_of_a_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int power(int x, int n)
+{
+	if(n==0)
+	{
+		return 1;
+	}
+	return x * power(x, n - 1);
+}
+int main()
+{
+	int x;
+	int n;
+	cin >> x >> n;
+	cout<<power(x, n);
+}
