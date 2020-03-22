4f6609a063a8fa7bc158c51226a5e902eb9f8e91
parikshit singh <pk223933@gmail.com>
2020-03-22 12:59:15 +0530

    Implemented

---
+++ Bit Manipulation/Set_ith_bit.cpp
@@ -0,0 +1,14 @@
+int turnOnIthBit(int n, int i){
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+    if(n&(1<<i))
+	{
+		return n;
+	}
+	else
+	return n + (1 << i);
+}
+
