9836873666032f564e9de6c5632f01d449b0c649
parikshit singh <pk223933@gmail.com>
2020-03-23 17:36:11 +0530

    Need to revise it

--- Fenwick Tree/Distinct_Query_Problem.cpp
+++ Fenwick Tree/Distinct_Query_Problem.cpp
@@ -1,82 +1,94 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-struct event
+/////////////////////////////////////////////////////////////////////////////////////////
+struct coder
 {
     int first, second, index;
-};
-bool sorter(event e1, event e2)
+} query[200000];
+/////////////////////////////////////////////////////////////////////////////////////////
+bool sorter(coder A, coder B)
 {
-    return e1.second < e2.second;
+
+    return A.second < B.second;
 }
-void update(int index, int value, int n, int *bit)
+
+int bit[30001];
+////////////////////////////////////////////////////////////////////////////////////////
+void update(int index, int value, int n)
 {
-    for(; index<=n; index+=index&(-index))
+
+    for (; index <= n; index += index & (-index))
     {
-        bit[index]+=value;
+        bit[index] += value;
     }
 }
-int value(int index, int *bit)
+int last[1000001];
+int arr[30001];
+///////////////////////////////////////////////////////////////////////////////////////////
+int value(int index)
 {
-    int count=0;
-    for(; index>0; index-=index&(-index))
+
+    int res = 0;
+    for (; index > 0; index -= index & (-index))
     {
-        count+=bit[index];
+        res += bit[index];
     }
-    return count;
+    return res;
 }
+////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
     int n;
     cin >> n;
-    int *arr = new int[n + 1];
-    for (int i = 1; i < n + 1; i++)
+
+    for (int i = 1; i <= n; i++)
     {
         cin >> arr[i];
     }
+
     int q;
     cin >> q;
-    event *query = new event[q];
-    for (int i = 0; i < n; i++)
+
+    for (int i = 0; i < q; i++)
     {
         cin >> query[i].first >> query[i].second;
         query[i].index = i;
     }
+
     sort(query, query + q, sorter);
-    int *bit = new int[n + 1];
+
+    memset(last, -1, sizeof(last));
+
+    int k = 0;
     int *ans = new int[q];
 
-    int total=0;
-    int k=0;
-    int *last=new int [1000001];
-    for(int i=0; i<1000001; i++)
+    int total = 0;
+    for (int i = 1; i <= n; i++)
     {
-        last[i]=-1;
-    }
-    //this is working as a map for us, it determines that, do we have encountered the ith element already or not.
-    for(int i=1; i<=n; i++)
-    {
-        if(last[arr[i]]!=-1)
+
+        if (last[arr[i]] != -1)
         {
-            update(last[arr[i]], -1, n, bit);
+            update(last[arr[i]], -1, n);
         }
         else
         {
             total++;
         }
-        update(i/*for ith index*/, 1/*number of contributions corresponding to ith element*/, n/*how long we can go in BIT*/, bit/*our binary index tree*/);
-        last[arr[i]]=i;
-        while(k<q && query[k].second==i)
+
+        update(i, 1, n);
+        last[arr[i]] = i;
+
+        while (k < q && query[k].second == i)
         {
-            ans[query[k].index]=total-value(query[k].first-1, bit);
+
+            ans[query[k].index] = total - value(query[k].first - 1);
+            k++;
         }
     }
-    for(int i=0; i<0; i++)
+
+    for (int i = 0; i < q; i++)
     {
-        cout<<ans[i]<<endl;
+        cout << ans[i] << endl;
     }
-    
-    
-    
     return 0;
 }
