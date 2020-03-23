f226e81476b9d9eae445cbcf27cf28b41e09bacd
parikshit singh <pk223933@gmail.com>
2020-03-24 00:07:08 +0530

    staged

---
+++ Fenwick Tree/KQUERY.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr=new int [n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int q;
+    cin>>q;
+    
+}
