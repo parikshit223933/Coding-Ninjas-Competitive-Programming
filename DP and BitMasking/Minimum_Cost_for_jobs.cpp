82208bf6ce3ece4936067094b1dc8c6893210c40
parikshit singh <pk223933@gmail.com>
2020-03-24 22:23:26 +0530

    DP Solution implemented

--- DP and BitMasking/Minimum_Cost_for_jobs.cpp
+++ DP and BitMasking/Minimum_Cost_for_jobs.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 #include<climits>
 using namespace std;
-int min_cost(int cost[4][4], int n, int p, int mask)
+int min_cost(int cost[4][4], int n, int p, int mask, int *dp)
 {
     if(p>=n)
     {
         return 0;
+    }
+    if(dp[mask]!=INT_MAX)
+    {
+        return dp[mask];
     }
     int minimum=INT_MAX;
     // what all jobs can i assign to pth person
@@ -13,17 +17,23 @@ int min_cost(int cost[4][4], int n, int p, int mask)
     {
         if(!(mask&(1<<i)))
         {
-            int ans=min_cost(cost, n, p+1, mask|(1<<i))+cost[p][i];
+            int ans=min_cost(cost, n, p+1, mask|(1<<i), dp)+cost[p][i];
             if(ans<minimum)
             {
                 minimum=ans;
             }
         }
     }
+    dp[mask]=minimum;
     return minimum;
 }
 int main()
 {
     int cost[][4]={{10, 2, 6, 5}, {1, 15, 12, 8}, {7, 8, 9, 3}, {15, 13, 4, 10}};
-    cout<<min_cost(cost, 4/*number of persons*/, 0/*0th person*/, 0/*mask*/)<<endl;
+    int*dp=new int [1<<4];
+    for(int i=0; i<(1<<4); i++)
+    {
+        dp[i]=INT_MAX;
+    }
+    cout<<min_cost(cost, 4/*number of persons*/, 0/*0th person*/, 0/*mask*/, dp)<<endl;
 }
