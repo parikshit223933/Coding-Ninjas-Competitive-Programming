8c1ea287879aa4af096a7112df938152b62e8b32
parikshit singh <pk223933@gmail.com>
2020-03-28 02:01:35 +0530

    staged

---
+++ DP and BitMasking/Dilemma_Code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int find_touches(int pos, int mask, vector<string>&v, int **dp)
+{
+
+}
+int minimumTouchesRequired(int n, vector<string>&v)
+{
+    return find_touches()
+}
