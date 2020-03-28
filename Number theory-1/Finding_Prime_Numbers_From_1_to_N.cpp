624148cde618159e529a4b14aeadf2592f2ecc04
parikshit singh <pk223933@gmail.com>
2020-03-29 04:46:13 +0530

    By parikh sir

--- Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
+++ Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
@@ -3,21 +3,28 @@
 using namespace std;
 bool check_prime(int n)
 {
-    for(int i=2; i<sqrt(n); i++)
+    int count_factors=0;
+    for(int i=1; i<=sqrt(n); i++)
     {
         if(n%i==0)
         {
-            return false;
+            if(i*i==n)
+            {
+                count_factors+=1;
+            }
+            else
+            count_factors+=2;
         }
     }
-    return true;
+    return count_factors==2;
+    
 }
 int main()
 {
     int n;
     cin>>n;
     int count=0;
-    for(int i=2; i<=n; i++)
+    for(int i=1; i<=n; i++)
     {
         if(check_prime(i))
         {
