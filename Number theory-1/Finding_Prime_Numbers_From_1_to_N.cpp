0fa27ea2c022ac78e64d31d70f792a4a60c60121
parikshit singh <pk223933@gmail.com>
2020-03-29 06:13:28 +0530

    Staged(tried my myself)

--- Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
+++ Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
@@ -3,33 +3,50 @@
 using namespace std;
 bool check_prime(int n)
 {
-    int count_factors=0;
-    for(int i=1; i<=sqrt(n); i++)
+    int count_factors = 0;
+    for (int i = 1; i <= sqrt(n); i++)
     {
-        if(n%i==0)
+        if (n % i == 0)
         {
-            if(i*i==n)
+            if (i * i == n)
             {
-                count_factors+=1;
+                count_factors += 1;
             }
             else
-            count_factors+=2;
+                count_factors += 2;
         }
     }
-    return count_factors==2;
-    
+    return count_factors == 2;
+
 }
 int main()
 {
     int n;
-    cin>>n;
-    int count=0;
-    for(int i=1; i<=n; i++)
+    cin >> n;
+    int count = 0;
+    bool* isprime = new bool[n + 1];
+    for (int i = 0; i < n + 1; i++)
     {
-        if(check_prime(i))
-        {
-            count+=1;
-        }
+        isprime[i] = true;
     }
-    cout<<count<<endl;
+    for (int i = 1; i <= n; i++)
+    {
+        if (isprime[i])
+        {
+            if (check_prime(i))
+            {
+                count += 1;
+                int temp = i;
+                while (temp * i <= n)
+                {
+                    isprime[temp * i] = false;
+                    temp++;
+                }
+            }
+        }
+
+    }
+    delete[]isprime;
+    cout << count << endl;
+    return 0;
 }
