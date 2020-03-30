cd8e815861a9f2d5a418308dd22d606b9e21caca
parikshit singh <pk223933@gmail.com>
2020-03-31 02:52:06 +0530

    Implemented with modulo

--- Applications of Number Theory-1/Divisors_of_a_factorial.cpp
+++ Applications of Number Theory-1/Divisors_of_a_factorial.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
-#include<math.h>
+#include <math.h>
+#define m 1000000007
+#define ll long long int
 using namespace std;
-void makeSieve(int n)
+void makeSieve(ll n)
 {
     bool *isprime = new bool[n + 1];
-    for (int i = 0; i < n + 1; i++)
+    for (ll i = 0; i < n + 1; i++)
     {
         isprime[i] = true;
     }
     isprime[0] = false;
     isprime[1] = false;
-    for (int i = 2; i <= sqrt(n); i++)
+    for (ll i = 2; i <= sqrt(n); i++)
     {
         if (isprime[i])
-            for (int j = i; j * i <= n; j++)
+            for (ll j = i; j * i <= n; j++)
             {
                 isprime[j * i] = false;
             }
     }
-    int total_divisors = 1;
-    for (int i = 0; i < n + 1; i++)
+    ll total_divisors = 1;
+    for (ll i = 0; i < n + 1; i++)
     {
-        if(isprime[i])
+        if (isprime[i])
         {
-            int current_sum=0;
-            for(int j=1; pow(i, j)<=n; j++)
+            ll current_sum = 0;
+            for (ll j = 1; pow(i, j) <= n; j++)
             {
-                current_sum+=n/pow(i, j);
+                current_sum += n / pow(i, j);
             }
-            total_divisors*=(current_sum+1);
+            total_divisors =(total_divisors%m * (current_sum + 1)%m)%m;
         }
     }
     cout << total_divisors << endl;
@@ -36,11 +38,11 @@ void makeSieve(int n)
 }
 int main()
 {
-    int t;
+    ll t;
     cin >> t;
     while (t--)
     {
-        int n;
+        ll n;
         cin >> n;
         makeSieve(n);
     }
