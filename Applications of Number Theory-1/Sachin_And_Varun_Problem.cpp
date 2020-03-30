3b539dce0e7d899d156976734893c85228867806
parikshit singh <pk223933@gmail.com>
2020-03-30 20:50:43 +0530

    Revise it

--- Applications of Number Theory-1/Sachin_And_Varun_Problem.cpp
+++ Applications of Number Theory-1/Sachin_And_Varun_Problem.cpp
@@ -1,17 +1,62 @@
 #include<iostream>
+#include<algorithm>
 using namespace std;
-int count_ways(int a, int b, int d)
+typedef long long int ll;
+class triplet
 {
-    
+public:
+    ll x, y, gcd;
+};
+triplet extended_euclid(ll a, ll b)
+{
+    if (b == 0)
+    {
+        triplet ans;
+        ans.x = 1;
+        ans.y = b;
+        ans.gcd = a;
+        return ans;
+    }
+    triplet small_ans = extended_euclid(b, a % b);
+    triplet ans;
+    ans.gcd = small_ans.gcd;
+    ans.x = small_ans.y;
+    ans.y = small_ans.x - small_ans.y * (a / b);
+    return ans;
+}
+ll Multiplicative_Modulo_Inverse(ll a, ll m)
+{
+    ll ans=extended_euclid(a, m).x;
+    return (ans%m+m)%m;
+}
+ll count_ways(ll a, int b, ll d)
+{
+    ll y1=((d%a)*Multiplicative_Modulo_Inverse(b, a))%a;
+    ll first_value=d/b;
+    if((d/b)<y1)
+    {
+        return 0;
+    }
+    ll n=(first_value-y1)/a;
+    return n+1;
 }
 int main()
 {
-    int t;
+    ll t;
     cin>>t;
     while(t--)
     {
-        int a, b, d;
+        ll a, b, d;
         cin>>a>>b>>d;
+        ll g=__gcd(a, b);
+        if(d%g!=0)
+        {
+            cout<<0<<endl;
+            continue;
+        }
+        a/=g;
+        b/=g;
+        d/=g;
         cout<<count_ways(a, b, d)<<endl;
     }
 }
