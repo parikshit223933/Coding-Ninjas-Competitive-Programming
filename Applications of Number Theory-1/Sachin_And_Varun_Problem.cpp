dbdc95ca62642bc95479fdf2c24cd75e415d66c0
parikshit singh <pk223933@gmail.com>
2020-03-30 12:49:40 +0530

    staged

---
+++ Applications of Number Theory-1/Sachin_And_Varun_Problem.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int count_ways(int a, int b, int d)
+{
+    
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a, b, d;
+        cin>>a>>b>>d;
+        cout<<count_ways(a, b, d)<<endl;
+    }
+}
