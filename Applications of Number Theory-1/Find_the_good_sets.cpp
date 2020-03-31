b28cd774129ba94bcc135a76f82782c5fc87a5ac
parikshit singh <pk223933@gmail.com>
2020-04-01 03:39:53 +0530

    basic slution without mod

--- Applications of Number Theory-1/Find_the_good_sets.cpp
+++ Applications of Number Theory-1/Find_the_good_sets.cpp
@@ -1,19 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+int size_of_sieve = 750001;
+void Seive(int *arr, int n)
+{
+    int *sieve = new int[size_of_sieve];
+    for (int i = 0; i < size_of_sieve; i++)
+    {
+        sieve[i] = 0;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        sieve[arr[i]] = 1;
+    }
+    for (int i = 2; i < size_of_sieve; i++)
+    {
+        int current_element = i;
+        if (sieve[current_element] !=0)
+            for (int j = 2; j * current_element < size_of_sieve; j++)
+            {
+                if (sieve[j * current_element] != 0)
+                {
+                    sieve[j * current_element] += sieve[i];
+                }
+            }
+    }
+    int total_sum = 0;
+    for (int i = 0; i < size_of_sieve; i++)
+    {
+        total_sum += sieve[i];
+    }
+    cout << total_sum << endl;
+    delete[] sieve;
+}
 int main()
 {
     int t;
-    cin>>t;
-    int *arr=new int [750001];
-    while(t--)
+    cin >> t;
+    int *arr = new int[750001];
+    while (t--)
     {
         int n;
-        cin>>n;
-        for(int i=0; i<n; i++)
+        cin >> n;
+        for (int i = 0; i < n; i++)
         {
-            cin>>arr[i];
+            cin >> arr[i];
         }
-        
+        Seive(arr, n);
     }
-    delete[]arr;
+    delete[] arr;
 }
