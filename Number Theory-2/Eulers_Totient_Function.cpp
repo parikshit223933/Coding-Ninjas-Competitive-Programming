f4887e7415e4c31d93e74f4c02a9b4a5604c40e1
parikshit singh <pk223933@gmail.com>
2020-04-01 23:54:21 +0530

    By sir (Implemented by me)

--- Number Theory-2/Eulers_Totient_Function.cpp
+++ Number Theory-2/Eulers_Totient_Function.cpp
@@ -1,31 +1,31 @@
 #include<iostream>
-#include<math.h>
 using namespace std;
-int makeSieve(int n)
+void eulerphi(int n)
 {
-    float *isprime=new float [n+1];
-    for(int i=0; i<n+1; i++)
+    int *phi=new int [n+1];
+    for(int i=1; i<=n; i++)
     {
-        isprime[i]=i;
+        phi[i]=i;
     }
-    for(int i=2; i<=(n); i++)
+    for(int i=2; i<=n; i++)
     {
-        if(isprime[i]==i)
+        if(phi[i]==i)
         {
-            isprime[i]=i-1;
-            for(int j=2; j*i<=n; j++)
+            phi[i]=i-1;
+            for(int j=i*2; j<=n; j+=i)
             {
-                isprime[j*i]*=(1.0-(1.0/(i*1.0)));
+                phi[j]=(phi[j]*(i-1))/i;
             }
         }
     }
-    int ans=isprime[n];
-    delete[]isprime;
-    return ans;
+    cout<< phi[n]<<endl;
+    delete[]phi;
 }
 int main()
 {
     int n;
     cin>>n;
-    cout<<makeSieve(n)<<endl;
+
+    for(int i=1; i<=n; i++)
+    eulerphi(i);
 }
