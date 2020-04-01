155ea6e1f6cdd5094df7b44350444bc8f536cb00
parikshit singh <pk223933@gmail.com>
2020-04-01 06:18:39 +0530

    CN SOL (see prev commits)

--- Applications of Number Theory-1/Card_Game.cpp
+++ Applications of Number Theory-1/Card_Game.cpp
@@ -1,114 +1,101 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <cmath>
-#include <map>
+#include<iostream>
+#include<utility>
+#include<vector>
+#define ll long long int
+#define PII pair<int, int>
+#define f first
+#define s second
+#define mk make_pair
+#define pb push_back
 using namespace std;
-
-map<int,int> returnPrimeFactors(long k)
-{
-    map<int,int> primeFactors;
-    while(k % 2 == 0)
-    {
-        primeFactors[2]++;
-        k /=2 ;
-    }
-
-    for(int i = 3; i <= sqrt(k) ; i++)
-    {
-        while(k % i == 0)
-        {
-            primeFactors[i]++;
-            k /= i;
-        } 
-    }
-
-    if(k > 2)
-        primeFactors[k]++;
-    
-    return primeFactors;
-}
-long long countWaysK(vector<long> arr,long n,long k)
-{
-    map<int,int> primeFactorsK = returnPrimeFactors(k);
-    map<int,int> primeFactorsA;
-    vector<bool> visited(n);
-
-    long i=0,j=0,s=0;
-    long ans = 0;
-    while(i<n && j<n)
-    {
-        // cout<<"\nj "<<j<<" i "<<i;
-        int flag = 0;
-        for(auto it:primeFactorsK)
-        {
-            int p = it.first;
-            int count = 0;
-            long val = arr[j];
-            while(val % p == 0)
-            {
-                count++;
-                val /= p;
-            }
-            if(!visited[j])
-                primeFactorsA[p] += count;
-            // cout<<"\nFact: "<<primeFactorsA[p]<<" "<<it.second;
-            if(primeFactorsA[p] < it.second)
-            {
-                // cout<<"\nFlagged"<<arr[j];
-                flag = 1;
-            }
-        }
-
-        if(!flag)
-        {
-            // cout<<"\nNot Flag: "<<arr[j];
-            ans += (n-j);
-            s = i;
-            visited[j] = true;
-            // cout<<"\nAns: "<<ans;
-            i++;
-            int m = i-1;
-            while(m >= s)
-            {
-                int flag = 0;
-                for(auto it:primeFactorsK)
-                {
-                    int p = it.first;
-                    int count = 0;
-                    long val = arr[m];
-                    while(val % p == 0)
-                    {
-                        count++;
-                        val /= p;
-                    }
-                    primeFactorsA[p] -= count;
-                    // cout<<"\nRem "<<arr[m]<<" "<<primeFactorsA[p]<<" "<<it.second;
-                    if(primeFactorsA[p] < it.second)
-                    {
-                        flag = 1;
-                    }
-                }
-                if(flag)
-                    break;
-                m--;
-            }
-        }
-        else
-            j++;
-    }
-    return ans;
-}
-
 int main()
 {
-    long n,k,i,j;
+    int n, i, j;
+    ll k, temp_for_k, ans = 0;
     cin >> n >> k;
-
-    vector<long> arr(n);
-    for(i = 0; i < n ; i++)
-        cin >> arr[i];
-
-    cout<<countWaysK(arr,n,k)<<endl;
-
+    temp_for_k = k;
+    vector<ll> a(n);
+    for (i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<PII> vp; //store prime factors of k with respective powers
+    for (i = 2; i * i <= temp_for_k; i++)
+    {
+        if (temp_for_k % i == 0)
+        {
+            int count = 0;
+            while (temp_for_k % i == 0)
+            {
+                temp_for_k /= i;
+                count++;
+            }
+            vp.push_back(make_pair(i, count));
+        }
+    }
+    if (temp_for_k != 1)
+    {
+        vp.push_back(make_pair(temp_for_k, 1));
+    }
+    //vq contains all prime factors of k with their respective powers 0
+    vector<PII> vq = vp;
+    for (i = 0; i < vq.size(); i++)
+    {
+        vq[i].s = 0;
+    }
+    //j is back pointer and i is front pointer
+    //We are checking if j..i is divisible by k by adding prime factors of a[i] to vq
+    //If it is divisible, then j...z for all z > i is also divisible by k
+    //So we add n-i to answer
+    //Then we remove j's contribution by subtracting it's prime factors from vq
+    j = 0;
+    for (i = 0; i < n; i++)
+    {
+        //add factors of a[i]
+        for (int z = 0; z < vp.size(); z++)
+        {
+            if (a[i] % vp[z].f == 0)
+            {
+                temp_for_k = a[i];
+                int cn = 0;
+                while (temp_for_k % vp[z].f == 0)
+                {
+                    temp_for_k /= vp[z].f;
+                    cn++;
+                }
+                vq[z].s += cn;
+            }
+        }
+        while (j <= i)
+        {
+            int z;
+            //check if divisible by k from j to i
+            for (z = 0; z < vp.size(); z++)
+            {
+                if (vp[z].s > vq[z].s)
+                    break;
+            }
+            if (z != vp.size())
+                break;
+            //if divisible update ans
+            ans += n - i;
+            //remove factors of a[j] from vq
+            for (int z = 0; z < vp.size(); z++)
+            {
+                if (a[j] % vp[z].f == 0)
+                {
+                    temp_for_k = a[j];
+                    int cn = 0;
+                    while (temp_for_k % vp[z].f == 0)
+                    {
+                        temp_for_k /= vp[z].f;
+                        cn++;
+                    }
+                    vq[z].s -= cn;
+                }
+            }
+            j++;
+        }
+    }
+    cout << ans;
 }
