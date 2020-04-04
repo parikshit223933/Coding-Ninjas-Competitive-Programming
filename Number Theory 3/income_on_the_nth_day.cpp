954be335787ba947514841713480aae18963653c
parikshit singh <pk223933@gmail.com>
2020-04-05 01:56:12 +0530

    CN SOL

--- Number Theory 3/income_on_the_nth_day.cpp
+++ Number Theory 3/income_on_the_nth_day.cpp
@@ -1,40 +1,112 @@
+//https://www.hackerearth.com/problem/algorithm/recurrence/description/
+/*
+F(n) = F(n-1)+F(n-2)+F(n-1)*F(n-2)
+F(n) = (1+F(n-1))*(1+F(n-2)) - 1
+1+F(n) = (1+F(n-1))*(1+F(n-2))
+Let G(n) = 1+F(n)
+-> G(n)=G(n-1)*G(n-2)
+let a=G(0)=1+F(0)
+b=G(1)=1+F(1)
+G(2)=a*b;
+G(3)=a*b^2
+G(4)=a^2*b^3
+.
+.
+G(n)=a^fib(n-1)*b^fib(n)
+where fib is fibbonacci series
+Now fib can also be very large..now as mod is prime, we can use fermat's little theorem
+*/
 #include<iostream>
-#define ll long long int
-#define m 1000000007
+#define ll long long
+#define mod 1000000007
 using namespace std;
-ll income(ll first, ll second, ll n, ll *dp)
+ll fib(ll n)
 {
-    if(n==0)
+    if (n == 0 || n == 1 || n == 5)
+        return n;
+    if (n == 2)
+        return 1;
+    n--;
+    ll a[2][2] = {1, 1,
+                  1, 0};
+    ll ans[2][2] = {
+        1, 0,
+        0, 1};
+    ll temp[2][2];
+    ll m = mod - 1, i, j, k;
+    while (n)
     {
-        return first;
+        if (n & 1)
+        {
+            //ans=ans*a
+            for (i = 0; i < 2; i++)
+                for (j = 0; j < 2; j++)
+                {
+                    temp[i][j] = 0;
+                    for (k = 0; k < 2; k++)
+                    {
+                        temp[i][j] += a[i][k] * ans[k][j];
+                        temp[i][j] %= m;
+                    }
+                }
+            for (i = 0; i < 2; i++)
+                for (j = 0; j < 2; j++)
+                    ans[i][j] = temp[i][j];
+        }
+        //a=a*a
+        for (i = 0; i < 2; i++)
+            for (j = 0; j < 2; j++)
+            {
+                temp[i][j] = 0;
+                for (k = 0; k < 2; k++)
+                {
+                    temp[i][j] += a[i][k] * a[k][j];
+                    temp[i][j] %= m;
+                }
+            }
+        for (i = 0; i < 2; i++)
+            for (j = 0; j < 2; j++)
+                a[i][j] = temp[i][j];
+        n >>= 1;
     }
-    if(n==1)
+    return ans[0][0];
+}
+ll mpow(ll a, ll b)
+{
+    ll ans = 1;
+    while (b)
     {
-        return second;
+        if (b & 1)
+            ans = (ans * a) % mod;
+        a = (a * a) % mod;
+        b >>= 1;
     }
-    if(dp[n]!=-1)
-    {
-        return dp[n]%m;
-    }
-    ll first_part=income(first, second, n-1, dp)%m;
-    ll second_part=income(first, second, n-2, dp)%m;
-    ll next=(first_part+second_part+((first_part%m)*(second_part%m))%m)%m;
-    dp[n]=next%m;
-    return next%m;
+    return ans;
 }
 int main()
 {
-    ll t;
-    cin>>t;
-    while(t--)
+    int t;
+    ll a, b, c, n, i, x, y;
+    cin >> t;
+    while (t--)
     {
-        ll first, second, n;
-        cin>>first>>second>>n;
-        ll *dp=new ll [n+1];
-        for(ll i=0; i<=n; i++)
+        cin >> a >> b >> n;
+        if (n == 0)
+            cout << a << endl;
+        else if (n == 1)
+            cout << b << endl;
+        else
         {
-            dp[i]=-1;
+            x = fib(n - 1);
+            y = fib(n);
+            //cout<<x<<" "<<y<<endl;
+            c = mpow(a + 1, x) * mpow(b + 1, y);
+            c--;
+            c = c % mod;
+            if (c < 0)
+                c += mod;
+            cout << c << endl;
         }
-        cout<<income(first, second, n, dp)<<endl;
     }
+    return 0;
 }
