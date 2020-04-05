be65b21aec8d0a7b357b64fd95e866cfe477f50d
parikshit singh <pk223933@gmail.com>
2020-04-05 09:46:48 +0530

    staged

--- Hackwithinfy/Codechef/UNIT_GCD.cpp
+++ Hackwithinfy/Codechef/UNIT_GCD.cpp
@@ -1,39 +1,116 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include<math.h>
 #include<algorithm>
 using namespace std;
-int stickers(int *arr, int n, int m, int k)
+bool find(vector<int> v, int element)
 {
-    if(n==0||m==0)
+    for (int i = 0; i < v.size(); i++)
     {
-        return 0;
+        if (v[i] == element)
+        {
+            return true;
+        }
     }
-    if(k!=0)
+    return false;
+}
+bool have_any_element_in_common(vector<int> v1, vector<int> v2)
+{
+    bool ans = false;
+    ;
+    for (auto i : v1)
     {
-        int option1=arr[0]+stickers(arr, n, m-1, k-1);
-        int option2=arr[0]+stickers(arr+1, n-1, m-1, k);
-        int option3=stickers(arr+1, n-1, m, k);
-        return max(option1, max(option2, option3));
+        if (find(v2, i))
+        {
+            ans = true;
+            break;
+        }
     }
-    else
+    return ans;
+}
+void makeSieve(int n)
+{
+    bool *isprime = new bool[n + 1];
+    for (int i = 0; i < n + 1; i++)
     {
-        int option1=stickers(arr+1, n-1, m, )
+        isprime[i] = true;
     }
-    
+    isprime[0] = false;
+    isprime[1] = false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (isprime[i])
+            for (int j = i; j * i <= n; j++)
+            {
+                isprime[j * i] = false;
+            }
+    }
+    vector<int> *prime_factors = new vector<int>[n + 1];
+    bool *marked = new bool[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        marked[i] = false;
+    }
+    vector<vector<int>> ans;
+    for (int i = 2; i <= n; i++)
+    {
+        if (isprime[i])
+        {
+            for (int j = 1; j * i <= n; j++)
+            {
+                prime_factors[j * i].push_back(i);
+            }
+        }
+    }
+    delete[] isprime;
+    for (int i = 2; i <= n; i++)
+    {
+        if (marked[i])
+        {
+            continue;
+        }
+        vector<int> temp;
+        temp.push_back(i);
+        if(i==2)//to adjust 1
+        {
+            temp.push_back(1);
+        }
+        marked[i] = true;
+        for (int j = i+1; j <= n; j++)
+        {
+            if(marked[j])
+            {
+                continue;
+            }
+            if (!have_any_element_in_common(prime_factors[i], prime_factors[j]))
+            {
+                temp.push_back(j);
+                marked[j] = true;
+            }
+        }
+        ans.push_back(temp);
+    }
+    int a=ans.size();
+    cout<<a<<endl;
+    for (auto i : ans)
+    {
+        cout<<i.size()<<" ";
+        sort(i.begin(), i.end());
+        for (auto j : i)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
 }
 int main()
 {
-    //n=number of stickers
-    //m=the number of times you can use stickers
-    //k=number of time i can use the same sticker in a row
-
-    int n, m, k;
-    cin>>n>>m>>k;
-    int n;
-    cin>>n;
-    int *arr=new int [n];
-    for(int i=0; i<n; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        cin>>arr[i];
+        int n;
+        cin >> n;
+        makeSieve(n);
     }
-    cout<<stickers(arr, n, m, k)<<endl;
 }
