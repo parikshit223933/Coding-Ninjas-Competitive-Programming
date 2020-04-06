ff03d26383829415a1aeab8ef0c9daf191747b55
parikshit singh <pk223933@gmail.com>
2020-04-06 23:56:04 +0530

    Implemented

--- Computational Geometry/Area_of_a_Polygon.cpp
+++ Computational Geometry/Area_of_a_Polygon.cpp
@@ -19,7 +19,16 @@ class polygon
 double area(polygon p, int n)
 {
     double total_area=0;
-    
+    for(int i=1; i<n-1; i++)
+    {
+        double x1=p.points[i].x-p.points[0].x;
+        double y1=p.points[i].y-p.points[0].y;
+        double x2=p.points[i+1].x-p.points[0].x;
+        double y2=p.points[i+1].y-p.points[0].y;
+        double cross_product=x1*y2-y1*x2;
+        total_area+=cross_product;
+    }
+    return abs(total_area/2);
 }
 int main()
 {
