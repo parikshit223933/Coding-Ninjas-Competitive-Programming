e3f9c1a617ed947062ecbf81170bcf7d492e3943
parikshit singh <pk223933@gmail.com>
2020-04-07 00:17:19 +0530

    Implemented

---
+++ Computational Geometry/Area_of_a_convex_polygon.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+class point
+{
+    public:
+    double x, y;
+};
+class polygon
+{
+    public:
+    point*points;
+    polygon(int number_of_points)
+    {
+        points=new point [number_of_points];
+    }
+
+};
+double area(polygon p, int n)
+{
+    double total_area=0;
+    for(int i=1; i<n-1; i++)
+    {
+        double x1=p.points[i].x-p.points[0].x;
+        double y1=p.points[i].y-p.points[0].y;
+        double x2=p.points[i+1].x-p.points[0].x;
+        double y2=p.points[i+1].y-p.points[0].y;
+        double cross_product=x1*y2-y1*x2;
+        total_area+=cross_product;
+    }
+    return abs(total_area/2);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    polygon p(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>p.points[i].x;
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin>>p.points[i].y;
+    }
+    //area
+    cout<<(int)area(p, n)<<endl;
+}
