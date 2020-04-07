e338aecb7b0aa9410e9b68794d748316b347e051
parikshit singh <pk223933@gmail.com>
2020-04-07 13:20:34 +0530

    Revise it again

--- Computational Geometry/Ware_House.cpp
+++ Computational Geometry/Ware_House.cpp
@@ -1,66 +1,275 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <vector>
+#include <deque>
+#include <set>
+#include <map>
 #include <algorithm>
-#define endl '\n'
-#include <climits>
-#include <math.h>
+#include <utility>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
 using namespace std;
+//64 bit integer definition
+#ifdef WIN32
+#define in_routine(type, spec)               \
+    istream &operator>>(istream &s, type &d) \
+    {                                        \
+        char b[30];                          \
+        s >> b;                              \
+        sscanf(b, spec, &d);                 \
+        return s;                            \
+    }
+#define out_routine(type, spec)             \
+    ostream &operator<<(ostream &s, type d) \
+    {                                       \
+        char b[30];                         \
+        sprintf(b, spec, d);                \
+        s << b;                             \
+        return s;                           \
+    }
+typedef signed __int64 i64;
+in_routine(i64, "%I64d") out_routine(i64, "%I64d") typedef unsigned __int64 u64;
+in_routine(u64, "%I64u") out_routine(u64, "%I64u")
+#else
+typedef signed long long i64;
+typedef unsigned long long u64;
+#endif
+//common routines
+#ifdef WIN32
+#define min(a, b) _cpp_min(a, b)
+#define max(a, b) _cpp_max(a, b)
+#endif
+#define abs(a) ((a) > 0 ? (a) : -(a))
+#define s2d(s, d)              \
+    {                          \
+        istringstream(s) >> d; \
+    }
+#define d2s(d, s)        \
+    {                    \
+        ostringstream t; \
+        t << d;          \
+        s = t.str();     \
+    }
+    int gcd(int a, int b)
+{
+    for (int c; b; c = a, a = b, b = c % b)
+        ;
+    return a;
+}
+int lcm(int a, int b) { return a / gcd(a, b) * b; }
+//remove routine
+template <class T>
+void remove(vector<T> &v, const T &e)
+{
+    v.resize(remove(v.begin(), v.end(), e) - v.begin());
+}
+template <class T>
+vector<T> remove_copy(vector<T> &v, const T &e)
+{
+    vector<T> result;
+    remove_copy(v.begin(), v.end(), back_inserter(result), e);
+    return result;
+}
+//output routine
+ostream &operator<<(ostream &s, string d)
+{
+    s << '\"' << d.c_str() << '\"';
+    return s;
+}
+template <class T>
+ostream &operator<<(ostream &s, vector<T> d)
+{
+    s << "{";
+    for (typename vector<T>::iterator i = d.begin(); i != d.end(); i++)
+        s << (i != d.begin() ? "," : "") << *i;
+    s << "}";
+    return s;
+}
+//parsing routine
+template <class T>
+vector<basic_string<T>> parse(const basic_string<T> &s, const basic_string<T> &delim)
+{
+    vector<basic_string<T>> ret(0);
+
+    for (int b, e = 0;; ret.push_back(s.substr(b, e - b)))
+        if ((b = s.find_first_not_of(delim, e)) == (e = s.find_first_of(delim, b)))
+            return ret;
+}
+vector<int> intparse(const string &s, const string &delim = " \t\n")
+{
+    vector<string> tmp = parse(s, delim);
+    vector<int> ret(0);
+    int t;
+    for (vector<string>::iterator i = tmp.begin(); i != tmp.end(); i++)
+        sscanf(i->c_str(), "%d", &t), ret.push_back(t);
+    return ret;
+}
+//name mapper
+class mapper
+{
+public:
+    map<string, int> m;
+    vector<string> v;
+    void reset()
+    {
+        v.clear();
+        m.clear();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    int get(const string str)
+    {
+        if (m.find(str) == m.end())
+        {
+            m[str] = v.size();
+            v.push_back(str);
+        }
+        return m[str];
+    }
+    string get(int i)
+    {
+        return v[i];
+    }
+    vector<int> get(const vector<string> &strs)
+    {
+        vector<int> ret;
+        ret.reserve(strs.size() + 2);
+        for (int i = 0; i < strs.size(); i++)
+            ret.push_back(get(strs[i]));
+        return ret;
+    }
+    vector<string> get(const vector<int> &is)
+    {
+        vector<string> ret;
+        ret.reserve(is.size() + 2);
+        for (int i = 0; i < is.size(); i++)
+            ret.push_back(get(is[i]));
+        return ret;
+    }
+};
 struct point
 {
     double x, y;
-};
-inline double distance(double x1, double y1, double x2, double y2)
-{
-    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-}
-inline bool sorter(point p1, point p2)
-{
-    if (p1.y < p2.y)
+    point() {}
+    point(double x, double y) : x(x), y(y) {}
+    point operator-(point p)
     {
-        return true;
+        return point(x - p.x, y - p.y);
     }
-    return false;
-}
+    double operator*(point p)
+    {
+        return x * p.y - y * p.x;
+    }
+    double dist(point p)
+    {
+        return sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+    }
+};
+class Warehouse
+{
+public:
+    int n;
+
+    vector<int> xx, yy;
+    double len(point a, point b, point c)
+    {
+        return ((c - a) * (c - b)) / a.dist(b);
+    }
+    int calc(point a, point b)
+    {
+        vector<double> ret;
+        for (int i = 0; i < n; ++i)
+        {
+            ret.push_back(len(a, b, point(xx[i], yy[i])));
+        }
+        sort(ret.begin(), ret.end());
+        double m1 = len(a, b, point(0, 0));
+        double m2 = len(a, b, point(200, 0));
+        double m3 = len(a, b, point(0, 200));
+        double m4 = len(a, b, point(200, 200));
+        double lower, upper;
+        if (max(m1, m2) > max(m3, m4))
+        {
+            upper = min(m1, m2);
+            lower = max(m3, m4);
+        }
+        else
+        {
+            upper = min(m3, m4);
+            lower = max(m1, m2);
+        }
+        int tt = 0;
+        for (int i = 0; i < n - 1; ++i)
+        {
+            double from = ret[i], to = ret[i + 1];
+            if (from < lower - 1e-8)
+                continue;
+            if (to > upper + 1e-8)
+                continue;
+            int t = (int)floor(to - from - 1e-8);
+            if (t > tt)
+                tt = t;
+        }
+        return tt;
+    }
+    int feetWide(vector<int> x, vector<int> y)
+    {
+        x.push_back(0), y.push_back(0);
+        x.push_back(0), y.push_back(200);
+        x.push_back(200), y.push_back(0);
+        x.push_back(200), y.push_back(200);
+        n = x.size();
+        xx = x, yy = y;
+        int ret = 0;
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)
+                if (x[i] != x[j])
+                {
+                    int t = calc(point(x[i], y[i]), point(x[j], y[j]));
+                    if (t > ret)
+                        ret = t;
+                }
+        for (int i = 0; i < n; ++i)
+            for (int j = i + 1; j < n; ++j)
+                if (y[i] != y[j])
+                {
+                    point a(x[i], y[i]);
+                    point b(x[j], y[j]);
+                    point c = a;
+                    c.x += b.y - a.y;
+                    c.y += a.x - b.x;
+                    int t = calc(a, c);
+                    if (t > ret)
+                        ret = t;
+                }
+        return ret;
+    }
+};
 int main()
 {
     int n;
     cin >> n;
-    vector<double> x_points;
-    vector<double> y_points;
+    vector<int> x;
+    vector<int> y;
     for (int i = 0; i < n; i++)
     {
-        double element;
-        cin >> element;
-        x_points.push_back(element);
+        int curr;
+        cin >> curr;
+        x.push_back(curr);
     }
+
     for (int i = 0; i < n; i++)
     {
-        double element;
-        cin >> element;
-        y_points.push_back(element);
+        int curr;
+        cin >> curr;
+        y.push_back(curr);
     }
-    point *arr = new point[n];
-    for (int i = 0; i < n; i++)
-    {
-        point p;
-        p.x = x_points[i];
-        p.y = y_points[i];
-        arr[i] = p;
-    }
-    sort(arr, arr + n, sorter);
-    double max_distance = INT_MIN;
-    for (int i = 0; i < n - 1; i++)
-    {
-        max_distance = max(max_distance, distance(arr[i].x, arr[i].y, arr[i + 1].x, arr[i + 1].y));
-    }
-    max_distance=max(max_distance, 200-arr[n-1].y);
-    max_distance=max(max_distance, arr[0].y-0);
-    if(max_distance==(double)((int)max_distance))
-    {
-        cout<<max_distance-1<<endl;
-    }
-    else
-    {
-        cout<<(int)max_distance<<endl;
-    }
+    Warehouse w;
+    cout << w.feetWide(x, y) << endl;
+    return 0;
 }
--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,21 +1,73 @@
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
 using namespace std;
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        vector<int>*egdes=new vector<int>[n];
-        for(int i=0; i<n-1; i++)
-        {
-            int j, k;
-            cin>>j>>k;
-            edges[j-1].push_back(k-1);
-            edges[k-1].push_back(j-1);
+int outside_the_line(ll a,ll b,ll c,ll x1,ll y1){
+    ll intersection=-c-a*x1;
+    if(b!=0) intersection/=b;
+    if(y1>intersection) return 1; // point is opposite side of Zero
+    else if(y1<intersection) return -1; // point is same side of Zero
+    else return 0; // point is on the line
+}
+ll perpendicular_dist(ll a,ll b,ll c,ll x1,ll y1){
+    double num=a*x1+b*y1+c;
+    double den=sqrt(a*a+b*b);
+    return ceil(num/den);
+}
+ll work1(vector<pair<ll,ll>> posts,ll n){
+    vector<ll> y;
+    y.pb(0);
+    for(ll i=0;i<posts.size();i++){
+        if(posts[i].ff<=200&&posts[i].ss<=200){
+            y.pb(posts[i].ss);
         }
     }
+    y.pb(200);
+    sort(y.begin(),y.end());
+    ll ans=0;
+    for(ll i=0;i<y.size()-1;i++){
+        ans=max(ans,(y[i+1]-y[i]));
+    }
+    return ans-1;
+}
+int main() {
+    ll n;
+    cin>>n;
+    vector<pair<ll,ll>> posts;
+    posts.resize(n+1);
+    for(ll i=0;i<n;i++){
+        cin>>posts[i].ff;
+    }
+    for(ll i=0;i<n;i++){
+        cin>>posts[i].ss;
+    }
+    ll work1_ans=work1(posts,n);
+    posts.pb({0,0});
+    ll ans=INT_MIN;
+    for(ll i=0;i<posts.size();i++){
+        ll a=posts[i].second;
+        ll b=200-posts[i].first;
+        ll c=-1*200*a;
+        vector<ll> curr_dist;
+        for(ll j=0;j<posts.size();j++){
+            if(i==j)continue;
+            if(outside_the_line(a,b,c,posts[j].ff,posts[j].ss)==1){
+                curr_dist.pb(perpendicular_dist(a,b,c,posts[j].ff,posts[j].ss));
+            }
+        }
+        sort(curr_dist.begin(),curr_dist.end());
+        for(ll j=curr_dist.size()-1;j>0;j--){
+            curr_dist[j]-=curr_dist[j-1];
+        }
+        ll curr_ans=INT_MIN;
+        for(ll j=0;j<curr_dist.size();j++){
+            // cout<<curr_dist[j]<<" ";
+            curr_ans=max(curr_ans,curr_dist[j]);
+        }
+        // cout<<endl;
+        ans=max(ans,curr_ans);
+    }
+    cout<<max(work1_ans,ans-1)<<endl;
 }
