13b5c893bee918d6537a09b5996a5b5fdd47ac00
parikshit singh <pk223933@gmail.com>
2020-04-09 09:40:39 +0530

    Done

--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,57 +1,59 @@
 #include<iostream>
 #include<climits>
+#include<iterator>
 #include<unordered_map>
 #define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define endl '\n'
 #define um unordered_map<int, int>
 using namespace std;
-void choices(int **arr, int n, int m, int k)
+void choices(int** arr, int n, int m, int k)
 {
-    um*arr_mapper=new um[n];
-    for(int i=0; i<n; i++)
+    um* arr_mapper = new um[n];
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<k; j++)
+        for (int j = 0; j < k; j++)
         {
-            arr_mapper[i][k]++;
+            arr_mapper[i][arr[i][j]] += 1;
         }
     }
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        int maximum_frequency_number;
-        int maximum_frequency=INT_MIN;
-        for(auto j:arr_mapper[i])
+        int maximum_frequency_number=0;
+        int maximum_frequency = INT_MIN;
+        unordered_map<int, int> m = arr_mapper[i];
+        for (auto j : m)
         {
-            if(j.second>maximum_frequency)
+            if (j.second > maximum_frequency)
             {
-                maximum_frequency=j.second;
-                maximum_frequency_number=j.first;
+                maximum_frequency = j.second;
+                maximum_frequency_number = j.first;
             }
         }
-        cout<<maximum_frequency_number<<" ";
+        cout << maximum_frequency_number << " ";
     }
     delete[]arr_mapper;
 }
 int main()
 {
     fast
-    int t;
-    cin>>t;
-    while(t--)
+        int t;
+    cin >> t;
+    while (t--)
     {
         int n, m, k;
-        cin>>n>>m>>k;
-        int **arr=new int *[n];
-        for(int i=0; i<n; i++)
+        cin >> n >> m >> k;
+        int** arr = new int* [n];
+        for (int i = 0; i < n; i++)
         {
-            arr[i]=new int[k];
-            for(int j=0; j<k; j++)
+            arr[i] = new int[k];
+            for (int j = 0; j < k; j++)
             {
-                cin>>arr[i][j];
-            } 
+                cin >> arr[i][j];
+            }
         }
         choices(arr, n, m, k);
-        cout<<endl;
-        for(int i=0; i<n; i++)
+        cout << endl;
+        for (int i = 0; i < n; i++)
         {
             delete[]arr[i];
         }
