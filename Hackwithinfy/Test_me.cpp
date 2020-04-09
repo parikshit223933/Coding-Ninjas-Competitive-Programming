47808007539dfecf64aae2703ec747b26f6c33e3
parikshit singh <pk223933@gmail.com>
2020-04-09 17:18:27 +0530

    Staged

--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,83 +1,71 @@
 #include <iostream>
 #include <vector>
+#include<math.h>
 #include<unordered_map>
 #define endl '\n'
 #define fast                          \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
+#define ll long long int
+#define mod 1000000007
 using namespace std;
-const int MAX = 1000001;
-
-int factor[MAX] = { 0 };
-unordered_map<int, int>m;
-void generatePrimeFactors()
+vector<ll>primes;
+ll MAX = 1000001;
+unordered_map<ll, ll>m;
+void makeSieve()
 {
-    factor[1] = 1;
-
-    for (int i = 2; i < MAX; i++)
-        factor[i] = i;
-
-    for (int i = 4; i < MAX; i += 2)
-        factor[i] = 2;
-
-    for (int i = 3; i * i < MAX; i++)
+    bool* isprime = new bool[MAX];
+    for (ll i = 0; i < MAX; i++)
     {
-
-        if (factor[i] == i)
-        {
-
-            for (int j = i * i; j < MAX; j += i)
-            {
-
-                if (factor[j] == j)
-                    factor[j] = i;
-            }
-        }
+        isprime[i] = true;
     }
-}
-
-int calculateNoOFactors(int n)
-{
-    if (n == 1)
-        return 1;
-    int ans = 1;
-    int dup = factor[n];
-    int c = 1;
-    int j = n / factor[n];
-    while (j != 1)
+    isprime[0] = false;
+    isprime[1] = false;
+    for (ll i = 2; i <= sqrt(MAX-1); i++)
     {
-        if (factor[j] == dup)
+        if (isprime[i])
+            for (ll j = i; j * i <= MAX-1; j++)
+            {
+                isprime[j * i] = false;
+            }
+    }
+    for (ll i = 0; i < MAX; i++)
+    {
+        if (isprime[i])
+            primes.push_back(i);
+    }
+    delete[]isprime;
+}
+int calculateNoOFactors(ll n)
+{
+    ll i = 0;
+    while (n != 1)
+    {
+        if (n % primes[i] == 0)
         {
-            c += 1;
+            n /= primes[i];
+            m[primes[i]]++;
         }
-            
-           
         else
         {
-            dup = factor[j];
-            ans = ans * (c + 1);
-            c = 1;
+            i++;
         }
-        j = j / factor[j];
-        m[factor[j]]++;
     }
-    ans = ans * (c + 1);
-    m[dup]++;
-    return ans;
+    return 1;
 }
-int happiness_quotient(vector<int>* edges, int n, int* arr, int start, int end, unordered_map<int, int>&visited)
+int happiness_quotient(vector<ll>* edges, ll n, ll* arr, ll start, ll end, unordered_map<ll, ll>&visited)
 {
     if (start == end)
     {
-        calculateNoOFactors(arr[end]);
+        return calculateNoOFactors(arr[end]);
     }
-    int total_happiness = 0;
-    for (int i = 0; i < edges[start].size(); i++)
+    ll total_happiness = 0;
+    for (ll i = 0; i < edges[start].size(); i++)
     {
         if (visited[i] == 0)
         {
             visited[i] = 1;
-            int forward_happiness = happiness_quotient(edges, n, arr, edges[start][i], end, visited);
+            ll forward_happiness = happiness_quotient(edges, n, arr, edges[start][i], end, visited);
             if (forward_happiness != 0)
             {
                 total_happiness = calculateNoOFactors(arr[start]) + forward_happiness;
@@ -91,39 +79,39 @@ int happiness_quotient(vector<int>* edges, int n, int* arr, int start, int end, 
 int main()
 {
     fast
-    generatePrimeFactors();
-    int t;
+    makeSieve();
+    ll t;
     cin >> t;
     while (t--)
     {
-        int n;
+        ll n;
         cin >> n;
-        vector<int>* edges = new vector<int>[n];
-        for (int i = 0; i < n - 1; i++) //adjacency list for edges(roads connecting two diff. cities)
+        vector<ll>* edges = new vector<ll>[n];
+        for (ll i = 0; i < n - 1; i++) //adjacency list for edges(roads connecting two diff. cities)
         {
-            int x, y;
+            ll x, y;
             cin >> x >> y;
             edges[x - 1].push_back(y - 1);
             edges[y - 1].push_back(x - 1);
         }
-        int* arr = new int[n]; //cost of a house in city i is Ai
-        for (int i = 0; i < n; i++)
+        ll* arr = new ll[n]; //cost of a house in city i is Ai
+        for (ll i = 0; i < n; i++)
         {
             cin >> arr[i];
         }
-        int q; //number of queries
+        ll q; //number of queries
         cin >> q;
-        unordered_map<int, int> visited;
+        unordered_map<ll, ll> visited;
         while (q--)
         {
             visited.clear();
-            int u, v;
+            ll u, v;
             cin >> u >> v;
-            int a=happiness_quotient(edges, n, arr, u-1, v-1, visited);
-            int ans = 1;
+            ll a=happiness_quotient(edges, n, arr, u-1, v-1, visited);
+            ll ans = 1;
             for (auto i : m)
             {
-                ans *= (i.second + 1);
+                ans =(ans%mod* (i.second + 1)%mod)%mod;
             }
             cout << ans << endl;
             m.clear();
