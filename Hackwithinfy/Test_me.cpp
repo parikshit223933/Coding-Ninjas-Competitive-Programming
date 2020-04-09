a6fe7f46ddd8b0f05380be92c1be134ccc3d6628
parikshit singh <pk223933@gmail.com>
2020-04-09 09:08:29 +0530

    Staged

--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,73 +1,61 @@
-#include<bits/stdc++.h>
-#define ll long long
-#define ff first
-#define ss second
-#define pb push_back
+#include<iostream>
+#include<climits>
+#include<unordered_map>
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define endl '\n'
+#define um unordered_map<int, int>
 using namespace std;
-int outside_the_line(ll a,ll b,ll c,ll x1,ll y1){
-    ll intersection=-c-a*x1;
-    if(b!=0) intersection/=b;
-    if(y1>intersection) return 1; // point is opposite side of Zero
-    else if(y1<intersection) return -1; // point is same side of Zero
-    else return 0; // point is on the line
-}
-ll perpendicular_dist(ll a,ll b,ll c,ll x1,ll y1){
-    double num=a*x1+b*y1+c;
-    double den=sqrt(a*a+b*b);
-    return ceil(num/den);
-}
-ll work1(vector<pair<ll,ll>> posts,ll n){
-    vector<ll> y;
-    y.pb(0);
-    for(ll i=0;i<posts.size();i++){
-        if(posts[i].ff<=200&&posts[i].ss<=200){
-            y.pb(posts[i].ss);
+void choices(int **arr, int n, int m, int k)
+{
+    um*arr_mapper=new um[n];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<k; j++)
+        {
+            arr_mapper[i][k]++;
         }
     }
-    y.pb(200);
-    sort(y.begin(),y.end());
-    ll ans=0;
-    for(ll i=0;i<y.size()-1;i++){
-        ans=max(ans,(y[i+1]-y[i]));
-    }
-    return ans-1;
-}
-int main() {
-    ll n;
-    cin>>n;
-    vector<pair<ll,ll>> posts;
-    posts.resize(n+1);
-    for(ll i=0;i<n;i++){
-        cin>>posts[i].ff;
-    }
-    for(ll i=0;i<n;i++){
-        cin>>posts[i].ss;
-    }
-    ll work1_ans=work1(posts,n);
-    posts.pb({0,0});
-    ll ans=INT_MIN;
-    for(ll i=0;i<posts.size();i++){
-        ll a=posts[i].second;
-        ll b=200-posts[i].first;
-        ll c=-1*200*a;
-        vector<ll> curr_dist;
-        for(ll j=0;j<posts.size();j++){
-            if(i==j)continue;
-            if(outside_the_line(a,b,c,posts[j].ff,posts[j].ss)==1){
-                curr_dist.pb(perpendicular_dist(a,b,c,posts[j].ff,posts[j].ss));
+    for(int i=0; i<n; i++)
+    {
+        int maximum_frequency_number;
+        int maximum_frequency=INT_MIN;
+        for(auto j:arr_mapper[i])
+        {
+            if(j.second>maximum_frequency)
+            {
+                maximum_frequency=j.second;
+                maximum_frequency_number=j.first;
             }
         }
-        sort(curr_dist.begin(),curr_dist.end());
-        for(ll j=curr_dist.size()-1;j>0;j--){
-            curr_dist[j]-=curr_dist[j-1];
-        }
-        ll curr_ans=INT_MIN;
-        for(ll j=0;j<curr_dist.size();j++){
-            // cout<<curr_dist[j]<<" ";
-            curr_ans=max(curr_ans,curr_dist[j]);
-        }
-        // cout<<endl;
-        ans=max(ans,curr_ans);
+        cout<<maximum_frequency_number<<" ";
     }
-    cout<<max(work1_ans,ans-1)<<endl;
+    delete[]arr_mapper;
+}
+int main()
+{
+    fast
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m, k;
+        cin>>n>>m>>k;
+        int **arr=new int *[n];
+        for(int i=0; i<n; i++)
+        {
+            arr[i]=new int[k];
+            for(int j=0; j<k; j++)
+            {
+                cin>>arr[i][j];
+            } 
+        }
+        choices(arr, n, m, k);
+        cout<<endl;
+        for(int i=0; i<n; i++)
+        {
+            delete[]arr[i];
+        }
+        delete[]arr;
+    }
+    return 0;
 }
