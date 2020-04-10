c5f0dd8a9b087502acccfaee3fba55593173d404
parikshit singh <pk223933@gmail.com>
2020-04-10 15:09:03 +0530

    Cubic Square Implemented

--- Number Theory 3/Cubic_square.cpp
+++ Number Theory 3/Cubic_square.cpp
@@ -1,13 +1,46 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+int power(long long int a, string b, int m)
+{
+	int len = b.length();
+	long long int final_ans = 1;
+	for(int i=len-1;i>=0;i--)
+	{
+		if(b[i] == '0')
+		{	
+			a %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+		if(b[i] == '1')
+		{	
+			final_ans = final_ans * a;
+			final_ans %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+		if(b[i] == '2')
+		{
+			final_ans = ((final_ans * a)%m) * a;
+			final_ans %= m;
+			a = (((a*a)%m)*a)%m;
+		}
+	}
+	return final_ans;
+}
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int a, b, m;
-        cin>>a>>b>>m;
-        
-    }
+	long long int t;
+	cin >> t;
+	//write code here
+	while(t--)
+	{
+		long long int a; //in base 10
+		string b; //in base 3
+		int m; //in base 10
+		cin >> a >> b >> m;
+
+		cout << power(a,b,m) << endl;
+	}
+
+	return 0 ; 
+
 }
