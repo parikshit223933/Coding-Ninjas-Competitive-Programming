6d8b533e5fa70c6c9a54c303ea3d8ae3ed9b1585
parikshit singh <pk223933@gmail.com>
2020-04-11 12:21:21 +0530

    Staged

--- Hackwithinfy/Codechef/test.cpp
+++ Hackwithinfy/Codechef/test.cpp
@@ -3,25 +3,41 @@
 #define fast ios::sync_with_stdio(false);cin.tie(0)
 #define ll long long int
 using namespace std;
-inline bool check_for_divisibility(ll x)
-{
-    if (abs(x) % 2 == 1 || x == 0 || abs(x) % 4 == 0)
-    {
-        return true;
-    }
-    return false;
-}
 ll count_subs(ll* arr, ll n)
 {
-    ll count=0;
+    ll total_count = 0;
+    int multiplier=1;
     for(int i=0; i<n; i++)
     {
-        if(arr[i]==0||arr[i]%4==0)
+        multiplier=multiplier*arr[i];
+        if(arr[i]%4==0||arr[i]==0)//divisible by 4 or 0
         {
-            count+=n-i;
+            total_count+=n-i;
         }
-        else if()
+        else if (multiplier%2!=0)//is odd
+        {
+            int count=0;
+            while(multiplier%4!=0&&i<n)
+            {
+                i++;
+                multiplier*=arr[i];
+                count++;
+            }
+            total_count+=(count*(count+1))/2;
+        }
+        else if (multiplier%2==0)
+        {
+            int count=0;
+            while(multiplier%2!=0&&i<n)
+            {
+                i++;
+                multiplier*=arr[i];
+                count++;
+            }
+            total_count+=(count*(count+1))/2;
+        }
     }
+    return total_count;
 }
 int main()
 {
