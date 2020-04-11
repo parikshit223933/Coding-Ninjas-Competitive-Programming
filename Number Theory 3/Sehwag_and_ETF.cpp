5ba27b8a75576f7e4f48d2a65a8a33fe85327d2b
parikshit singh <pk223933@gmail.com>
2020-04-11 21:15:09 +0530

    CN sol

--- Number Theory 3/Sehwag_and_ETF.cpp
+++ Number Theory 3/Sehwag_and_ETF.cpp
@@ -1,45 +1,85 @@
-#include<iostream>
-#include<vector>
-#define endl '\n'
-#define int long long int
-#define fast ios_base::sync_with_stdio(false);cin.tie(0);
+#include<bits/stdc++.h>
+#define ll long long
+#define MAX 1100001
+#define ss(n) scanf("%lld", &n)
 using namespace std;
-vector<int>primes;
-void makeSieve(int n=1000000)
-{
-    bool *isprime=new bool [n+1];
-    for(int i=0; i<n+1; i++)
-    {
-        isprime[i]=true;
-    }
-    isprime[0]=false;
-    isprime[1]=false;
-    for(int i=2; i<=sqrt(n); i++)
-    {
-        if(isprime[i])
-            for(int j=i; j*i<=n; j++)
-            {
-                isprime[j*i]=false;
-            }
-    }
-    int count=0;
-    for(int i=0; i<n+1; i++)
-    {
-        if(isprime[i])
-        primes.push_back(i);
-    }
-    delete[]isprime;
-}
+bool primes[MAX];
+ll phi[MAX];
+ll p[300001];
+ll num[MAX];
 int main()
 {
-    fast
-    makeSieve();
-    int t;
-    cin>>t;
-    while(t--)
+    ll t, a, b, k, i, j, x, base;
+    cin >> t;
+    for (i = 3; i < MAX; i += 2)
+        primes[i] = 1;
+    primes[2] = 1;
+    for (i = 3; i * i < MAX; i += 2)
     {
-        int l, r, k;
-        cin>>l>>r>>k;
-
+        if (primes[i])
+        {
+            for (j = i * i; j < MAX; j += 2 * i)
+                primes[j] = 0;
+        }
     }
+    p[0] = 2;
+    ll c = 1;
+    for (i = 3; i < MAX; i += 2)
+    {
+        if (primes[i])
+            p[c++] = i;
+    }
+    while (t--)
+    {
+        cin >> a >> b >> k;
+        if (k == 1)
+        {
+            std::cout << std::fixed;
+            std::cout << std::setprecision(6) << 1 << endl;
+            continue;
+        }
+        for (i = a; i <= b; i++)
+        {
+            phi[i - a] = i;
+            num[i - a] = i;
+        }
+        for (i = 0; p[i] * p[i] <= b; i++)
+        {
+            base = a / p[i] * p[i];
+            while (base < a)
+                base += p[i];
+            while (base < p[i])
+                base += p[i];
+            if (base == p[i])
+            {
+                // cout<<phi[base-a]<<endl;
+                base += p[i];
+            }
+            for (j = base; j <= b; j += p[i])
+            {
+                while (num[j - a] % p[i] == 0)
+                    num[j - a] /= p[i];
+                phi[j - a] -= phi[j - a] / p[i];
+                //cout<<j<<" "<<phi[j-a]<<endl;
+            }
+        }
+        for (i = a; i <= b; i++)
+        {
+            if (num[i - a] > 1)
+                phi[i - a] -= phi[i - a] / num[i - a];
+            num[i - a] = 1;
+        }
+        ll c = 0;
+        for (i = a; i <= b; i++)
+        {
+            //cout<<i<<" "<<phi[i-a]<<endl;
+            if (phi[i - a] % k == 0)
+                c++;
+        }
+        double ans = c;
+        ans /= (b - a + 1);
+        std::cout << std::fixed;
+        std::cout << std::setprecision(6) << ans << endl;
+    }
+    return 0;
 }
