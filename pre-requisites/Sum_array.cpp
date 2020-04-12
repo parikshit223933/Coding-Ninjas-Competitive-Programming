563913ec9fc5c0591819aadcb0d0a63839b9d8ed
parikshit singh <pk223933@gmail.com>
2020-04-12 23:21:18 +0530

    Added Question

--- pre-requisites/Sum_array.cpp
+++ pre-requisites/Sum_array.cpp
@@ -1,3 +1,22 @@
+/* Sum Array
+
+Given a 2-d square matrix of order ‘n’, find the sum of elements of both diagonals and all boundaries elements. Boundary elements refer to the elements present on all the four boundaries of matrix.
+Input:
+First line will have a single integer ‘n’ denoting the order of matrix.
+Next ‘n’ lines will have ‘n’ space separated integers each denoting the elements of matrix.
+Constraints:
+1<=n<=100
+Output:
+Print a single integer denoting the sum.
+Sample input:
+3
+1 2 3
+4 5 6
+7 8 9
+Sample Output:
+45 */
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
