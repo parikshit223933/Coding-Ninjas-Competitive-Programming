aa33010f389eeca07fd9c04f9e055b2f6e5a65e8
parikshit singh <pk223933@gmail.com>
2020-04-12 23:20:24 +0530

    Added Question

--- pre-requisites/Even_and_odd_indexes.cpp
+++ pre-requisites/Even_and_odd_indexes.cpp
@@ -1,3 +1,22 @@
+/* Even and Odd Indexes
+Given an array of integers, print two integer values:
+First, the sum of all numbers which are even as well as whose index are even.
+Second, the sum of all numbers which are odd as well as whose index are odd.
+Print the two integers space separated. (Arrays is 0-indexed)
+Input:
+Given an integer denoting the size of array.
+Next line will have a line containing ‘n’ space separated integers.
+Constraints:
+1<=n<=10^5
+1 <= Ai <= 10^6 
+Output:
+Two space separated integers denoting even and odd sums respectively.
+Sample Input:
+5
+2 3 5 1 4
+Sample Output:
+6 4 */
+
 #include<iostream>
 #include<vector>
 using namespace std;
