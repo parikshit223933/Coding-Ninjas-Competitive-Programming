77d7666345ed8d4a54c30df61596d506b56333b5
parikshit singh <pk223933@gmail.com>
2020-04-13 20:25:34 +0530

    done

--- Hackwithinfy/Test_me.cpp
+++ Hackwithinfy/Test_me.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 #define endl '\n'
+#define int long long int
 #define fast ios_base::sync_with_stdio(false);cin.tie(0);
 using namespace std;
 int count_ways(int n)
 {
-    if(n==0)
+    if(n==1||n==2)
     {
         return 0;
     }
-    int a=1+count_ways(n-1)
+    if(n%2==0)
+    {
+        return (n/2)-1;
+    }
+    else
+    {
+        return n/2;
+    }
 }
-int main()
+int32_t main()
 {
     fast;
     int t;
