0e22a4d1841f2942d2b2b408bb9673a071d7380a
parikshit singh <pk223933@gmail.com>
2020-04-14 10:35:17 +0530

    Added Question

--- Language Tools + Time and Complexity Assignment/Pair_Sum_To_Zero.cpp
+++ Language Tools + Time and Complexity Assignment/Pair_Sum_To_Zero.cpp
@@ -1,3 +1,23 @@
+/* Given a random integer array A of size N. Find and print the pair of elements in the array which sum to 0.
+Array A can contain duplicate elements.
+While printing a pair, print the smaller element first.
+That is, if a valid pair is (6, -6) print "-6 6". There is no constraint that out of 5 pairs which have to be printed in 1st line. You can print pairs in any order, just be careful about the order of elements in a pair.
+Input format :
+Line 1 : Integer N (Array size)
+Line 2 : Array elements (separated by space)
+Output format :
+Line 1 : Pair 1 elements (separated by space)
+Line 2 : Pair 2 elements (separated by space)
+Line 3 : and so on
+Constraints :
+0 <= N <= 10^4
+Sample Input:
+5
+2 1 -2 2 3
+Sample Output :
+-2 2
+-2 2 */
+
 #include<iostream>
 #include<algorithm>
 #include<map>
