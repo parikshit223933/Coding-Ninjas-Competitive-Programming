d5497ed24ffb8fe81c3f376e2e47d6f0a1e4a0d2
parikshit singh <pk223933@gmail.com>
2020-04-14 09:51:16 +0530

    Added Question

--- Lecture- Basics of Recursion/Power.cpp
+++ Lecture- Basics of Recursion/Power.cpp
@@ -1,3 +1,21 @@
+/* Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+Do this recursively.
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints :
+1 <= x <= 30
+0 <= n <= 30
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32 */
+
 int power(int x, int n) 
 {
     if(n==0)
