1b69f095518f96d585354d48014a5c523c2da5cd
parikshit singh <pk223933@gmail.com>
2020-04-14 10:34:22 +0530

    Added Question

--- Language Tools + Time and Complexity Assignment/Find_UniqueElement.cpp
+++ Language Tools + Time and Complexity Assignment/Find_UniqueElement.cpp
@@ -1,4 +1,18 @@
-
+/* Given an integer array of size 2N + 1. In this given array, N numbers are present twice and one number is present only once in the array.
+You need to find and return that number which is unique in the array.
+Note : Given array will always contain odd number of elements.
+Input format :
+Line 1 : Array size i.e. 2N+1
+Line 2 : Array elements (separated by space)
+Output Format :
+Unique element present in the array
+Constraints :
+1 <= N <= 10^6
+Sample Input :
+7
+2 3 1 6 3 6 2
+Sample Output :
+1 */
 #include<map>
 using namespace std;
 int FindUnique(int arr[], int n){
