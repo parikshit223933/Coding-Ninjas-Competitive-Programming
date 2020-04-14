cc0777236e34561b6f4d3a6a1b84580664f051ac
parikshit singh <pk223933@gmail.com>
2020-04-14 10:33:56 +0530

    Added Question

--- Language Tools + Time and Complexity Assignment/Rotate_Array.cpp
+++ Language Tools + Time and Complexity Assignment/Rotate_Array.cpp
@@ -1,3 +1,20 @@
+/* Given a random integer array of size n, write a function that rotates the given array by d elements (towards left)
+Change in the input array itself. You don't need to return or print elements.
+Input format :
+Line 1 : Integer n (Array Size)
+Line 2 : Array elements (separated by space)
+Line 3 : Integer d
+Output Format :
+Updated array elements (separated by space)
+Constraints :
+1 <= N <= 1000
+1 <= d <= N
+Sample Input :
+7
+1 2 3 4 5 6 7
+2
+Sample Output :
+3 4 5 6 7 1 2 */
 // arr - input array
 // size - size of array
 // d - array to be rotated by d elements
