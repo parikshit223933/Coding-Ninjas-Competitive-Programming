ee8fe7ddca7e9ffbb6e449abcda1c0b8d8ae7cdb
parikshit singh <pk223933@gmail.com>
2020-04-14 10:37:10 +0530

    Added Question

--- Searching & Sorting Applications/Inversion_count.cpp
+++ Searching & Sorting Applications/Inversion_count.cpp
@@ -1,3 +1,25 @@
+/* Let A[0 ... n-1] be an array of n distinct positive integers. If i < j and A[i] > A[j] then the pair (i, j) is called an inversion of A (where i and j are indexes of A). Given an integer array A, your task is to find the number of inversions in A.
+Input format :
+Line 1 : n, array size
+Line 2 : Array elements (separated by space).
+Output format :
+Count of inversions
+Constraints :
+1 <= n <= 10^5
+1 <= A[i] <= 10^9
+Sample Input 1 :
+3
+3 2 1
+Sample Output 1 :
+3
+Sample Input 2 :
+5
+2 5 1 3 4
+Sample Output 1 :
+4 */
+
+
+
 #include<iostream>
 using namespace std;
 long long merge(int *arr, int start, int mid, int end)
