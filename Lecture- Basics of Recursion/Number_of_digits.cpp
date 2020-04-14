5ae7c9e425204437c2892d8f46fb4dbcc059b187
parikshit singh <pk223933@gmail.com>
2020-04-14 09:53:06 +0530

    Added Question

--- Lecture- Basics of Recursion/Number_of_digits.cpp
+++ Lecture- Basics of Recursion/Number_of_digits.cpp
@@ -1,3 +1,14 @@
+/* Given the code to find out and return the number of digits present in a number recursively. But it contains few bugs, that you need to rectify such that all the test cases should pass.
+Input Format :
+Integer n
+Output Format :
+Count of digits
+Constraints :
+1 <= n <= 10^6
+Sample Input :
+ 156
+Sample Output :
+3 */
 int count(int n){
     if(n>=0&&n<10){
         return 1;
