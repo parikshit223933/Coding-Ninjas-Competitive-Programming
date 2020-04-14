75eb250873a9594a93f739fc1dd7e3ec7581884c
parikshit singh <pk223933@gmail.com>
2020-04-14 10:00:10 +0530

    Added Question

--- Language Tools/Extract_unique_characters.cpp
+++ Language Tools/Extract_unique_characters.cpp
@@ -1,3 +1,18 @@
+/* Given a string, you need to remove all the duplicates. That means, the output string should contain each character only once. The respective order of characters should remain same.
+Input format :
+String S
+Output format :
+Output String
+Constraints :
+0 <= Length of S <= 10^8
+Sample Input 1 :
+ababacd
+Sample Output 1 :
+abcd
+Sample Input 2 :
+abcde
+Sample Output 2 :
+abcde */
 #include<bits/stdc++.h>
 char* uniqueChar(char *str)
 {
