c2342d95a5ddf723100355a32c25badb90e98c2b
parikshit singh <pk223933@gmail.com>
2020-04-14 10:36:29 +0530

    Added Question

--- Searching & Sorting Applications/Aggressive_cows_Problems.cpp
+++ Searching & Sorting Applications/Aggressive_cows_Problems.cpp
@@ -1,3 +1,26 @@
+/* Farmer John has built a new long barn, with N (2 <= N <= 100,000) stalls. The stalls are located along a straight line at positions x1,...,xN (0 <= xi <= 1,000,000,000).
+His C (2 <= C <= N) cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, FJ wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+Input
+t – the number of test cases, then t test cases follows. 
+* Line 1: Two space-separated integers: N and C
+* Lines 2..N+1: Line i+1 contains an integer stall location, xi
+Output
+For each test case output one integer: the largest minimum distance.
+Sample Input :
+1
+5 3
+1
+2
+8
+4
+9
+Sample Output:
+3 
+Output details:
+FJ can put his 3 cows in the stalls at positions 1, 4 and 8, 
+resulting in a minimum distance of 3. */
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
