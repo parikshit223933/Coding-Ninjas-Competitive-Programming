7451a5139582401aaa25ee180fce44249e940d00
parikshit singh <pk223933@gmail.com>
2020-04-14 09:55:06 +0530

    Added Question

--- Lecture- Basics of Recursion/Last_Index_of_number.cpp
+++ Lecture- Basics of Recursion/Last_Index_of_number.cpp
@@ -1,3 +1,22 @@
+/* Given an array of length N and an integer x, you need to find and return the last index of integer x present in the array. Return -1 if it is not present in the array.
+Last index means - if x is present multiple times in the array, return the index at which x comes last in the array.
+You should start traversing your array from 0, not from (N - 1).
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+last index or -1
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+4
+9 8 10 8
+8
+Sample Output :
+3 */
+
 int lastIndex(int input[], int size, int x) {
   /* Don't write main().
      Don't read input, it is passed as function argument.
