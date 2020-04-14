5da449f56fbeb92ff9302406b6a8e68431ddeb93
parikshit singh <pk223933@gmail.com>
2020-04-14 09:58:13 +0530

    Added Question

--- Language Tools/Love_for_Characters.cpp
+++ Language Tools/Love_for_Characters.cpp
@@ -1,3 +1,17 @@
+/* Ayush loves the characters ‘a’, ‘s’, and ‘p’. He got a string of lowercase letters and he wants to find out how many times characters ‘a’, ‘s’, and ‘p’ occurs in the string respectively. Help him find it out.
+Input:
+First line contains an integer denoting length of the string.
+Next line contains the string.
+Constraints:
+1<=n<=10^5
+‘a’<= each character of string <= ‘z’
+Output:
+Three space separated integers denoting the occurrence of letters ‘a’, ‘s’ and ‘p’ respectively.
+Sample Input:
+6
+aabsas
+Sample output:
+3 2 0 */
 #include<bits/stdc++.h>
 using namespace std;
 int main()
