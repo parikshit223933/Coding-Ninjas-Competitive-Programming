51adb165f98e9f9e6d5618841ba5b7b982c6c743
parikshit singh <pk223933@gmail.com>
2020-04-14 09:54:07 +0530

    Added Question

--- Lecture- Basics of Recursion/Check_number.cpp
+++ Lecture- Basics of Recursion/Check_number.cpp
@@ -1,3 +1,19 @@
+/* Given an array of length N and an integer x, you need to find if x is present in the array or not. Return true or false.
+Do this recursively.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+true or false
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+3
+9 8 10
+8
+Sample Output :
+true */
 bool checkNumber(int input[], int size, int x)
 {
     if(size==0)
