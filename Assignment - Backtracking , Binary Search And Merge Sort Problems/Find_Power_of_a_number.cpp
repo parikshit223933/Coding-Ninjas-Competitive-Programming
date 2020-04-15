2036427b2a606952ff07edcccf3e837adeb743c4
parikshit singh <pk223933@gmail.com>
2020-04-15 15:23:30 +0530

    Added Question

--- Assignment - Backtracking , Binary Search And Merge Sort Problems/Find_Power_of_a_number.cpp
+++ Assignment - Backtracking , Binary Search And Merge Sort Problems/Find_Power_of_a_number.cpp
@@ -1,3 +1,25 @@
+/* Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to print the answer.
+Note : For this question, you can assume that 0 raised to the power of 0 is 1
+
+
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints:
+0 <= x <= 8 
+0 <= n <= 9
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32 */
+
+
+
 #include<iostream>
 using namespace std;
 int power(int x, int n)
