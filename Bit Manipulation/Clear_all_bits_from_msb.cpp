d53486012067219c7422a40f68af624a595227c0
parikshit singh <pk223933@gmail.com>
2020-04-15 15:26:34 +0530

    Added Question

--- Bit Manipulation/Clear_all_bits_from_msb.cpp
+++ Bit Manipulation/Clear_all_bits_from_msb.cpp
@@ -1,3 +1,22 @@
+/* You are given two integers N and i. You need to clear all bits from MSB to ith bit (start i from right to left) and return the updated N.
+Counting of bits starts from 0 from right to left.
+Input Format :
+Two integers N and i (separated by space)
+Output Format :
+Updated N
+Sample Input 1 :
+15 2
+Sample Output 1 :
+3
+Sample Output 1 Explanation :
+We need to clear all bits from MSB to ith bit i.e. clear all bits except 0th and 1st.
+Sample Input 2 :
+4 4
+Sample Output 2 :
+4 */
+
+
+
 int clearAllBits(int n, int i){
     /* Don't write main().
      * Don't read input, it is passed as function argument.
