e15614661b37f9bfee1fcd2fbb7aeb028b37d989
parikshit singh <pk223933@gmail.com>
2020-04-15 15:27:05 +0530

    Added Question

--- Bit Manipulation/Turn_off_first_set_bit.cpp
+++ Bit Manipulation/Turn_off_first_set_bit.cpp
@@ -1,3 +1,20 @@
+/* You are given an integer Ni. You need to make first set bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Integer N 
+Output Format :
+Updated N
+Sample Input 1 :
+4
+Sample Output 1 :
+0
+Sample Input 2 :
+12 
+Sample Output 2 :
+8 */
+
+
+
 int turnOffFirstSetBit(int n)
 {
     if(n==0)
--- Bit Manipulation/Unset_ith_bit.cpp
+++ Bit Manipulation/Unset_ith_bit.cpp
@@ -1,3 +1,19 @@
+/* You are given two integers N and i. You need to make ith bit of binary representation of N to 0 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Two integers N and i (separated by space)
+Output Format :
+Updated N
+Sample Input 1 :
+7 2
+Sample Output 1 :
+3
+Sample Input 2 :
+12 1
+Sample Output 2 :
+12 */
+
+
 int turnOffIthBit(int n, int i){
     /* Don't write main().
      * Don't read input, it is passed as function argument.
