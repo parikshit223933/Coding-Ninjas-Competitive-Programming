c5bf096e4ed421dc820db6f3e625109c1ef8bd98
parikshit singh <pk223933@gmail.com>
2020-04-15 15:29:50 +0530

    Added Question

--- Modulo Arithmatic/Number_of_Balanced_BTS.cpp
+++ Modulo Arithmatic/Number_of_Balanced_BTS.cpp
@@ -1,3 +1,23 @@
+/* Given an integer h, find the possible number of balanced binary trees of height h. You just need to return the count of possible binary trees which are balanced.
+This number can be huge, so return output modulus 10^9 + 7.
+Write a simple recursive solution.
+Input Format :
+Integer h
+Output Format :
+Count % 10^9 + 7
+Input Constraints :
+1 <= h <= 40
+Sample Input 1:
+3
+Sample Output 1:
+15
+Sample Input 2:
+4
+Sample Output 2:
+315 */
+
+
+
 #include<iostream>
 using namespace std;
 typedef long long ll;
