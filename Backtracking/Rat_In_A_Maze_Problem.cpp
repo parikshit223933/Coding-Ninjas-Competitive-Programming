554bec4a3ee4cc4ae3b0aab6d6082a87da2027e0
parikshit singh <pk223933@gmail.com>
2020-04-15 15:21:39 +0530

    Added Question

--- Backtracking/Rat_In_A_Maze_Problem.cpp
+++ Backtracking/Rat_In_A_Maze_Problem.cpp
@@ -1,3 +1,35 @@
+/* You are given a N*N maze with a rat placed at maze[0][0]. Find and print all paths that rat can follow to reach its destination i.e. maze[N-1][N-1]. Rat can move in any direc­tion ( left, right, up and down).
+Value of every cell in the maze can either be 0 or 1. Cells with value 0 are blocked means rat can­not enter into those cells and those with value 1 are open.
+Input Format
+Line 1 : Integer N
+Next N Lines : Each line will contain ith row elements (separated by space)
+Output Format :
+One Line for every possible solution. 
+Every line will have N*N maze elements printed row wise and are separated by space. Only cells that are part of solution path should be 1, rest all cells should be 0.
+Sample Input 1 :
+3
+1 0 1
+1 0 1
+1 1 1
+Sample Output 1 :
+1 0 0 1 0 0 1 1 1 
+Sample Output 1 Explanation :
+Only 1 path is possible
+Sample Input 2 :
+3
+1 0 1
+1 1 1
+1 1 1
+Sample Output 2 :
+1 0 0 1 1 1 1 1 1 
+1 0 0 1 0 0 1 1 1 
+1 0 0 1 1 0 0 1 1 
+1 0 0 1 1 1 0 0 1 
+Sample Output 2 Explanation :
+4 paths are possible */
+
+
+
 #include<iostream>
 using namespace std;
 void printer(int **arr, int n)
