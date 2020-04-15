774baf9eb86c7271718dd61ae67c95d6f7cbdddd
parikshit singh <pk223933@gmail.com>
2020-04-15 15:24:03 +0530

    Added Question

--- Assignment - Backtracking , Binary Search And Merge Sort Problems/Sudoku_Solver.cpp
+++ Assignment - Backtracking , Binary Search And Merge Sort Problems/Sudoku_Solver.cpp
@@ -1,3 +1,23 @@
+/* Given a 9*9 sudoku board, in which some entries are filled and others are 0 (0 indicates that the cell is empty), you need to find out whether the Sudoku puzzle can be solved or not i.e. return true or false.
+Input Format :
+9 Lines where ith line contains ith row elements separated by space
+Output Format :
+ true or false
+Sample Input :
+9 0 0 0 2 0 7 5 0 
+6 0 0 0 5 0 0 4 0 
+0 2 0 4 0 0 0 1 0 
+2 0 8 0 0 0 0 0 0 
+0 7 0 5 0 9 0 6 0 
+0 0 0 0 0 0 4 0 1 
+0 1 0 0 0 5 0 8 0 
+0 9 0 0 7 0 0 0 4 
+0 8 2 0 4 0 0 0 6
+Sample Output :
+true */
+
+
+
 #include<iostream>
 using namespace std;
 int n=9;
