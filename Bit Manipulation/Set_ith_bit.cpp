ff98b7f2fc199c002228c9435cabb23509b32e2d
parikshit singh <pk223933@gmail.com>
2020-04-15 15:26:55 +0530

    Added Question

--- Bit Manipulation/Set_ith_bit.cpp
+++ Bit Manipulation/Set_ith_bit.cpp
@@ -1,3 +1,19 @@
+/* You are given two integers N and i. You need to make ith bit of binary representation of N to 1 and return the updated N.
+Counting of bits start from 0 from right to left.
+Input Format :
+Two integers N and i (separated by space)
+Output Format :
+Updated N
+Sample Input 1 :
+4 1
+Sample Output 1 :
+6
+Sample Input 2 :
+4 4
+Sample Output 2 :
+20 */
+
+
 int turnOnIthBit(int n, int i){
     /* Don't write main().
      * Don't read input, it is passed as function argument.
