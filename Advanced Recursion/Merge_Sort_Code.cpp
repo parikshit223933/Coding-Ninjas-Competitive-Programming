0760db703d2e7ad3ab4b6388c861292c0a0d379c
parikshit singh <pk223933@gmail.com>
2020-04-15 15:18:29 +0530

    Added Question

--- Advanced Recursion/Merge_Sort_Code.cpp
+++ Advanced Recursion/Merge_Sort_Code.cpp
@@ -1,3 +1,19 @@
+/* Sort an array A using Merge Sort.
+Change in the input array itself. So no need to return or print anything.
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 1000
+Sample Input:
+6 
+2 6 8 5 4 3
+Sample Output:
+2 3 4 5 6 8 */
+
+
 #include<iostream>
 using namespace std;
 void merge(int *part1, int size1, int *part2, int size2, int*output, int n)
