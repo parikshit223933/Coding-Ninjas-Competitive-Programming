1de4e85fcd20c3b63586066c3daa1c279022b13f
parikshit singh <pk223933@gmail.com>
2020-04-15 15:18:43 +0530

    Added Question

--- Advanced Recursion/Print_Keypad_Combinations_Code.cpp
+++ Advanced Recursion/Print_Keypad_Combinations_Code.cpp
@@ -1,3 +1,26 @@
+/* Given an integer n, using phone keypad find out and print all the possible strings that can be made using digits of input n.
+Note : The order of strings are not important. Just print different strings in new lines.
+Input Format :
+Integer n
+Output Format :
+All possible strings in different lines
+Constraints :
+1 <= n <= 10^6
+Sample Input:
+23
+Sample Output:
+ad
+ae
+af
+bd
+be
+bf
+cd
+ce
+cf */
+
+
+
 #include <iostream>
 #include <string>
 using namespace std;
