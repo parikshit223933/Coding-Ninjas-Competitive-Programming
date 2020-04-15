475b07abcd042a7fb8d6efb19e598e2af4ca72a8
parikshit singh <pk223933@gmail.com>
2020-04-15 15:21:14 +0530

    Added Question

--- Backtracking/Crossword_Problem.cpp
+++ Backtracking/Crossword_Problem.cpp
@@ -1,3 +1,60 @@
+/* CodingNinjas has provided a crossword of 10*10 grid. The grid contains '+' or '-' as its cell values. Now, you are also provided with a word list that needs to placed accurately in the grid. Cells marked with '-' are to be filled with word list.
+For example, The following is an example for the input crossword grid and the word list.
++-++++++++
++-++-+++++
++-------++
++-++-+++++
++-++-+++++
++-++-+++++
+++++-+++++
+++++-+++++
+++++++++++
+----------
+CALIFORNIA;NIGERIA;CANADA;TELAVIV
+Output for the given input should be:
++C++++++++
++A++T+++++
++NIGERIA++
++A++L+++++
++D++A+++++
++A++V+++++
+++++I+++++
+++++V+++++
+++++++++++
+CALIFORNIA
+Note: We have provided such test cases that there is only one solution for the given input.
+Input format:
+The first 10 lines of input contain crossword. Each of 10 lines has a character array of size 10. Input characters are either '+' or '-'.
+The next line of input contains the word list, in which each word is separated by ';'. 
+Output format:
+Print the crossword grid, after placing the words of word list in '-' cells.  
+Sample Test Cases:
+Sample Input 1:
++-++++++++
++-++-+++++
++-------++
++-++-+++++
++-++-+++++
++-++-+++++
+++++-+++++
+++++-+++++
+++++++++++
+----------
+CALIFORNIA;NIGERIA;CANADA;TELAVIV
+Sample Output 1:
++C++++++++
++A++T+++++
++NIGERIA++
++A++L+++++
++D++A+++++
++A++V+++++
+++++I+++++
+++++V+++++
+++++++++++
+CALIFORNIA */
+
+
+
 #include<iostream>
 #include<string>
 #include<vector>
