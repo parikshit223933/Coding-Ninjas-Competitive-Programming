2a56703f825dcd7a3b833d2d737f6a8d11dbbd6d
parikshit singh <pk223933@gmail.com>
2020-04-15 15:18:57 +0530

    Added Question

--- Advanced Recursion/Quick_Sort_Code.cpp
+++ Advanced Recursion/Quick_Sort_Code.cpp
@@ -1,3 +1,20 @@
+/* Sort an array A using Quick Sort.
+Change in the input array itself. So no need to return or print anything.
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 1000
+Sample Input:
+6 
+2 6 8 5 4 3
+Sample Output:
+2 3 4 5 6 8 */
+
+
+
 #include<iostream>
 using namespace std;
 int partition(int *arr, int start, int end)
