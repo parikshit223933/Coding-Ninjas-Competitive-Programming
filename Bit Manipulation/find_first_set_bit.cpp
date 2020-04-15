55a00c548b26703977627176d94b6df3d606c270
parikshit singh <pk223933@gmail.com>
2020-04-15 15:26:44 +0530

    Added Question

--- Bit Manipulation/find_first_set_bit.cpp
+++ Bit Manipulation/find_first_set_bit.cpp
@@ -1,3 +1,20 @@
+/* You are given an integer N. You need to return an integer M, in which only one bit is set which at position of lowest set bit of N (from right to left).
+Input Format :
+Integer N
+Output Format :
+Integer M
+Sample Input 1 :
+7
+Sample Output 1 :
+1
+Sample Input 2 :
+12
+Sample Output 2 :
+4 */
+
+
+
+
 int returnFirstSetBit(int n)
 {
     if(n==0)
