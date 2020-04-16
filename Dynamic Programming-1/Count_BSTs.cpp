a7b30c0005a43abf10696d334495f7f8e077197a
parikshit singh <pk223933@gmail.com>
2020-04-16 13:12:26 +0530

    Added Question

--- Dynamic Programming-1/Count_BSTs.cpp
+++ Dynamic Programming-1/Count_BSTs.cpp
@@ -1,3 +1,24 @@
+/* Given an integer N, find and return the count of unique Binary search trees (BSTs) are possible with nodes valued from 1 to N.
+Output count can be very large, so return the count modulo 10^9+7.
+Input Format :
+Integer n 
+Output Format :
+Count of BSTs
+Contraints :
+1<= N <=1000
+Sample Input 1:
+8
+Sample Output 1:
+1430
+Sample Input 2:
+3
+Sample Output 2:
+5 */
+
+
+
+
+
 #include<iostream>
 
 using namespace std;
