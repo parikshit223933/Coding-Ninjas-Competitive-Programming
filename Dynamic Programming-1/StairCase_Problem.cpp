8936627da92661527470d106ab3bc905feccf50f
parikshit singh <pk223933@gmail.com>
2020-04-16 13:14:17 +0530

    Added Question

--- Dynamic Programming-1/StairCase_Problem.cpp
+++ Dynamic Programming-1/StairCase_Problem.cpp
@@ -1,3 +1,18 @@
+/* A child is running up a staircase with n steps and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method to count how many possible ways the child can run up to the stairs. You need to return all possible number of ways.
+Time complexity of your code should be O(n).
+Input format :
+Integer n (No. of steps)
+Constraints :
+n <= 70
+Sample Input 1:
+4
+Sample Output 1:
+7 */
+
+
+
+
+
 long staircase(int n, long *arr=new long[71]()){
     /*  Don't write main().
      *  Don't read input, it is passed as function argument.
