27aa4c69d93e27c5ca0c44b484c2ef664b63ca8a
parikshit singh <pk223933@gmail.com>
2020-04-16 13:12:58 +0530

    Added Question

--- Dynamic Programming-1/Maximum_Square_Matrix_with_All_Zeros.cpp
+++ Dynamic Programming-1/Maximum_Square_Matrix_with_All_Zeros.cpp
@@ -1,3 +1,20 @@
+/* Given a n*m matrix which contains only 0s and 1s, find out the size of maximum square sub-matrix with all 0s. You need to return the size of square with all 0s.
+Input format :
+Line 1 : n and m (space separated positive integers)
+Next n lines : m elements of each row (separated by space).
+Output Format:
+Line 1 : Size of maximum square sub-matrix
+Sample Input :
+3 3
+1 1 0
+1 1 1
+1 1 1
+Sample Output :
+1 */
+
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
