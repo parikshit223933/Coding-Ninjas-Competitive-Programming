f971b95ae1e722dc7eb4d1e999aa72436e552b67
parikshit singh <pk223933@gmail.com>
2020-04-16 13:13:56 +0530

    Added Question

--- Dynamic Programming-1/Roy_and_Coin_Boxes.cpp
+++ Dynamic Programming-1/Roy_and_Coin_Boxes.cpp
@@ -1,3 +1,49 @@
+/* Roy has N coin boxes numbered from 1 to N.
+Every day he selects two indices [L,R] and adds 1 coin to each coin box starting from L to R (both inclusive).
+He does this for M number of days.
+
+After M days, Roy has a query: How many coin boxes have atleast X coins.
+He has Q such queries.
+Input
+First line contains N - number of coin boxes.
+Second line contains M - number of days. Each of the next M lines consists of two space separated integers L and R. Followed by integer Q - number of queries.
+Each of next Q lines contain a single integer X.a
+Output
+For each query output the result in a new line.
+Constraints
+1 ≤ N ≤ 1000000
+
+1 ≤ M ≤ 1000000
+
+1 ≤ L ≤ R ≤ N
+
+1 ≤ Q ≤ 1000000
+
+1 ≤ X ≤ N
+Sample Input
+7
+4
+1 3
+2 5
+1 2
+5 6
+4
+1
+7
+4
+2
+Sample Output
+6
+0
+0
+4 */
+
+
+
+
+
+
+
 #include<iostream>
 using namespace std;
 int main()
