c546b9c8fcafcf2e194d877993e66864bfe64fce
parikshit singh <pk223933@gmail.com>
2020-04-16 13:12:16 +0530

    Added Question

--- Dynamic Programming-1/Largest_Bitonic_Subarray.cpp
+++ Dynamic Programming-1/Largest_Bitonic_Subarray.cpp
@@ -1,3 +1,33 @@
+/* You are given an array of positive integers as input. Write a code to return the length of the largest such subsequence in which the values are arranged first in strictly ascending order and then in strictly descending order.
+Such a subsequence is known as bitonic subsequence. A purely increasing or purely decreasing subsequence will also be considered as a bitonic sequence with the other part empty.
+Note that the elements in bitonic subsequence need not be consecutive in the given array but the order should remain same.
+Input Format:
+Line 1 : A positive Integer N, i.e., the size of array
+Line 2 : N space-separated integers as elements of the array 
+Output Format:
+Length of Largest Bitonic subsequence
+Input Constraints:
+1<= N <= 100000
+Sample Input 1:
+6
+15 20 20 6 4 2
+Sample Output 1:
+5
+Sample Output 1 Explanation:
+Here, longest Bitonic subsequence is {15, 20, 6, 4, 2} which has length = 5.
+Sample Input 2:
+2
+1 5
+Sample Output 2:
+2
+Sample Input 3:
+2
+5 1
+Sample Output 3:
+2 */
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
