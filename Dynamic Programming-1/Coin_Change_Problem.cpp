7f63945712e11e2eb0a7e7da2b927295bc217537
parikshit singh <pk223933@gmail.com>
2020-04-16 13:12:36 +0530

    Added Question

--- Dynamic Programming-1/Coin_Change_Problem.cpp
+++ Dynamic Programming-1/Coin_Change_Problem.cpp
@@ -1,3 +1,25 @@
+/* You are given an infinite supply of coins of each of denominations D = {D0, D1, D2, D3, ...... Dn-1}. You need to figure out the total number of ways W, in which you can make change for Value V using coins of denominations D.
+Note : Return 0, if change isn't possible.
+Input Format
+Line 1 : Integer n i.e. total number of denominations
+Line 2 : N integers i.e. n denomination values
+Line 3 : Value V
+Output Format
+Line 1 :  Number of ways i.e. W
+Constraints :
+1<=n<=10
+1<=V<=1000
+Sample Input 1 :
+3
+1 2 3
+4
+Sample Output
+4
+Sample Output Explanation :
+Number of ways are - 4 total i.e. (1,1,1,1), (1,1, 2), (1, 3) and (2, 2). */
+
+
+
 #include <iostream>
 using namespace std;
 int worker(int arr[], int n, int value, int **temp)
