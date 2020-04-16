7ee090d4709dfaf663562fed55dbf166ccba88b0
parikshit singh <pk223933@gmail.com>
2020-04-16 13:13:19 +0530

    Added Question

--- Dynamic Programming-1/Loot_Houses.cpp
+++ Dynamic Programming-1/Loot_Houses.cpp
@@ -1,3 +1,22 @@
+/* A thief wants to loot houses. He knows the amount of money in each house. He cannot loot two consecutive houses. Find the maximum amount of money he can loot.
+Input Format
+Line 1 : An integer N 
+Line 2 : N spaced integers denoting money in each house
+Output Format
+ Line 1 : Maximum amount of money looted
+Input Constraints
+1 <= n <= 10^4
+1 <= A[i] < 10^4
+Sample Input :
+6
+5 5 10 100 10 5
+Sample Output 1 :
+110 */
+
+
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
