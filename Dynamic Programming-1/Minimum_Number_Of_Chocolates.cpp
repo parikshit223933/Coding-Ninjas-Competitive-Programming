d1f299f51923def954564ff2e3273ed8f2380a66
parikshit singh <pk223933@gmail.com>
2020-04-16 13:13:44 +0530

    Added Question

--- Dynamic Programming-1/Minimum_Number_Of_Chocolates.cpp
+++ Dynamic Programming-1/Minimum_Number_Of_Chocolates.cpp
@@ -1,3 +1,25 @@
+/* Noor is a teacher. She wants to give some chocolates to the students in her class. All the students sit in a line and each of them has a score according to performance. Noor wants to give at least 1 chocolate to each student. She distributes chocolates to them such that If two students sit next to each other then the one with the higher score must get more chocolates. Noor wants to save money, so she wants to minimise the total number of chocolates.
+Note that when two students have equal score they are allowed to have different number of chocolates.
+Input Format:
+First Line: Integer N, the number of students in Noor’s class. 
+Second Line: Each of the student's score separated by spaces.
+Output Format:
+Output a single line containing the minimum number of chocolates Noor must give.
+Input Constraints
+ 1 <= N <= 100000
+ 1 <= score <= 100000
+Sample Input:
+4
+1 4 4 6
+sample Output:
+6
+Sample Input:
+3
+8 7 5
+sample Output:
+6 */
+
+
 #include <iostream>
 using namespace std;
 int getMin(int *arr, int n)
