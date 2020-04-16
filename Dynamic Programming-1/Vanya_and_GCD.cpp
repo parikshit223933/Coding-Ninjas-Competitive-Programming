ff96de509f5ac417e722c4b5febb1fb98a48458f
parikshit singh <pk223933@gmail.com>
2020-04-16 13:14:26 +0530

    Added Question

--- Dynamic Programming-1/Vanya_and_GCD.cpp
+++ Dynamic Programming-1/Vanya_and_GCD.cpp
@@ -1,3 +1,23 @@
+/* Vanya has been studying all day long about sequences and other Complex Mathematical Terms. She thinks she has now become really good at it. So, her friend Vasya decides to test her knowledge and keeps the following challenge it front of her:
+Vanya has been given an integer array A of size N. Now, she needs to find the number of increasing sub-sequences of this array with length ≥1 and GCD=1. A sub-sequence of an array is obtained by deleting some (or none) elements and maintaining the relative order of the rest of the elements. As the answer may be large, print it Modulo 109+7
+She finds this task really easy, and thinks that you can do it too. Can you?
+Input Format:
+The first line contains a single integer N denoting size of array A. The next line contains N space separated integers denoting the elements of array A
+Output Format:
+Print the required answer Modulo 10^9+7 on a single line.
+Constraints:
+1≤N≤500
+
+1≤A[i]≤100
+Sample Input
+3
+1 2 3
+Sample Output
+5 */
+
+
+
+
 #include<bits/stdc++.h>
 #define m 1000000007
 using namespace std;
