e9e823bee4420cc4403b818dc76dd373eb518fe4
parikshit singh <pk223933@gmail.com>
2020-04-16 13:11:28 +0530

    Added Question

--- Dynamic Programming-1/Alpha_Code_Question.cpp
+++ Dynamic Programming-1/Alpha_Code_Question.cpp
@@ -1,3 +1,30 @@
+/* Alice and Bob need to send secret messages to each other and are discussing ways to encode their messages:
+Alice: “Let’s just use a very simple code: We’ll assign ‘A’ the code word 1, ‘B’ will be 2, and so on down to ‘Z’ being assigned 26.”
+
+Bob: “That’s a stupid code, Alice. Suppose I send you the word ‘BEAN’ encoded as 25114. You could decode that in many different ways!”
+
+Alice: “Sure you could, but what words would you get? Other than ‘BEAN’, you’d get ‘BEAAD’, ‘YAAD’, ‘YAN’, ‘YKD’ and ‘BEKD’. I think you would be able to figure out the correct decoding. And why would you send me the word ‘BEAN’ anyway?”
+
+Bob: “OK, maybe that’s a bad example, but I bet you that if you got a string of length 5000 there would be tons of different decodings and with that many you would find at least two different ones that would make sense.”
+
+Alice: “How many different decodings?”
+
+Bob: “Jillions!”
+For some reason, Alice is still unconvinced by Bob’s argument, so she requires a program that will determine how many decodings there can be for a given string using her code.
+Input
+Input will consist of multiple input sets. Each set will consist of a single line of at most 5000 digits representing a valid encryption (for example, no line will begin with a 0). There will be no spaces between the digits. An input line of ‘0’ will terminate the input and should not be processed.
+Output
+For each input set, output the number of possible decodings for the input string. Print your answer taking modulo "10^9+7"
+Sample Input:
+25114
+1111111111
+3333333333
+0
+Sample Output:
+6
+89
+1 */
+
 #include<iostream>
 #include<string>
 using namespace std;
--- Dynamic Programming-1/Angry_children.cpp
+++ Dynamic Programming-1/Angry_children.cpp
@@ -1,3 +1,41 @@
+/* Bill Gates is on one of his philanthropic journeys to a village in Utopia. He has N packets of candies and would like to distribute one packet to each of the K children in the village (each packet may contain different number of candies). To avoid a fight between the children, he would like to pick K out of N packets such that the unfairness is minimized.
+Suppose the K packets have (x1, x2, x3,....xk) candies in them, where xi denotes the number of candies in the ith packet, then we define unfairness as
+unfairness=0;
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
+    unfairness+=abs(xi-xj)
+abs(x) denotes absolute value of x.
+Input Format
+The first line contains an integer N.
+The second line contains an integer K.
+N lines follow each integer containing the candy in the ith packet.
+Output Format
+A single integer which will be minimum unfairness.
+Constraints
+2<=N<=10^5
+
+2<=K<=N
+
+0<= number of candies in each packet <=10^9
+Sample Input
+7
+3
+10
+100
+300
+200
+1000
+20
+30
+Sample Output
+40
+Explanation
+Bill Gates will choose packets having 10, 20 and 30 candies.So unfairness will be |10-20| + |20-30| + |10-30| = 40. We can verify that it will be minimum in this way. */
+
+
+
+
+
 #include <iostream>
 #include<algorithm>
 using namespace std;
