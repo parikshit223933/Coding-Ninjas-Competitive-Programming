a8ce959964c75d8e94dc15374ee927c05c64b544
parikshit singh <pk223933@gmail.com>
2020-04-16 13:11:54 +0530

    Added Question

--- Dynamic Programming-1/Boredom.cpp
+++ Dynamic Programming-1/Boredom.cpp
@@ -1,3 +1,22 @@
+/* Gary is bored and wants to play an interesting but tough game . So he figured out a new board game called "destroy the neighbours" . In this game there are N integers on a board. In one move, he can pick any integer x from the board and then all the integers with value x+1 or x-1 gets destroyed .This move will give him x points.
+He plays the game until the board becomes empty . But as he want show this game to his friend Steven, he wants to learn techniques to maximise the points to show off . Can you help Gary in finding out the maximum points he receive grab from the game ?
+Input Format :
+Line 1 : Integer N 
+Line 2 : A list of N integers
+Output Format :
+Maximum points Gary can recieve from the Game setup
+Constraints :
+1<=N<=10^5
+1<=A[i]<=1000
+Sample Input :
+2
+1 2
+Sample Output :
+2 */
+
+
+
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
