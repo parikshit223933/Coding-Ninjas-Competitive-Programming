75e31633bc36fb89aedc3ee06b2777462ab48106
parikshit singh <pk223933@gmail.com>
2020-04-17 18:19:21 +0530

    Added Question

--- Greedy Problems/Weighted_Job_Scheduling.cpp
+++ Greedy Problems/Weighted_Job_Scheduling.cpp
@@ -1,3 +1,28 @@
+/* You are given N jobs where every job is represented as:
+1.Start Time
+2.Finish Time
+3.Profit Associated
+Find the maximum profit subset of jobs such that no two jobs in the subset overlap.
+Input
+The first line of input contains one integer denoting N.
+Next N lines contains three space separated integers denoting the start time, finish time and the profit associated with the ith job. 
+Output
+Output one integer, the maximum profit that can be achieved.
+Constraints
+1 ≤ N ≤ 10^6
+1 ≤ ai, di, p ≤ 10^6
+Sample Input
+4
+3 10 20
+1 2 50
+6 19 100
+2 100 200
+Sample Output
+250 */
+
+
+
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
