bb0fd567479d7393b98332710bff6d26e0c163eb
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:47 +0530

    Added Question

--- Dynamic Programming - 2/Distinct_Subsequences.cpp
+++ Dynamic Programming - 2/Distinct_Subsequences.cpp
@@ -1,3 +1,26 @@
+/* Given a string, count the number of distinct subsequences of it ( including empty subsequence ). For the uninformed, A subsequence of a string is a new string which is formed from the original string by deleting some of the characters without disturbing the relative positions of the remaining characters.
+For example, "AGH" is a subsequence of "ABCDEFGH" while "AHG" is not.
+Input
+First line of input contains an integer T which is equal to the number of test cases. You are required to process all test cases. Each of next T lines contains a string s.
+Output
+Output consists of T lines. Ith line in the output corresponds to the number of distinct subsequences of ith input string. Since, this number could be very large, you need to output ans%1000000007 where ans is the number of distinct subsequences. 
+Constraints and Limits
+T ≤ 100, length(S) ≤ 100000
+
+All input strings shall contain only uppercase letters.
+Sample Input
+3
+AAA
+ABCDEFG
+CODECRAFT
+Sample Output
+4
+128
+496 */
+
+
+
+
 #include<iostream>
 #include<string>
 #define m 1000000007
