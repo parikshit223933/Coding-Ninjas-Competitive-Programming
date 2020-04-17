9ef426b41cc0b1f4aa0a32d420efb8e1ab57d5d5
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:51 +0530

    Added Question

--- Dynamic Programming - 2/Smallest_Super_Sequence.cpp
+++ Dynamic Programming - 2/Smallest_Super_Sequence.cpp
@@ -1,3 +1,21 @@
+/* Given two strings S and T, find and return the length of their smallest super-sequence.
+A shortest super sequence of two strings is defined as the shortest possible string containing both strings as subsequences.
+Note that if the two strings do not have any common characters, then return the sum of lengths of the two strings.
+Input Format:
+Line 1 : A string
+Line 2: Another string
+Output Format:
+Length of the smallest super-sequence of given two strings. 
+Sample Input:
+ab
+ac
+Sample Output:
+3
+Sample Output Explanation:
+Their smallest super-sequence can be "abc" which has length=3. */
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
