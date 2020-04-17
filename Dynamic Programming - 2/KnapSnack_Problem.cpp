92b6190a3c6a569f42ed7056e4a493c50580ac49
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:29 +0530

    Added Question

--- Dynamic Programming - 2/KnapSnack_Problem.cpp
+++ Dynamic Programming - 2/KnapSnack_Problem.cpp
@@ -1,3 +1,26 @@
+/* A thief robbing a store and can carry a maximal weight of W into his knapsack. There are N items and ith item weigh wi and is of value vi. What is the maximum value V, that thief can take ?
+Space complexity should be O(W).
+Input Format :
+Line 1 : N i.e. number of items
+Line 2 : N Integers i.e. weights of items separated by space
+Line 3 : N Integers i.e. values of items separated by space
+Line 4 : Integer W i.e. maximum weight thief can carry
+Output Format :
+Line 1 : Maximum value V
+Constraints
+1 <= N <= 10^4
+1<= wi <= 100
+1 <= vi <= 100
+Sample Input 1 :
+4
+1 2 4 5
+5 4 8 6
+5
+Sample Output :
+13 */
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
