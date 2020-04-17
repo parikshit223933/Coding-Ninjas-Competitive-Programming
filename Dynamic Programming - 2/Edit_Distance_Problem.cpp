6ee3ac4188fbedda2c3cfb742a4b506a11b45779
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:18 +0530

    Added Question

--- Dynamic Programming - 2/Edit_Distance_Problem.cpp
+++ Dynamic Programming - 2/Edit_Distance_Problem.cpp
@@ -1,3 +1,25 @@
+/* Given two strings s and t of lengths m and n respectively, find the Edit Distance between the strings. Edit Distance of two strings is minimum number of steps required to make one string equal to other. In order to do so you can perform following three operations only :
+1. Delete a character
+
+2. Replace a character with another one
+
+3. Insert a character
+Note - Strings don't contain spaces
+Input Format :
+Line 1 : String s
+Line 2 : String t
+Output Format :
+Line 1 : Edit Distance value
+Constraints
+1<= m,n <= 20
+Sample Input 1 :
+abc
+dc
+Sample Output 1 :
+2 */
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
