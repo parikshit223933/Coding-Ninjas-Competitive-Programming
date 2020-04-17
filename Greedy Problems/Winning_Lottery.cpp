78f7857d8a5b781597e493ff3bf310ca23147f7f
parikshit singh <pk223933@gmail.com>
2020-04-17 18:19:24 +0530

    Added Question

--- Greedy Problems/Winning_Lottery.cpp
+++ Greedy Problems/Winning_Lottery.cpp
@@ -1,3 +1,21 @@
+/* Harshit knows by his resources that this time the winning lottery number is the smallest number whose sum of the digits is S and the number of digits is D. You have to help Harshit and print the winning lottery number.
+Input Format
+The Input line contains two space-separated integers: S,D
+Output Format
+The output contains a single integer denoting the winning lottery number
+Constraints
+1 <= D <= 1000
+1 <= S <= 9*D
+Time Limit: 1 second
+Sample Input1:
+9 2
+Sample Output1:
+18
+Explanation
+There are many other possible numbers like 45, 54, 90, etc with the sum of digits as 9 and number of digits as 2. The smallest of them is 18. */
+
+
+
 #include<iostream>
 using namespace std;
 void lottery(int s, int n)
