568cd957900612aea717680386a870b5b26c7b7f
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:55 +0530

    Added Question

--- Dynamic Programming - 2/LCS_Problem.cpp
+++ Dynamic Programming - 2/LCS_Problem.cpp
@@ -1,3 +1,19 @@
+/* Given 2 strings of S1 and S2 with lengths m and n respectively, find the length of longest common subsequence.
+A subsequence of a string S whose length is n, is a string containing characters in same relative order as they are present in S, but not necessarily contiguous. Subsequences contain all the strings of length varying from 0 to n. E.g. subsequences of string "abc" are - "",a,b,c,ab,bc,ac,abc.
+Input Format :
+Line 1 : String S1
+Line 2 : String s2
+Output Format :
+Line 1 : Length of lcs
+Sample Input :
+adebc
+dcadb
+Sample Output :
+3 */
+
+
+
+
 #include<iostream>
 #include<string>
 #include<algorithm>
