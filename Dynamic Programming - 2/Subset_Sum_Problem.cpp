7242dabe58030f999f08a2c5c251474a9ad44f82
parikshit singh <pk223933@gmail.com>
2020-04-17 18:15:03 +0530

    Added Question

--- Dynamic Programming - 2/Subset_Sum_Problem.cpp
+++ Dynamic Programming - 2/Subset_Sum_Problem.cpp
@@ -1,3 +1,20 @@
+/* Given a set of n integers, find if a subset of sum k can be formed from the given set. Print Yes or No.
+
+Input Format
+First line contains a single integer n (1<=n<=1000)
+Second line contains n space separated integers (1<=a[i]<=1000)
+Last line contains a single positive integer k (1<=k<=1000)
+Output Format
+Output Yes if there exists a subset whose sum is k, else output No.
+Sample Input
+3
+1 2 3
+4
+Sample Output
+Yes */
+
+
+
 #include<iostream>
 
 bool checker(int *arr, int n, int k, int **dp)
