6822d417078dffda9e026de55b1cb5f3b91bb945
parikshit singh <pk223933@gmail.com>
2020-04-17 18:19:07 +0530

    Added Question

--- Greedy Problems/Minimum_Absolute_Difference_In_Array.cpp
+++ Greedy Problems/Minimum_Absolute_Difference_In_Array.cpp
@@ -1,3 +1,21 @@
+/* Given an integer array A of size N, find and return the minimum absolute difference between any two elements in the array.
+We define the absolute difference between two elements ai, and aj (where i != j ) is |ai - aj|.
+Input format :
+Line 1 : Integer N, Array Size
+Line 2 : Array elements (separated by space)
+Output Format :
+Minimum difference
+Constraints :
+1 <= N <= 10^6
+Sample Input :
+5
+2 9 0 4 5
+Sample Input :
+1 */
+
+
+
+
 // arr - input array
 // n - size of array
 #include<iostream>
