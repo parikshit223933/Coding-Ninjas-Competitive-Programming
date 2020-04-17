9127b5fa3f84a0b59a516d183aceeead6e5427bf
parikshit singh <pk223933@gmail.com>
2020-04-17 18:14:41 +0530

    Added Question

--- Dynamic Programming - 2/Shortest_Subsequence.cpp
+++ Dynamic Programming - 2/Shortest_Subsequence.cpp
@@ -1,3 +1,18 @@
+/* Gary has two string S and V. Now Gary wants to know the length shortest subsequence in S such that it is not a subsequence in V.
+Note: input data will be such so there will always be a solution.
+Input Format :
+Line 1 : String S of length N (1 <= N <= 1000)
+Line 2 : String V of length M (1 <= M <= 1000)
+Output Format :
+Length of shortest subsequence in S such that it is not a subsequence in V
+Sample Input :
+babab
+babba
+Sample Output :
+3 */
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
