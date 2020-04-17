2e1fcafdfb95fc5b549006800dd81439834cb9b8
parikshit singh <pk223933@gmail.com>
2020-04-17 18:18:58 +0530

    Added Question

--- Greedy Problems/Activity_Selection.cpp
+++ Greedy Problems/Activity_Selection.cpp
@@ -1,3 +1,27 @@
+/* You are given n activities with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
+Input
+The first line of input contains one integer denoting N.
+Next N lines contains two space separated integers denoting the start time and finish time for the ith activity.
+
+Output
+Output one integer, the maximum number of activities that can be performed
+Constraints
+1 ≤ N ≤ 10^6
+1 ≤ ai, di ≤ 10^9
+Sample Input
+6
+1 2
+3 4
+0 6
+5 7
+8 9
+5 9
+Sample Output
+4 */
+
+
+
+
 #include<iostream>
 #include<utility>
 #include<algorithm>
