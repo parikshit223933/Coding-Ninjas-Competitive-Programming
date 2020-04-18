31c4ffb7920b8b1ee507ae8e342eecf4407dc7dd
parikshit singh <pk223933@gmail.com>
2020-04-18 20:59:52 +0530

    Added Question

--- Graphs 1/Code_Has_Path.cpp
+++ Graphs 1/Code_Has_Path.cpp
@@ -1,3 +1,36 @@
+/* Given an undirected graph G(V, E) and two vertices v1 and v2(as integers), check if there exists any path between them or not. Print true or false.
+V is the number of vertices present in graph G and vertices are numbered from 0 to V-1.
+E is the number of edges present in graph G.
+Input Format :
+Line 1: Two Integers V and E (separated by space)
+Next E lines : Two integers a and b, denoting that there exists an edge between vertex a and vertex b (separated by space)
+Line (E+2) : Two integers v1 and v2 (separated by space)
+Output Format :
+true or false
+Constraints :
+2 <= V <= 1000
+1 <= E <= 1000
+0 <= v1, v2 <= V-1
+Sample Input 1 :
+4 4
+0 1
+0 3
+1 2
+2 3
+1 3
+Sample Output 1 :
+true
+Sample Input 2 :
+6 3
+5 3
+0 1
+3 4
+0 3
+Sample Output 2 :
+false */
+
+
+
 #include<iostream>
 using namespace std;
 bool haspath(int** arr, int v, bool* visited, int current_vertex, int v2)
