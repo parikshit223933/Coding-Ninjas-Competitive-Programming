c0043bd60b064288dd45edc55243786d12c2e443
parikshit singh <pk223933@gmail.com>
2020-04-18 21:00:08 +0530

    Added Question

--- Graphs 1/Largest_Piece.cpp
+++ Graphs 1/Largest_Piece.cpp
@@ -1,3 +1,22 @@
+/* Its Gary's birthday today and he has ordered his favourite square cake consisting of '0's and '1's . But Gary wants the biggest piece of '1's and no '0's . A piece of cake is defined as a part which consist of only '1's, and all '1's share an edge with eachother on the cake. Given the size of cake N and the cake , can you find the size of the biggest piece of '1's for Gary ?
+Constraints :
+1<=N<=50
+Input Format :
+Line 1 : An integer N denoting the size of cake 
+Next N lines : N characters denoting the cake
+Output Format :
+Size of the biggest piece of '1's and no '0's
+Sample Input :
+2
+11
+01
+Sample Output :
+3 */
+
+
+
+
+
 #include <iostream>
 using namespace std;
 int count_ones(int **arr, int n, int i, int j, bool **visited)
