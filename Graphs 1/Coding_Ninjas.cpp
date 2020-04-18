fed4a5eca541ea8efb9451739637baddf1edfc49
parikshit singh <pk223933@gmail.com>
2020-04-18 20:59:58 +0530

    Added Question

--- Graphs 1/Coding_Ninjas.cpp
+++ Graphs 1/Coding_Ninjas.cpp
@@ -1,3 +1,23 @@
+/* Given a NxM matrix containing Uppercase English Alphabets only. Your task is to tell if there is a path in the given matrix which makes the sentence “CODINGNINJA” .
+There is a path from any cell to all its neighbouring cells. A neighbour may share an edge or a corner.
+Input Format :
+Line 1 : Two space separated integers N  and M, where N is number of rows and M is number of columns in the matrix.
+Next N lines : N rows of the matrix. First line of these N line will contain 0th row of matrix, second line will contain 1st row and so on
+Assume input to be 0-indexed based
+Output Format :
+Return 1 if there is a path which makes the sentence “CODINGNINJA” else return 0.
+Constraints :
+1 <= N <= 100
+1 <= M <= 100
+Sample Input :
+2 11
+CXDXNXNXNXA
+XOXIXGXIXJX
+Sample Output :
+1 */
+
+
+
 #include<iostream>
 using namespace std;
 bool checker(char arr[][MAXN], int n, int m, int i, int j, string s, bool**visited)
