79f5ed72a1b60ecae9ffe191c26e9c4816c91e42
parikshit singh <pk223933@gmail.com>
2020-04-18 21:00:01 +0530

    Added Question

--- Graphs 1/Connecting_Dots.cpp
+++ Graphs 1/Connecting_Dots.cpp
@@ -1,3 +1,27 @@
+/* Gary has a board of size NxM. Each cell in the board is a coloured dot. There exist only 26 colours denoted by uppercase Latin characters (i.e. A,B,...,Z). Now Gary is getting bore and wants to play a game. The key of this game is to find a cycle that contain dots of same colour. Formally, we call a sequence of dots d1, d2, ..., dk a cycle if and only if it meets the following condition:
+1. These k dots are different: if i ≠ j then di is different from dj.
+2. k is at least 4.
+3. All dots belong to the same colour.
+4. For all 1 ≤ i ≤ k - 1: di and di + 1 are adjacent. Also, dk and d1 should also be adjacent. Cells x and y are called adjacent if they share an edge.
+Since Gary is colour blind, he wants your help. Your task is to determine if there exists a cycle on the board.
+Assume input to be 0-indexed based.
+Input Format :
+Line 1 : Two integers N and M, the number of rows and columns of the board
+Next N lines : a string consisting of M characters, expressing colors of dots in each line. Each character is an uppercase Latin letter.
+Output Format :
+Return 1 if there is a cycle else return 0
+Constraints :
+2 ≤ N, M ≤ 50
+Sample Input :
+3 4
+AAAA
+ABCA
+AAAA
+Sample Output :
+1 */
+
+
+
 #include<iostream>
 using namespace std;
 bool checker(char arr[][MAXN], int n, int m, int i, int j, bool** visited, char current_character)
