35724ed5b94c7dff52e6614b1361138632975ebd
parikshit singh <pk223933@gmail.com>
2020-04-18 20:59:33 +0530

    Added Question

--- Graphs 1/Code_Is_Connected.cpp
+++ Graphs 1/Code_Is_Connected.cpp
@@ -1,3 +1,34 @@
+/* Given an undirected graph G(V,E), check if the graph G is connected graph or not.
+V is the number of vertices present in graph G and vertices are numbered from 0 to V-1.
+E is the number of edges present in graph G.
+Input Format :
+Line 1: Two Integers V and E (separated by space)
+Next 'E' lines, each have two space-separated integers, 'a' and 'b', denoting that there exists an edge between Vertex 'a' and Vertex 'b'.
+Output Format :
+"true" or "false"
+Constraints :
+2 <= V <= 1000
+1 <= E <= 1000
+Sample Input 1:
+4 4
+0 1
+0 3
+1 2
+2 3
+Sample Output 1:
+true
+Sample Input 2:
+4 3
+0 1
+1 3 
+0 3
+Sample Output 2:
+false 
+Sample Output 2 Explanation
+The graph is not connected, even though vertices 0,1 and 3 are connected to each other but there isn’t any path from vertices 0,1,3 to vertex 2.  */
+
+
+
 #include<iostream>
 #include<queue>
 #include<map>
