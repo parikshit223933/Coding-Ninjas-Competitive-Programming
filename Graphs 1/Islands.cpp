f1d68cf838f90afca190bb584ceecdc7080d6464
parikshit singh <pk223933@gmail.com>
2020-04-18 21:00:05 +0530

    Added Question

--- Graphs 1/Islands.cpp
+++ Graphs 1/Islands.cpp
@@ -1,3 +1,23 @@
+/* An island is a small piece of land surrounded by water . A group of islands is said to be connected if we can reach from any given island to any other island in the same group . Given N islands (numbered from 1 to N) and two lists of size M (u and v) denoting island u[i] is connected to island v[i] and vice versa . Can you count the number of connected groups of islands.
+Constraints :
+1<=N<=100
+1<=M<=(N*(N-1))/2
+1<=u[i],v[i]<=N
+Input Format :
+Line 1 : Two integers N and M
+Line 2 : List u of size of M
+Line 3 : List v of size of M
+Output Return Format :
+The count the number of connected groups of islands
+Sample Input :
+2 1
+1
+2
+Sample Output :
+1  */
+
+
+
 #include<iostream>
 #include<queue>
 #include<vector>
