eb64bde94feec53ba0eac3002c39479695ced990
parikshit singh <pk223933@gmail.com>
2020-04-18 20:56:10 +0530

    Added Question

--- Segment Tree/Maximum_Sum_In_Subarray.cpp
+++ Segment Tree/Maximum_Sum_In_Subarray.cpp
@@ -1,3 +1,26 @@
+/* You are given a sequence A[1], A[2], ..., A[N] . ( |A[i]| ≤ 15007 , 1 ≤ N ≤ 50000 ). A query is defined as follows:
+Query(x,y) = Max { a[i]+a[i+1]+...+a[j] ; x ≤ i ≤ j ≤ y }.
+Given M queries, your program must output the results of these queries.
+Input
+The first line of the input file contains the integer N.
+In the second line, N numbers follow.
+The third line contains the integer M.
+M lines follow, where line i contains 2 numbers xi and yi.
+Output
+Your program should output the results of the M queries, one 
+query per line.
+Sample Input:
+3 
+-1 2 3 
+1
+1 2
+Sample Output:
+2 */
+
+
+
+
+
 #include<iostream>
 #include<climits>
 #include<algorithm>
