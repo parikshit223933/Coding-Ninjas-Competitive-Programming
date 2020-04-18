6d21ba3e4945473090de75f89623d104f9e0618d
parikshit singh <pk223933@gmail.com>
2020-04-18 20:55:53 +0530

    Added Question

--- Segment Tree/Counting_Even_Odd.cpp
+++ Segment Tree/Counting_Even_Odd.cpp
@@ -1,3 +1,44 @@
+/* Ashu and Shanu are best buddies. One day Shanu gives Ashu a problem to test his intelligence.He gives him an array of N natural numbers and asks him to solve the following queries:-
+Query 0 :- modify the element present at index i to x.
+Query 1:- count the number of even numbers in range l to r inclusive.
+Query 2:- count the number of odd numbers in range l to r inclusive.
+Input:
+First line of the input contains the number N. Next line contains N natural numbers. 
+Next line contains an integer Q followed by Q queries.
+
+0 x y - modify the number at index x to y. 
+
+1 x y - count the number of even numbers in range l to r inclusive.
+
+2 x y - count the number of odd numbers in range l to r inclusive.
+Constraints:
+1<=N,Q<=10^5
+
+1<=l<=r<=N 
+
+0<=Ai<=10^9
+
+1<=x<=N
+
+0<=y<=10^9
+Note:-
+indexing starts from 1.
+Sample Input
+6
+1 2 3 4 5 6
+4
+1 2 5
+2 1 4
+0 5 4
+1 1 6
+Sample Output
+2
+2
+4 */
+
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
