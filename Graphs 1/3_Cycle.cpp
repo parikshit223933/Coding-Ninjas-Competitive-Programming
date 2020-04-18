3538d01495b80a94d1e31d5811a789419cc2e09a
parikshit singh <pk223933@gmail.com>
2020-04-18 20:59:37 +0530

    Added Question

--- Graphs 1/3_Cycle.cpp
+++ Graphs 1/3_Cycle.cpp
@@ -1,3 +1,23 @@
+/* Given a graph with N vertices (numbered from 1 to N) and Two Lists (U,V) of size M where (U[i],V[i]) and (V[i],U[i]) are connected by an edge , then count the distinct 3-cycles in the graph. A 3-cycle PQR is a cycle in which (P,Q), (Q,R) and (R,P) are connected an edge.
+Input Format :
+Line 1 : Two integers N and M
+Line 2 : List u of size of M
+Line 3 : List v of size of M
+Return Format :
+The count the number of 3-cycles in the given Graph
+Constraints :
+1<=N<=100
+1<=M<=(N*(N-1))/2
+1<=u[i],v[i]<=N
+Sample Input:
+3 3
+1 2 3
+2 3 1
+Sample Output:
+1 */
+
+
+
 #include<iostream>
 #include<vector>
 using namespace std;
