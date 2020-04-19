cb59c6482cb318152c834120e8c4b5d0bc97ef31
parikshit singh <pk223933@gmail.com>
2020-04-19 23:18:42 +0530

    Added Question

--- Tries/Maximum_XOR_Subarray.cpp
+++ Tries/Maximum_XOR_Subarray.cpp
@@ -1,3 +1,20 @@
+/* Given an array of n integers, find subarray whose xor is maximum.
+Input Format
+First line contains single integer n (1<=n<=1000000).
+Next line contains n space separated integers.
+Output Format
+Print xor of the subarray whose xor of all elements in subarray is maximum over all subarrays.
+Constraints
+1 <= n <= 1000000
+1 <= A[i] <=100000 
+Sample Input
+4
+1 2 3 4
+Sample Output
+7 */
+
+
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
