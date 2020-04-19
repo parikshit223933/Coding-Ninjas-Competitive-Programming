c49184417d703a19487c983a0b9d2baff3d30bb7
parikshit singh <pk223933@gmail.com>
2020-04-19 23:18:48 +0530

    Added Question

--- Tries/SUBXOR.cpp
+++ Tries/SUBXOR.cpp
@@ -1,3 +1,23 @@
+/* A straightforward question. Given an array of positive integers you have to print the number of subarrays whose XOR is less than K. Subarrays are defined as a sequence of continuous elements Ai, Ai+1, ..., Aj . XOR of a subarray is defined as Ai ^ Ai+1 ^ ... ^ Aj. Symbol ^ is Exclusive Or.
+Input Format
+First line contains T, the number of test cases. 
+Each of the test case consists of N and K in one line, followed by N space separated integers in next line.
+Output Format
+For each test case, print the required answer.
+Constraints:
+1 ≤ T ≤ 5
+1 ≤ N ≤ 10^5
+1 ≤ A[i] ≤ 10^5
+1 ≤ K ≤ 10^6
+Sample Input
+1
+5 2
+4 1 3 2 7   
+Sample Output
+3 */
+
+
+
 #include <iostream>
 using namespace std;
 class trienode
