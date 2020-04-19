9fe8424ec4371c12265b46f3a8b667d86096cfa0
parikshit singh <pk223933@gmail.com>
2020-04-19 23:15:34 +0530

    Added Question

--- Advanced Graphs/Monk_and_the_islands.cpp
+++ Advanced Graphs/Monk_and_the_islands.cpp
@@ -1,3 +1,32 @@
+/* MONK AND THE ISLAND
+Monk visits the land of Islands. There are a total of N islands numbered from 1 to N. Some pairs of islands are connected to each other by Bidirectional bridges running over water.
+Monk hates to cross these bridges as they require a lot of efforts. He is standing at Island #1 and wants to reach the Island #N. Find the minimum the number of bridges that he shall have to cross, if he takes the optimal route.
+Input:
+First line contains T. T testcases follow.
+First line of each test case contains two space-separated integers N, M.
+Each of the next M lines contains two space-separated integers X and Y , denoting that there is a bridge between Island X and Island Y.
+Output:
+Print the answer to each test case in a new line.
+Constraints:
+1 ≤ T ≤ 10
+1 ≤ N ≤ 10000
+1 ≤ M ≤ 100000
+1 ≤ X, Y ≤ N
+SAMPLE INPUT
+2
+3 2
+1 2
+2 3
+4 4
+1 2
+2 3
+3 4
+4 2
+SAMPLE OUTPUT
+2
+2 */
+
+
 #include <iostream>
 #include <vector>
 #include <queue>
