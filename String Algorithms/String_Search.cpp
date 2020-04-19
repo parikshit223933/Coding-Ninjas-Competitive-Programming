9552ba7537eef9af0f4348c740d997c9fe748a36
parikshit singh <pk223933@gmail.com>
2020-04-19 23:17:02 +0530

    Added Question

--- String Algorithms/String_Search.cpp
+++ String Algorithms/String_Search.cpp
@@ -1,3 +1,24 @@
+/* Given two strings S and T, write a function to find if T is present as a substring inside S or not. If yes, return the starting index otherwise return -1.
+Input format :
+
+Line 1 : String S
+
+Line 2 : String T
+
+Sample Input 1:
+WelcomeBack
+come 
+Sample Output 1:
+3
+Sample Input 2:
+WelcomeBack
+code
+Sample Output 2:
+-1 */
+
+
+
+
 // S and T - input strings
 // You need to check if string T is present in S or not
 #include <iostream>
