2e9e2e2acd20ab06eb5997bcb1786a3402cb7b0c
parikshit singh <pk223933@gmail.com>
2020-04-19 23:16:58 +0530

    Added Question

--- String Algorithms/Palindromic_Substrings.cpp
+++ String Algorithms/Palindromic_Substrings.cpp
@@ -1,3 +1,20 @@
+/* Given a string S, count and return the number of substrings of S that are palindrome.
+Single length substrings are also palindromes. You just need to calculate the count, not the substrings.
+Input Format :
+String S
+Output Format :
+count of palindrome substrings
+Constraints :
+1 <= Length of S <= 1000
+Sample Input :
+aba
+Sample Output :
+4
+**Note : Here 4 corresponds to ("a","b","a","aba"). */
+
+
+
+
 #include <iostream>
 using namespace std;
 int size(char s[])
