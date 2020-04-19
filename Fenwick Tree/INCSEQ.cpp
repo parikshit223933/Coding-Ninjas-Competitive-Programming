a372b291c9d3333520c3067c608037cd6ffabfc8
parikshit singh <pk223933@gmail.com>
2020-04-19 23:21:04 +0530

    Added Question

--- Fenwick Tree/INCSEQ.cpp
+++ Fenwick Tree/INCSEQ.cpp
@@ -1,3 +1,19 @@
+/* Given a sequence of N (1 ≤ N ≤ 10,000) integers S1, ..., SN (0 ≤ Si < 100,000), compute the number of increasing subsequences of S with length K (1 ≤ K ≤ 50 and K ≤ N); that is, the number of K-tuples i1, ..., iK such that 1 ≤ i1 < ... < iK ≤ N and Si1 < ... < SiK.
+Input
+The first line contains the two integers N and K. The following N lines contain the integers of the sequence in order.
+Output
+Print a single integer representing the number of increasing subsequences of S of length K, modulo 5,000,000.
+Sample Input
+4 3
+1
+2
+2
+10
+Output:
+2 */
+
+
+
 #include<iostream>
 #include<vector>
 #include<unordered_map>
