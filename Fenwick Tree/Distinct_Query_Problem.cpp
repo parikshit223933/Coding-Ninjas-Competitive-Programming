a1400716dbf3edb6f0986e88df5923b0eeb56055
parikshit singh <pk223933@gmail.com>
2020-04-19 23:20:59 +0530

    Added Question

--- Fenwick Tree/Distinct_Query_Problem.cpp
+++ Fenwick Tree/Distinct_Query_Problem.cpp
@@ -1,3 +1,26 @@
+/* Given a sequence of n numbers a1, a2, ..., an and a number of d-queries. A d-query is a pair (i, j) (1 ≤ i ≤ j ≤ n). For each d-query (i, j), you have to return the number of distinct elements in the subsequence ai, ai+1, ..., aj.
+Input
+Line 1: n (1 ≤ n ≤ 30000).
+Line 2: n numbers a1, a2, ..., an (1 ≤ ai ≤ 10^6).
+Line 3: q (1 ≤ q ≤ 200000), the number of d-queries.
+In the next q lines, each line contains 2 numbers i, j representing a d-query (1 ≤ i ≤ j ≤ n).
+Output
+For each d-query (i, j), print the number of distinct elements in the subsequence ai, ai+1, ..., aj in a single line.
+Sample Input
+5
+1 1 2 1 3
+3
+1 5
+2 4
+3 5
+Sample Output
+3
+2
+3  */
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 /////////////////////////////////////////////////////////////////////////////////////////
