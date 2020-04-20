95e5d4adb2e8001ae3bc99e7ed9de556f02cbb38
parikshit singh <pk223933@gmail.com>
2020-04-20 14:06:22 +0530

    Added Question

--- Number Theory 3/Fibonacci_Sum.cpp
+++ Number Theory 3/Fibonacci_Sum.cpp
@@ -1,3 +1,20 @@
+/* The fibonacci sequence is defined by the following relation:
+ F(0) = 0
+ F(1) = 1
+ F(N) = F(N - 1) + F(N - 2), N >= 2
+Your task is very simple. Given two non-negative integers N and M (N <= M), you have to calculate and return the sum (F(N) + F(N + 1) + ... + F(M)) mod 1000000007.
+Input Format :
+Two non-negative integers N and M. (N <= M)
+Output Format :
+A single line containing the answer for the task.
+Sample Input :
+10 
+19
+Sample Output :
+10857 */
+
+
+
 #include <iostream>
 #define mod 1000000007
 #define ll unsigned long long
