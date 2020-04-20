b0293c15dff70590b6d172bbb66becab90b54cbe
parikshit singh <pk223933@gmail.com>
2020-04-20 13:27:37 +0530

    Added Question

--- DP and BitMasking/Counting_Strings.cpp
+++ DP and BitMasking/Counting_Strings.cpp
@@ -1,3 +1,12 @@
+/* Given a string 's' consisting of upper case alphabets, i.e. from 'A' to 'Z'. Your task is to find how many strings 't' with length equal to that of 's', also consisting of upper case alphabets are there satisfying the following conditions:
+-> String 't' is lexicographical larger than string 's'.
+-> When you write both 's' and 't' in the reverse order, 't' is still lexicographical larger than 's'.
+Find out number of such strings 't'. As the answer could be very large, take modulo 10^9 + 7.
+ */
+
+
+
+
 #define mod 1000000007
 #define ll long long int
 #include<string>
