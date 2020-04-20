a17b67fa33d4e168b2d0255e44c202d6f9cee323
parikshit singh <pk223933@gmail.com>
2020-04-20 13:28:53 +0530

    Added Question

--- Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
+++ Number theory-1/Finding_Prime_Numbers_From_1_to_N.cpp
@@ -1,3 +1,16 @@
+/* Given a number N, find number of primes in the range [1,N].
+Input:
+The only line of input consists of a number N
+Output:
+Print the number of primes in the range [1,N].`
+Constraints:
+1≤N≤1000000
+Sample Input :
+3 
+Sample Output -
+2 */
+
+
 #include<iostream>
 #include<math.h>
 using namespace std;
