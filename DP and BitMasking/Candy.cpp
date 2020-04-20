01ff3dd72bc7e3da9e5eebd8b3a50be53a3c2790
parikshit singh <pk223933@gmail.com>
2020-04-20 13:27:33 +0530

    Added Question

--- DP and BitMasking/Candy.cpp
+++ DP and BitMasking/Candy.cpp
@@ -1,3 +1,20 @@
+/* Gary is a teacher at XYZ school. To reward his N students he bought a packet of N candies all with different flavours. But the problem is some students like certain flavour while some doesn't. Now Gary wants to know the number of ways he can distribute these N candies to his N students such that every student gets exactly one candy he likes.
+Input Format :
+Line 1 : An integer N (1<= N <= 16) denoting number of students and candies.
+Next N lines : N integers describing the preferences of one student. 1 at i'th (0 <= i < N) position denotes that this student likes i'th candy , 0 means he doesn't.
+Assume input to be 0-indexed based.
+Output Format :
+Return the number of ways Gary can distribute these N candies to his N students such that every student gets exactly one candy he likes.``
+Sample Input:
+3
+1 1 1
+1 1 1
+1 1 1
+Sample Output:
+6 */
+
+
+
 int candies(int like[][MAXN], int n, int person, int mask, int *dp)
 {
     if (person >= n)
