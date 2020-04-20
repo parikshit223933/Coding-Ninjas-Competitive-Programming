04e6820754c0c176b0d6260a650292e33cef8f20
parikshit singh <pk223933@gmail.com>
2020-04-20 14:02:08 +0530

    Added Question

--- Applications of Number Theory-1/Advanced_GCD.cpp
+++ Applications of Number Theory-1/Advanced_GCD.cpp
@@ -1,3 +1,29 @@
+/* Varun explained its friend Sanchit the algorithm of Euclides to calculate the GCD of two numbers. Then Sanchit implements the algorithm
+int gcd(int a, int b)
+{
+
+    if (b==0)
+    return a;
+    else
+    return gcd(b,a%b);
+}
+and challenges to Varun to calculate gcd of two integers, one is a little integer and other integer has 250 digits.
+Your task is to help Varun an efficient code for the challenge of Sanchit.
+Input
+The first line of the input file contains a number representing the number of lines to follow. Each line consists of two number A and B (0 <= A <= 40000 and A <= B < 10^250).
+Output
+Print for each pair (A,B) in the input one integer representing the GCD of A and B..
+Sample Input:
+2
+2 6
+10 11
+Sample Output:
+2
+1 */
+
+
+
+
 #include<iostream>
 #include<string>
 using namespace std;
