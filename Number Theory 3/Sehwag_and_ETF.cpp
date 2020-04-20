96d6300b0db299701cbc1994a1eb13f5549d31ab
parikshit singh <pk223933@gmail.com>
2020-04-20 14:06:35 +0530

    Added Question

--- Number Theory 3/Sehwag_and_ETF.cpp
+++ Number Theory 3/Sehwag_and_ETF.cpp
@@ -1,3 +1,27 @@
+/* Sehwag has been solving a lot of mathematical problems recently. He was learning ETF (Euler Totient Function) and found the topic quite interesting. So, he tried solving a question on ETF. He will be given two numbers L and R. He has to find the probability that the ETF of a number in the range [L, R] is divisible by a number K.
+Input:
+The first line contains the number of test cases i.e. T.
+The next T lines will contain three integers L, R and K.
+Output:
+Print the answer in a new line after rounding off the first 6 digits after the decimal place.
+Constraints:
+1 <= T <= 10
+1 <= L <= R <= 10^12
+0 <= R - L <= 10^5
+1 <= K <= 10^6
+Sample Input:
+3
+1 4 2
+2 5 2
+3 10 4
+Sample Output:
+0.500000
+0.750000
+0.375000 */
+
+
+
+
 #include<bits/stdc++.h>
 #define ll long long
 #define MAX 1100001
