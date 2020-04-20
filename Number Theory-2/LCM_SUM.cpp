5bcd8e27069fe62e08b659cb23756944935f1ca6
parikshit singh <pk223933@gmail.com>
2020-04-20 14:03:28 +0530

    Added Question

--- Number Theory-2/LCM_SUM.cpp
+++ Number Theory-2/LCM_SUM.cpp
@@ -1,3 +1,23 @@
+/* Given n, calculate and print the sum :
+LCM(1,n) + LCM(2,n) + .. + LCM(n,n)
+where LCM(i,n) denotes the Least Common Multiple of the integers i and n.
+Input Format :
+Line 1 : Integer n
+Output Format :
+Required sum
+Constraints :
+1 <= T <= 300000
+1 <= n <= 1000000
+Sample Input 1 :
+5
+Sample Output 1 :
+55
+Sample Input 2 :
+2
+Sample Output 2 :
+4 */
+
+
 #include<iostream>
 using namespace std;
 typedef unsigned long long int ll;
