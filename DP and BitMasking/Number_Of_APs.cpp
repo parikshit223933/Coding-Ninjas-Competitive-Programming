05671057850e4d474b878b517a3e5ad6ead413d0
parikshit singh <pk223933@gmail.com>
2020-04-20 13:27:49 +0530

    Added Question

--- DP and BitMasking/Number_Of_APs.cpp
+++ DP and BitMasking/Number_Of_APs.cpp
@@ -1,3 +1,29 @@
+/* Given an array of n positive integers. The task is to count the number of Arithmetic Progression subsequences in the array. As the answer could be very large, output it modulo 100001.
+Note: Empty sequence or single element sequence is Arithmetic Progression.
+Input Format:
+First Line: N (the size of the array)
+Second Line: Elements of the array separated by spaces.
+Output:
+ Print total number of subsequences
+Input Constraints:
+1 <= arr[i] <= 1000
+1 <= sizeof(arr) <= 1000
+Sample Input 1 :
+3
+1 2 3
+Sample output:
+8
+Sample Output Explanation:
+Total subsequence are: {}, { 1 }, { 2 }, { 3 }, { 1, 2 }, { 2, 3 }, { 1, 3 }, { 1, 2, 3 }
+Sample Input 2:
+7
+1 2 3 4 5 9 10
+Sample Output:
+37 */
+
+
+
+
 #include<iostream>
 #define m 100001
 #include<unordered_map>
