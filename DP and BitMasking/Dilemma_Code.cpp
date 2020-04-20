c0c57b0d0a05238597f31818c30ace4f0f6a12c7
parikshit singh <pk223933@gmail.com>
2020-04-20 13:27:40 +0530

    Added Question

--- DP and BitMasking/Dilemma_Code.cpp
+++ DP and BitMasking/Dilemma_Code.cpp
@@ -1,3 +1,22 @@
+/* Abhishek, a blind man recently bought N binary strings all of equal length .A binary string only contains '0's and '1's . The strings are numbered from 1 to N and all are distinct, but Abhishek can only differentiate between these strings by touching them. In one touch Abhishek can identify one character at a position of a string from the set. Find the minimum number of touches T Abhishek has to make so that he learn that all strings are different .
+Constraints :
+2<=N<=10
+1<=|L|<=100 , L is length of the strings .
+Input Format:
+Line 1 : An Integer N, denoting the number of binary strings .
+Next N lines : strings of equal length
+Output Format:
+Return the minimum number of touches
+Sample Input :
+2
+111010
+100100
+Sample Output :
+2 */
+
+
+
+
 #include<vector>
 #include<algorithm>
 #include<climits>
