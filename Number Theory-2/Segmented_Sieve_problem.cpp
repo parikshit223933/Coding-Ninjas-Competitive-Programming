9b1a45575baca780a856f0ed92299d71d1d8ac7a
parikshit singh <pk223933@gmail.com>
2020-04-20 14:03:31 +0530

    Added Question

--- Number Theory-2/Segmented_Sieve_problem.cpp
+++ Number Theory-2/Segmented_Sieve_problem.cpp
@@ -1,3 +1,25 @@
+/* In this problem you have to print all primes from given interval.
+Input
+t - the number of test cases, then t lines follows. [t <= 150]
+On each line are written two integers L and U separated by a blank. L - lower bound of 
+interval, U - upper bound of interval. [2 <= L < U <= 2147483647] [U-L <= 1000000].
+Output
+For each test case output must contain all primes from interval [L; U] in increasing order.
+Sample Input:
+2
+2 10
+3 7
+Sample Output:
+2
+3
+5
+7
+3
+5
+7  */
+
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
