14ffcfd0a2edf0358b7ccdc034c043ef34f54562
parikshit singh <pk223933@gmail.com>
2020-04-20 14:02:27 +0530

    Added Question

--- Applications of Number Theory-1/Strange_Order.cpp
+++ Applications of Number Theory-1/Strange_Order.cpp
@@ -1,3 +1,24 @@
+/* Given an integer n . Initially you have permutation p of size n : p[i] = i . To build new array a from p following steps are done while permutation p is not empty:
+Choose maximum existing element in p and define it as x ; Choose all such y in p that gcd ( x , y ) ≠ 1 ; Add all chosen numbers into array a in decreasing order and remove them from permutation. Your task is to find a after p became empty.
+Note: gcd ( a , b ) is the greatest common divisor of integers a and b .
+Input format
+Input contains single integer n ( 1 ≤ n ≤ 2 * 10^6) — the size of permutation p p.
+Output format
+Print n integers — array a .
+Sample Input:
+5
+Sample Output:
+5 4 2 3 1
+Explanation
+It's needed 4 iterations to create array a:
+Add 5
+Add 4 and 2
+Add 3
+Add 1 */
+
+
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
