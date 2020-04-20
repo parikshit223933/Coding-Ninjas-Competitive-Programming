05f0cfa9d809dfa6b330251bcc27358313fd6324
parikshit singh <pk223933@gmail.com>
2020-04-20 14:02:14 +0530

    Added Question

--- Applications of Number Theory-1/Divisors_of_a_factorial.cpp
+++ Applications of Number Theory-1/Divisors_of_a_factorial.cpp
@@ -1,3 +1,28 @@
+/* Given a number, find the total number of divisors of the factorial of the number.
+Since the answer can be very large, print answer modulo 10^9+7.
+Input
+The first line contains T, number of testcases.
+
+
+T lines follows each containing the number N.
+Output
+Print T lines of output each containing the answer.
+Constraints
+1 <= T <= 500
+
+0 <= N <= 50000
+Sample Input:
+3
+2
+3
+4
+Sample Output:
+2
+4
+8 */
+
+
+
 #include <iostream>
 #include <math.h>
 #define m 1000000007
