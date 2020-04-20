df230eb00bf122b6cb55a01c5717bcb1e48da72a
parikshit singh <pk223933@gmail.com>
2020-04-20 13:28:57 +0530

    Added Question

--- Number theory-1/GCD.cpp
+++ Number theory-1/GCD.cpp
@@ -1,3 +1,22 @@
+/* Calculate and return GCD of two given numbers x and y. Numbers are within range of Integer.
+Input format :
+x and y (separated by space)
+Output format :
+GCD of x and y
+Sample Input 1:
+20 
+5
+Sample Output 1:
+5
+Sample Input 2:
+96 
+14
+Sample Output 2:
+2 */
+
+
+
+
 #include<iostream>
 #include<algorithm>
 using namespace std;
