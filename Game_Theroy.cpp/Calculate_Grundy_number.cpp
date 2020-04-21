305cc934a0ff22f77628f645712d129d24b0e42b
parikshit singh <pk223933@gmail.com>
2020-04-21 12:42:01 +0530

    Added Question

--- Game_Theroy.cpp/Calculate_Grundy_number.cpp
+++ Game_Theroy.cpp/Calculate_Grundy_number.cpp
@@ -1,3 +1,16 @@
+/* Calculate the Grundy Number for the given 'n' in the game.
+The game starts with a number- ‘n’ and the player to move divides the number- ‘n’ with 2, 3 or 6 and then takes the floor. If the integer becomes 0, it is removed. The last player to move wins. Which player wins the game?
+Input Format
+An Integer 'n'
+Output Format
+Grundy Number(n)
+Sample Input 1 -
+10
+Sample Output 1-
+0 */
+
+
+
 #include<iostream>
 #include<math.h>
 #include<algorithm>
