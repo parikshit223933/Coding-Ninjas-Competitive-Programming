812c0c00b91d86618df4e4b09960a1d0710663e2
parikshit singh <pk223933@gmail.com>
2020-04-21 12:41:48 +0530

    Added Question

--- Computational Geometry/Convex_Hull.cpp
+++ Computational Geometry/Convex_Hull.cpp
@@ -1,3 +1,27 @@
+/* Given a set of points in the plane. the convex hull of the set is the smallest convex polygon that contains all the points of it.
+Find out the convex hull for the given set of points.
+Input Format
+Integer N(Number Of points)
+Array of X coordinates
+Array of Y coordinates
+Output Format
+X coordinate and Y coordinate of all the points seperated by space. Order doesn't matter.
+Sample Testcase
+Input
+7
+2 1 6 5 3 7 9
+2 5 1 5 7 6 4
+Output
+1 5
+2 2
+6 1
+9 4
+7 6
+3 7 */
+
+
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
