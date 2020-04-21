517985f4ab778ee99563930fabb981aafbee0f58
parikshit singh <pk223933@gmail.com>
2020-04-21 12:41:51 +0530

    Added Question

--- Computational Geometry/Surveyor.cpp
+++ Computational Geometry/Surveyor.cpp
@@ -1,3 +1,29 @@
+/* A plot has been surveyed. Its boundary consists of segments that form a polygon. Each segment runs either North-South or East-West. Calculate the enclosed area.
+The i-th character of direction and the i-th element of length describe the direction and length of the i-th segment of the polygon's boundary as the surveyor walked it. If you start at the surveyor's starting point and walk according to the sequence of directions and lengths reported by the surveyor, you will traverse the boundary of the polygon, ending up back at the starting point. This traversal described by the given segments may be either clockwise or counterclockwise.
+Input Format
+String s
+Array of length of string
+Output Format
+Area
+Constraints
+direction will have between 4 and 50 characters inclusive.
+length will have the same number of elements as the number of characters in direction.
+Each element of direction will be an uppercase letter 'N', 'E', 'S', or 'W'.
+Each element of length will be between 1 and 1000 inclusive.
+The segments will represent a simple polygon. No two segments will touch or intersect (except that the last point of a segment is the first point of the next segment, and the last point of the final segment is the first point of the first segment).
+Sample Test Case
+"NWWSE"
+{10,3,7,10,10}
+Returns: 100
+
+
+"NESWNWSW"
+{20,200,30,100,20,30,10,70}
+Returns: 4700 */
+
+
+
+
 #include <bits/stdc++.h>
 #define endl '\n'
 using namespace std;
