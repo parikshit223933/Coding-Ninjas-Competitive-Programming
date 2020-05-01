91a7470d1d3fbfb28e58f1cc8e351046f3d9c8bd
parikshit singh <pk223933@gmail.com>
2020-05-02 03:57:03 +0530

    base

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -1,4 +1,4 @@
-/* #include <iostream>
+#include <iostream>
 #include <unordered_map>
 #include <vector>
 #include <limits.h>
@@ -8,41 +8,8 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 using namespace std;
-int find_in_queue(string s, int n, int c, unordered_map<char, int> m)
-{
-
-    int total_in_queue = 0;
-    for (auto i : m)
-    {
-        if (i.second > c)
-        {
-            total_in_queue += i.second - c;
-        }
-    }
-    return total_in_queue;
-}
 int main()
 {
     fast;
-    int t; //number of test cases
-    cin >> t;
-    while (t--)
-    {
-        int n; //number of people
-        int q; //number of queries
-        cin >> n >> q;
-        string s; //diseases -> n sized -> for n people
-        cin >> s;
-        unordered_map<char, int> m;
-        for (auto i : s)
-        {
-            m[i]++;
-        }
-        while (q--)
-        {
-            int c; //number of available isolation centres
-            cin >> c;
-            cout << find_in_queue(s, n, c, m) << endl;
-        }
-    }
-} */
+    
+}
