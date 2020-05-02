204a54d8d4b9ff67f5b4d8a68b564d30371715a8
parikshit singh <pk223933@gmail.com>
2020-05-02 20:49:26 +0530

    staged

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -8,8 +8,26 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 using namespace std;
+void performer(int *arr, int n, int k)
+{
+    
+}
 int main()
 {
     fast;
-    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        int k;
+        cin>>n>>k;
+        int *arr=new int [n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        performer(arr, n, k);
+    }
+    return 0;
 }
