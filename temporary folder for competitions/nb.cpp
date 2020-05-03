1403bb3ed1d2549c6288592cb1f1c9d40522ef89
parikshit singh <pk223933@gmail.com>
2020-05-03 06:21:22 +0530

    staged

--- temporary folder for competitions/nb.cpp
+++ temporary folder for competitions/nb.cpp
@@ -10,17 +10,52 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 using namespace std;
+struct possibility
+{
+    char direction;
+    int index;
+};
+bool arr_is_empty(int **arr, int n)
+{
+    bool check=true;
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(arr[i][j]!=0)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int laser(int **arr, int n, int f, int k, possibility*poss, int n_poss)
+{
+    
+}
 void solve(int **arr, int n, int f, int k)
 {
+    vector<possibility>poss;
     char dirs[]={'L', 'R', 'U', 'D'};
-    vector<string>possibilities;
     for(auto d:dirs)
     {
         for(int i=0; i<n; i++)
         {
-            
+            possibility p;
+            p.direction=d;
+            p.index=i;
+            poss.push_back(p);
         }
     }
+    int n_poss=poss.size();
+    possibility*possibility_array=new possibility[n_poss];
+    for(int i=0; i<n_poss; i++)
+    {
+        possibility_array[i]=poss[i];
+    }
+    int minimum_shots=laser(arr, n, f, k, possibility_array, n_poss);
+    cout<<minimum_shots<<endl;
 }
 int32_t main()
 {
