f464e14dcd21a74712eb67c617c75ebbb70c5f88
parikshit singh <pk223933@gmail.com>
2020-05-07 02:13:50 +0530

    staged

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -46,59 +46,115 @@ bool arr_is_empty(int **arr, int n)
 }
 int **create_backup(int **backup_array, int **arr, int n)
 {
-    backup_array=new int *[n];
-    for(int i=0;i<n; i++)
+    backup_array = new int *[n];
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; i<n; j++)
+        backup_array[i] = new int[n];
+        for (int j = 0; i < n; j++)
         {
-            backup_array[i][j]=arr[i][j];
+            backup_array[i][j] = arr[i][j];
         }
     }
     return backup_array;
 }
 void restore_original_array(int **arr, int **backup_array, int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0; j<n; j++)
+        for (int j = 0; j < n; j++)
         {
-            arr[i][j]=backup_array[i][j];
+            arr[i][j] = backup_array[i][j];
         }
     }
 }
 void fire(int **arr, int n, bots way, int f)
 {
-    
+    char dir = way.direction;
+    int idx = way.index;
+    if (dir == 'L')
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[idx][i] <= f)
+            {
+                f -= arr[idx][i];
+                arr[idx][i] = 0;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    if (dir == 'R')
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr[idx][i] <= f)
+            {
+                f -= arr[idx][i];
+                arr[idx][i] = 0;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    if (dir == 'U')
+    {
+        for (int i = 0; i <n; i++)
+        {
+            if (arr[i][idx] <= f)
+            {
+                f -= arr[i][idx];
+                arr[i][idx] = 0;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    if (dir == 'D')
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr[i][idx] <= f)
+            {
+                f -= arr[i][idx];
+                arr[i][idx] = 0;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
 }
-bool shots(int **arr, int n, int k, int f, bots *ways, bool *visited)
+bool shots(int **arr, int n, int k, int f, bots *ways)
 {
-    if ((all_visited(visited, 4 * n) && !arr_is_empty(arr, n)) || k > 0)
+    if ((!arr_is_empty(arr, n) && k <= 0) || (k < 0))
     {
         return false;
     }
-    if(k>=0&&arr_is_empty(arr, n))
+    if (k >= 0 && arr_is_empty(arr, n))
     {
         return true;
     }
     for (int i = 0; i < 4 * n; i++)
     {
-        if (!visited[i])
+        int **backup_array;
+        backup_array = create_backup(backup_array, arr, n);
+        fire(arr, n, ways[i], f);
+        if (shots(arr, n, k, f, ways))
         {
-            visited[i]=true;
-            int **backup_array;
-            backup_array=create_backup(backup_array, arr, n);
-            fire(arr, n, ways[i], f);
-            if(shots(arr, n, k, f, ways, visited))
-            {
-               return true; 
-            }
-            else
-            {
-                restore_original_array(arr, backup_array, n);
-                visited[i]=false;
-                continue;
-            }
-            
+            return true;
+        }
+        else
+        {
+            restore_original_array(arr, backup_array, n);
+            continue;
         }
     }
 }
@@ -113,13 +169,8 @@ void solver(int **arr, int n, int k, int f)
             ways[i].index = i;
         }
     }
-    bool *visited = new bool[4 * n];
-    for (int i = 0; i < 4 * n; i++)
-    {
-        visited[i] = false;
-    }
-    int number_of_shots = shots(arr, n, k, f, ways, visited);
-    cout<<shots(arr, n, k, f, ways, visited)<<endl;
+    int number_of_shots = shots(arr, n, k, f, ways);
+    cout << shots(arr, n, k, f, ways) << endl;
 }
 int32_t main()
 {
