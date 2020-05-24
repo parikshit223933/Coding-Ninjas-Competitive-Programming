e1532d3b84baf78fd13072aed7edc4faf33fc758
parikshit singh <pk223933@gmail.com>
2020-05-24 23:57:29 +0530

    not a good approach

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -1,8 +1,10 @@
-/* #include <algorithm>
+#include <algorithm>
 #include <iostream>
 #include <utility>
 #include<string>
 #include <iterator>
+#include<cstring>
+#include<math.h>
 #include <limits.h>
 #include <vector>
 #include<unordered_map>
@@ -12,54 +14,7 @@
     cin.tie(NULL);
 #define endl '\n'
 using namespace std;
-bool checker(int *arr, int n)
-{
-    unordered_map<int, int> m;
-    for(int i=0; i<n; i++)
-    {
-        m[arr[i]]++;
-    }
-    unordered_set<int>keys;
-    unordered_set<int>values;
-    for(auto i:m)
-    {
-        keys.insert(i.first);
-        values.insert(i.second);
-    }
-    if(keys.size()!=values.size())
-    {
-        return false;
-    }
-    
-
-
-
-    int *arr_temp=new int [1001];
-    bool *visited=new bool[1001];
-    for(int i=0; i<1001; i++)
-    {
-        arr_temp[i]=0;
-        visited[i]=false;
-    }
-    arr_temp[arr[0]]++;
-    for(int i=1; i<n; i++)
-    {
-        arr_temp[arr[i]]++;
-        if(arr[i]!=arr[i-1])
-        {
-            visited[arr[i-1]]=true;
-        }
-        if(visited[arr[i-1]])
-        {
-            if(m[arr[i-1]]!=arr_temp[arr[i-1]])
-            {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-int32_t main()
+int main()
 {
     fast;
     int t;
@@ -68,19 +23,29 @@ int32_t main()
     {
         int n;
         cin>>n;
-        int *arr=new int [n];
-        for(int i=0; i<n; i++)
+        n=sqrt(n);
+        cout<<n;
+        int i=1;
+        int j=n;
+        bool switcher=false;
+        int ans=0;
+        while(i!=j)
         {
-            cin>>arr[i];
+            ans+=(i&j);
+            if(switcher)
+            {
+                j--;
+                switcher=false;
+            }
+            else
+            {
+                i++;
+                switcher=true;
+            }
         }
-        if(checker(arr, n))
-        {
-            cout<<"YES"<<endl;
-        }
+        if(ans==0)
+            cout<<-1<<endl;
         else
-        {
-            cout<<"NO"<<endl;
-        }
-        
+            cout<<ans<<endl;
     }
-} */
+}
