a038f64b4b4245696021038c3463b15c0fbaac71
parikshit singh <pk223933@gmail.com>
2020-06-07 03:58:53 +0530

    implemented

--- temporary folder for competitions/.vscode/temp.cpp
+++ temporary folder for competitions/.vscode/temp.cpp
@@ -11,9 +11,35 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
+#define ll long long int
 using namespace std;
+
+ll solve(int *arr, int n, int k)
+{
+    ll sum=0;
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]>k)
+        {
+            sum+=arr[i]-k;
+        }
+    }
+    return sum;
+}
 int main()
 {
     fast;
-    
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, k;
+        cin>>n>>k;
+        int *arr=new int [n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<solve(arr, n, k)<<endl;
+    }
 }
