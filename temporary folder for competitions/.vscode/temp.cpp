d916141215aa0a8a1f2c2bd854c969b29175d84d
parikshit singh <pk223933@gmail.com>
2020-06-07 17:25:12 +0530

    base file

--- temporary folder for competitions/.vscode/temp.cpp
+++ temporary folder for competitions/.vscode/temp.cpp
@@ -16,53 +16,8 @@
 #define ll long long int
 using namespace std;
 
-void print(int**arr, int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n; j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-int **get_array(int n)
-{
-    int **arr = new int*[n];
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = new int[n];
-    }
-    int count = 1;
-    for (int j = 0; j < n; j++)
-    {
-        for (int i = 0; i <= j; i++)
-        {
-            arr[i][j] = count;
-            count++;
-        }
-        for (int k = j - 1; k >= 0; k--)
-        {
-            arr[j][k]=count;
-            count++;
-        }
-    }
-    return arr;
-}
-
 int main()
 {
     fast;
-    int **arr=get_array(1000);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        print(arr, n);
-    }
-    delete[]arr;
+    
 }
