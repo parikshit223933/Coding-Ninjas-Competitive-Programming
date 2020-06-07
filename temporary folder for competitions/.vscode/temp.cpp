4e2824dbef80359bd7fa333cd244f8a8f4dfe671
parikshit singh <pk223933@gmail.com>
2020-06-07 19:01:21 +0530

    implemented

--- temporary folder for competitions/.vscode/temp.cpp
+++ temporary folder for competitions/.vscode/temp.cpp
@@ -13,11 +13,105 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
 
-int main()
+bool check(int *arr, int n)
+{
+    if(n==0)
+    {
+        return true;
+    }
+    if(arr[0]!=5)
+    {
+        return false;
+    }
+    int ans=1; // denotes true
+    int count5=1;
+    int count10=0;
+    int count15=0;
+    for(int i=1; ans==1&&i<n; i++)
+    {
+        if(arr[i]==5)
+        {
+            count5++;
+        }
+        else if(arr[i]==10)
+        {
+            count10++;
+        }
+        else
+        {
+            count15++;
+        }
+        int remaining=arr[i]-5;
+        if(remaining==5)
+        {
+            if(count5>0)
+            {
+                count5--;
+            }
+            else
+            {
+                ans=0;
+                return false;
+            }
+        }
+        else if(remaining==10)
+        {
+            if(count10>0)
+            {
+                count10--;
+            }
+            else if(count5>=2)
+            {
+                count5-=2;
+            }
+            else
+            {
+                ans=0;
+                return false;
+            }
+        }
+    }
+    if(ans==1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+string isPossible(int *arr, int n)
+{
+    if (check(arr, n))
+    {
+        return "YES";
+    }
+    else
+    {
+        return "NO";
+    }
+}
+
+int32_t main()
 {
     fast;
-    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int *arr = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+        cout << isPossible(arr, n) << endl;
+    }
+    return 0;
 }
--- temporary folder for competitions/.vscode/tempCodeRunnerFile.cpp
+++ temporary folder for competitions/.vscode/tempCodeRunnerFile.cpp
@@ -13,133 +13,42 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
 
-int maximum_pairs(string s)
+int calculate(int ts)
 {
-    int n = s.length();
-    int *arr = new int[n];
-
-    for (int i = 0; i < n; i++)
+    if( ts <= 2 )
     {
-        if (s[i] == 'x')
-        {
-            arr[i] = 1;
-        }
-        else
-        {
-            arr[i] = 0;
-        }
+        return 0;
     }
-
-    unordered_map<int, int> m;
-    for (int i = 0; i < n; i++)
-    {
-        m[arr[i]]++;
-    }
-
-    int count = 0;
-    if (m[0] > m[1]) /* there are less ones */
-    {
-        for (int i = 0; i < n; i++)
+	if( ts % 2 != 0 )
+		return  (ts/2) ;
+	else
+	{
+        int count=0;
+		int js = ts ;
+		while( js > 1 && js % 2 == 0 )
+		{
+			js /= 2 ;
+		}
+		if( js > 1 )
         {
-            if (arr[i] == 1)
-            {
-                if (i == 0)
-                {
-                    if (arr[i + 1] == 0)
-                    {
-                        count++;
-                        arr[i] = 2;
-                        arr[i+1]=2;
-                    }
-                    continue;
-                }
-                else if (i == n - 1)
-                {
-                    if (arr[i - 1] == 0)
-                    {
-                        count++;
-                        arr[i] = 2;
-                        arr[i-1]=2;
-                    }
-                    continue;
-                }
-                else
-                {
-                    if (arr[i - 1] == 0)
-                    {
-                        arr[i] = 2;
-                        arr[i - 1] = 2;
-                        count++;
-                    }
-                    else if (arr[i + 1] == 0)
-                    {
-                        arr[i] = 2;
-                        arr[i + 1] = 2;
-                        count++;
-                    }
-                }
-            }
+            count=js/2;
         }
+        return count;
     }
-    else /* there are less zeros */
-    {
-        for (int i = 0; i < n; i++)
-        {
-            if (arr[i] == 0)
-            {
-                if (i == 0)
-                {
-                    if (arr[i + 1] == 1)
-                    {
-                        count++;
-                        arr[i] = 2;
-                        arr[i+1]=2;
-                    }
-                    continue;
-                }
-                if (i == n - 1)
-                {
-                    if (arr[i - 1] == 1)
-                    {
-                        count++;
-                        arr[i] = 2;
-                        arr[i-1]=2;
-                    }
-                    continue;
-                }
-                else
-                {
-                    if (arr[i - 1] == 1)
-                    {
-                        arr[i] = 2;
-                        arr[i - 1] = 2;
-                        count++;
-                    }
-                    else if (arr[i + 1] == 1)
-                    {
-                        arr[i] = 2;
-                        arr[i + 1] = 2;
-                        count++;
-                    }
-                }
-            }
-        }
-    }
-    delete[] arr;
-    return count;
 }
-int main()
+
+int32_t main()
 {
     fast;
     int t;
-    cin >> t;
-    while (t--)
+    cin>>t;
+    while(t--)
     {
-        string s;
-        cin >> s;
-        cout << maximum_pairs(s) << endl;
+        int ts;
+        cin>>ts;
+        cout<<calculate(ts)<<endl;
     }
 }
