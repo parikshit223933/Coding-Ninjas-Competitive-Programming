661e0cb3efb788e491b9c79a5e7d86e75412f8bf
parikshit singh <pk223933@gmail.com>
2020-06-08 12:18:32 +0530

    partially correct

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -21,7 +21,7 @@ void ask(int y)
     cout << y << endl;
 }
 
-void ask_questions(int start, int end)
+/* void ask_questions(int start, int end)
 {
     int mid = (start + end) / 2;
     int count_L = 0;
@@ -53,12 +53,78 @@ void ask_questions(int start, int end)
         }
         ask_questions(mid, end);
     }
+} */
+
+bool accept_odd_questions(int &start, int &end)
+{
+    int mid=(start+end)/2;
+    ask(mid);
+    char x;
+    cin>>x;
+
+    if(start>end)
+    {
+        return false;
+    }
+
+    if(x=='L')
+    {
+        end=mid-1;
+        return false;
+    }
+    else if(x=='G')
+    {
+        start=mid+1;
+        return false;
+    }
+    else if(x=='E')
+    {
+        return true;
+    }
+}
+
+bool accept_even_questions(int &start, int &end)
+{
+    int mid=(start+end)/2;
+    ask(mid);
+    char x;
+    cin>>x;
+    if(x=='L')
+    {
+        end=mid-1;
+        return false;
+    }
+    else if(x=='G')
+    {
+        start=mid+1;
+        return false;
+    }
+    else if(x=='E')
+    {
+        return true;
+    }
 }
 
 int main()
 {
     int n;
     cin >> n;
-    ask_questions(1, n);
+    int end_even=n;
+    int end_odd=n;
+    int start_even=1;
+    int start_odd=1;
+    for(int i=0; i<150; i++)
+    {
+        if(accept_odd_questions(start_odd, end_odd))
+        {
+            return 0;
+        }
+        if(accept_even_questions(start_even, end_even))
+        {
+            return 0;
+        }
+    }
+
+    /* ask_questions(1, n); */
     return 0;
 }
