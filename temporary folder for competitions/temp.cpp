c5e5381acb747f3921895d721b4348b5a43ebb2a
parikshit singh <pk223933@gmail.com>
2020-06-08 13:13:20 +0530

    improved

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -16,115 +16,186 @@
 #define ll long long int
 using namespace std;
 
-void ask(int y)
+void print(int** arr, int n)
 {
-    cout << y << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
-/* void ask_questions(int start, int end)
+void ask(int r1, int c1, int r2, int c2)
 {
-    int mid = (start + end) / 2;
-    int count_L = 0;
-    int count_G = 0;
+    cout << 1 << " " << r1+1 << " " << c1+1 << " " << r2+1 << " " << c2+1 << endl;
+}
 
-    while (count_L == count_G)
+int current_array_sum(int **arr, int n)
+{
+    int sum=0;
+    for(int i=0; i<n; i++)
     {
-        for (int i = 0; i < 2; i++)
+        for(int j=0; j<n; j++)
         {
-            ask(mid);
-            char x;
-            cin >> x;
-            if (x == 'G')
-                count_G++;
-            else if (x == 'L')
-                count_L++;
-            else
+            sum+=arr[i][j];
+        }
+    }
+    return sum;
+}
+
+void ask_queries_and_fill_array(int **arr, int n)
+{
+    /* lets check the whole array first */
+    ask(0, 0, n-1, n-1);
+    int sum_of_whole_array;
+    cin>>sum_of_whole_array;
+    if(sum_of_whole_array==0)
+    {
+        return;
+    }
+    else if(sum_of_whole_array==n*n)
+    {
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                arr[i][j]=1;
+            }
+        }
+        return;
+    }
+
+
+
+
+
+
+
+
+
+    /* filling first row optimally */
+    int r1, c1, r2, c2;
+    int sum=0;
+    for(int i=0; i<n; i++)
+    {
+        r1=0;
+        r2=0;
+        c1=0;
+        c2=i;
+        ask(r1, c1, r2, c2);
+        int x;
+        cin>>x;
+        arr[0][i]=x-sum;
+        sum=x;
+    }
+    if(current_array_sum(arr, n)==sum_of_whole_array)
+    {
+        return;
+    }
+    /* filling first column optimally */
+    sum=arr[0][0];
+    for(int i=1; i<n; i++)
+    {
+        c1=0;
+        c2=0;
+        r1=0;
+        r2=i;
+        ask(r1, c1, r2, c2);
+        int x;
+        cin>>x;
+        arr[i][0]=x-sum;
+        sum=x;
+    }
+    if(current_array_sum(arr, n)==sum_of_whole_array)
+    {
+        return;
+    }
+    /* filling the remaining part of the matrix */
+    for(int i=1; i<n; i++)
+    {
+        for(int j=1; j<n; j++)
+        {
+            /* calculating the sum */
+            sum=0;
+            for(int x=0; x<=i; x++)
+            {
+                for(int y=0; y<=j; y++)
+                {
+                    if(x==i&&y==j)
+                    {
+                        break;
+                    }
+                    sum+=arr[x][y];
+                }
+            }
+            r1=0;
+            c1=0;
+            r2=i;
+            c2=j;
+            ask(r1, c1, r2, c2);
+            int x;
+            cin>>x;
+            arr[i][j]=x-sum;
+
+            if(current_array_sum(arr, n)==sum_of_whole_array)
+            {
                 return;
+            }
         }
     }
-
-    if (count_L > count_G)
-        ask_questions(1, mid);
-    else if (count_L < count_G)
-    {
-        if (end - mid == 1)
-        {
-            mid = end;
-        }
-        ask_questions(mid, end);
-    }
-} */
-
-bool accept_odd_questions(int &start, int &end)
-{
-    int mid=(start+end)/2;
-    ask(mid);
-    char x;
-    cin>>x;
-
-    if(start>end)
-    {
-        return false;
-    }
-
-    if(x=='L')
-    {
-        end=mid-1;
-        return false;
-    }
-    else if(x=='G')
-    {
-        start=mid+1;
-        return false;
-    }
-    else if(x=='E')
-    {
-        return true;
-    }
 }
-
-bool accept_even_questions(int &start, int &end)
-{
-    int mid=(start+end)/2;
-    ask(mid);
-    char x;
-    cin>>x;
-    if(x=='L')
-    {
-        end=mid-1;
-        return false;
-    }
-    else if(x=='G')
-    {
-        start=mid+1;
-        return false;
-    }
-    else if(x=='E')
-    {
-        return true;
-    }
-}
-
 int main()
 {
-    int n;
-    cin >> n;
-    int end_even=n;
-    int end_odd=n;
-    int start_even=1;
-    int start_odd=1;
-    for(int i=0; i<150; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        if(accept_odd_questions(start_odd, end_odd))
+        int n, p;
+        cin >> n >> p;
+        int** arr = new int* [n];
+        /* asking queries */
+        /* dont forget to add 1 while asking for queries */
+        for(int i=0; i<n; i++)
         {
-            return 0;
+            arr[i]=new int [n];
+            for(int j=0; j<n; j++)
+            {
+                arr[i][j]=0;
+            }
         }
-        if(accept_even_questions(start_even, end_even))
+        ask_queries_and_fill_array(arr, n);
+
+        /* for (int i = 0; i < n; i++)
         {
-            return 0;
+            arr[i] = new int[n];
+            for (int j = 0; j < n; j++)
+            {
+                int r1 = i;
+                int c1 = j;
+                int r2 = i;
+                int c2 = j;
+                cout << 1 << " " << r1+1 << " " << c1+1 << " " << r2+1 << " " << c2+1 << endl;
+                int x;
+                cin >> x;
+                arr[i][j] = x;
+            }
+        } */
+
+        cout << 2 << endl;
+        print(arr, n);
+        int x;
+        cin >> x;
+        if (x == -1)
+        {
+            break;
+        }
+        else
+        {
+            continue;
         }
     }
-
-    /* ask_questions(1, n); */
     return 0;
 }
