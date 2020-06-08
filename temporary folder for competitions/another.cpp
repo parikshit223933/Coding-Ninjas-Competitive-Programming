2a6336bf98fa9733ed6d5f55dce2420bb5e81222
parikshit singh <pk223933@gmail.com>
2020-06-08 23:26:18 +0530

    failed

---
+++ temporary folder for competitions/another.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<(1!=1!=1)<<endl;
+}
--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -37,158 +37,77 @@ int minimum_operations(int p, int q, int r, int a, int b, int c)
         }
     }
 
-    if(equal==3)
+    if (equal == 3)
     {
         return 0;
     }
-    else if(equal==2)
+    else if (equal == 2)
     {
         return 1;
     }
-    else if(equal==1)
+    else if (equal == 1)
     {
-        if(p==a)
+        if (p == a)
         {
-            if(q-b==r-c)
+            if (q - b == r - c)
             {
                 return 1;
             }
-            else if((q!=0&&r!=0)&&(b/q==c/r)&&(b%q==0&&c%r==0))
+            else if ((q != 0 && r != 0) && (b / q == c / r) && (b % q == 0 && c % r == 0))
             {
                 return 1;
             }
         }
-        else if(q==b)
+        else if (q == b)
         {
-            if(p-a==r-c)
+            if (p - a == r - c)
             {
                 return 1;
             }
-            else if((p!=0&&r!=0)&&(a/p==c/r)&&(a%p==0&&c%r==0))
+            else if ((p != 0 && r != 0) && (a / p == c / r) && (a % p == 0 && c % r == 0))
             {
                 return 1;
             }
         }
-        else if(r==c)
+        else if (r == c)
         {
-            if(p-a==q-b)
+            if (p - a == q - b)
             {
                 return 1;
             }
-            else if((q!=0&&p!=0)&&(b/q==a/p)&&(b%q==0&&a%p==0))
+            else if ((q != 0 && p != 0) && (b / q == a / p) && (b % q == 0 && a % p == 0))
             {
                 return 1;
             }
         }
         return 2;
     }
-    else if(equal==0)
+    else if (equal == 0)
     {
-        int diff1=a-p;
-        int diff2=b-q;
-        int diff3=c-r;
-        int mod1=a%p;
-        int mod2=b%q;
-        int mod3=c%r;
-        float quo1=(a*1.0)/(p*1.0);
-        float quo2=(b*1.0)/(q*1.0);
-        float quo3=(c*1.0)/(r*1.0);
-        /* All cases when 1 operation is needed */
-        if(diff1==diff2&&diff2==diff3)
+        int diff1 = a - p;
+        int diff2 = b - q;
+        int diff3 = c - r;
+        int mod1 = a % p;
+        int mod2 = b % q;
+        int mod3 = c % r;
+        float quo1 = (a * 1.0) / (p * 1.0);
+        float quo2 = (b * 1.0) / (q * 1.0);
+        float quo3 = (c * 1.0) / (r * 1.0);
+        /* conditions for returning 1 */
+        if (diff1 == diff2 == diff3)
         {
             return 1;
         }
-        if(diff1!=diff2&&diff2==diff3)
+        if (p != 0 && q != 0 && r != 0 && mod1 == mod2 == mod3 == 0 && quo1 == quo2 == quo3)
         {
-            return 2;
+            return 1;
         }
-        if(diff1==diff2&&diff2!=diff3)
+        /* conditions for returning 3 */
+        if (diff1 != diff2 && diff2 != diff3 && diff3 != diff1 && quo1 != quo2 && quo2 != quo3 && quo3 != quo1)
         {
-            return 2;
-        }
-        /* their differences are different */
-        if(p!=0&&q!=0&&r!=0)
-        {
-            if(mod1==0&&mod2==0&&mod3==0)
-            {
-                if(quo1==quo2&&quo2==quo3)
-                {
-                    return 1;
-                }
-                if(quo1==quo2&&quo2!=quo3)
-                {
-                    return 2;
-                }
-                if(quo1!=quo2&&quo2==quo3)
-                {
-                    return 2;
-                }
-                return 3;
-            }
-            if(mod1!=0&&mod2==0&&mod3==0)
-            {
-                if(quo2==quo3)
-                {
-                    return 2;
-                }
-                return 3;
-            }
-            if(mod1==0&&mod2!=0&&mod3==0)
-            {
-                if(quo1==quo3)
-                {
-                    return 2;
-                }
-                return 3;
-            }
-            if(mod1==0&&mod2==0&&mod3!=0)
-            {
-                if(quo2==quo1)
-                {
-                    return 2;
-                }
-                return 3;
-            }
-            /////////////////////////////////////
-            if(mod1!=0&&mod2!=0&&mod3==0)
-            {
-                if(mod1==mod2)
-                {
-                    if((a-mod1)/p==(b-mod2)/q&&(b-mod2)/q==quo3)
-                    {
-                        return 2;
-                    }
-                    return 3;
-                }
-                return 3;
-            }
-            if(mod1==0&&mod2!=0&&mod3!=0)
-            {
-                if(mod2==mod3)
-                {
-                    if((c-mod3)/r==(b-mod2)/q&&(b-mod2)/q==quo1)
-                    {
-                        return 2;
-                    }
-                    return 3;
-                }
-                return 3;
-            }
-            if(mod1!=0&&mod2==0&&mod3!=0)
-            {
-                if(mod1==mod3)
-                {
-                    if((a-mod1)/p==(c-mod3)/r&&(b-mod2)/q==quo2)
-                    {
-                        return 2;
-                    }
-                    return 3;
-                }
-                return 3;
-            }
             return 3;
         }
-        return 3;
+        return 2;
     }
 }
 
