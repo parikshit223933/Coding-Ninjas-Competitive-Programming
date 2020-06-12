900faac90d7a2778f33bb26523c4e06e4756395e
parikshit singh <pk223933@gmail.com>
2020-06-13 03:34:25 +0530

    tpl base templ

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -1,12 +1,3 @@
-/* When you add or multiply a subset of triplet by number t,
- the dependence of resulting triplet p(t) is a line in 3D space.
-  There are 7 non-empty subsets of a triple and two operations,
-   so in general, you have 14 lines, which tells you what points can be achieved
-    from a given triplet in one operation.
-
-With that in mind, you just need to spawn all 14 lines from both given triplets.
- If they share the same line: it will take one operation.
-  If any of those lines intersect at integer point: it will take two operations. Otherwise, three. */
 #include <algorithm>
 #include <iostream>
 #include <utility>
@@ -26,72 +17,76 @@ With that in mind, you just need to spawn all 14 lines from both given triplets.
 #define int long long int
 using namespace std;
 
-int minimum_operations(int p, int q, int r, int a , int b, int c, bool selector[][3], char operations[])
+int minimum_operations(int p, int q, int r, int a, int b, int c)
 {
-    if(p>a||q>b||r>c)
+    int *src = new int[3];
+    int *des = new int[3];
+    int equal = 0;
+    src[0] = p;
+    src[1] = q;
+    src[2] = r;
+    des[0] = a;
+    des[1] = b;
+    des[2] = c;
+
+    for (int i = 0; i < 3; i++)
     {
-        return INT_MAX;
+        if (src[i] == des[i])
+        {
+            equal++;
+        }
     }
-    if(p==a&&q==b&&r==c)
+
+    if (equal == 3)
     {
         return 0;
     }
-    int ans=0;
-    int number_add[]={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int number_mul[]={0, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    for(int i=0; i<7; i++)/* for selector */
+    else if (equal == 2)
     {
-        for(int j=0; j<2; j++)/* for operation */
+        return 1;
+    }
+    else if (equal == 1)
+    {
+        if (p == a)
         {
-            if(operations[j]=='+')
+            if (q - b == r - c)
             {
-                for(int k=0; k<10; k++)
-                {
-                    auto first_selector=selector[i][0];
-                    auto second_selector=selector[i][1];
-                    auto third_selector=selector[i][2];
-                    int new_p, new_q, new_r;
-                    if(first_selector)
-                    {
-                        new_p+=number_add[k];
-                    }
-                    if(second_selector)
-                    {
-                        new_q+=number_add[k];
-                    }
-                    if(third_selector)
-                    {
-                        new_r+=number_add[k];
-                    }
-                    ans+=1+min(ans, minimum_operations(new_p, new_q, new_r, a, b, c, selector, operations));
-                }
+                return 1;
             }
-            else
+            else if ((q != 0 && r != 0) && (b / q == c / r) && (b % q == 0 && c % r == 0))
             {
-                for(int k=0; k<10; k++)
-                {
-                    auto first_selector=selector[i][0];
-                    auto second_selector=selector[i][1];
-                    auto third_selector=selector[i][2];
-                    int new_p, new_q, new_r;
-                    if(first_selector)
-                    {
-                        new_p*=number_mul[k];
-                    }
-                    if(second_selector)
-                    {
-                        new_q*=number_mul[k];
-                    }
-                    if(third_selector)
-                    {
-                        new_r*=number_mul[k];
-                    }
-                    ans+=1+min(ans, minimum_operations(new_p, new_q, new_r, a, b, c, selector, operations));
-                }
+                return 1;
             }
         }
+        else if (q == b)
+        {
+            if (p - a == r - c)
+            {
+                return 1;
+            }
+            else if ((p != 0 && r != 0) && (a / p == c / r) && (a % p == 0 && c % r == 0))
+            {
+                return 1;
+            }
+        }
+        else if (r == c)
+        {
+            if (p - a == q - b)
+            {
+                return 1;
+            }
+            else if ((q != 0 && p != 0) && (b / q == a / p) && (b % q == 0 && a % p == 0))
+            {
+                return 1;
+            }
+        }
+        return 2;
     }
-    return ans;
+    else if (equal == 0)
+    {
+        
+    }
+    return 3;
 }
 
 int32_t main()
@@ -106,15 +101,6 @@ int32_t main()
         int a, b, c;
         cin >> a >> b >> c;
 
-        bool selector[][3]={{true, true, true},
-                            {false, true, true},
-                            {true, false, true},
-                            {false, false, true},
-                            {true, true, false},
-                            {false, true, false},
-                            {true, false, false}
-                            };
-        char operations[]={'+', '*'};
-        cout << minimum_operations(p, q, r, a, b, c, selector, operations) << endl;
+        cout << minimum_operations(p, q, r, a, b, c) << endl;
     }
 }
