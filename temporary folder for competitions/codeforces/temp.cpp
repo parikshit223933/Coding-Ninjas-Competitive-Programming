7a65b89d925a880a988dc08c98c648587ee9cb5b
parikshit singh <pk223933@gmail.com>
2020-06-13 22:40:04 +0530

    structured directories

---
+++ temporary folder for competitions/codeforces/temp.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <iterator>
+#include <limits.h>
+#include <math.h>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define int long long int
+using namespace std;
+int main()
+{
+    fast;
+}
