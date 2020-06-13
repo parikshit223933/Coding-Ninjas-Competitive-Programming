33b0327a90257f6122922c7cae96718b4a675864
parikshit singh <pk223933@gmail.com>
2020-06-13 21:15:20 +0530

    base

--- temporary folder for competitions/temp.cpp
+++ temporary folder for competitions/temp.cpp
@@ -16,46 +16,7 @@
 #define endl '\n'
 #define int long long int
 using namespace std;
-
-
-int longest_subarray(int *arr, int n, int x)
-{
-    int sum=0;
-    int j=0;
-    int max_length=0;
-    for(int i=0; i<n; i++)
-    {
-        sum+=arr[i];
-        if(sum%x!=0)
-        {
-            max_length=i-j+1;
-        }
-        else
-        {
-            sum-=arr[j];
-            j++;
-        }
-    }   
-    if(max_length==0)
-    {
-        return -1;
-    }
-    return max_length;
-}
-int32_t main()
+int main()
 {
     fast;
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, x;
-        cin>>n>>x;
-        int *arr=new int [n];
-        for(int i=0; i<n; i++)
-        {
-            cin>>arr[i];
-        }
-        cout<<longest_subarray(arr, n, x)<<endl;
-    }
 }
