aff3d1d6fc747ee085621db14a33d621360dded8
parikshit singh <pk223933@gmail.com>
2020-06-14 16:47:58 +0530

    staged

---
+++ temporary folder for competitions/leetcode questions/count and say.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <iterator>
+#include <limits.h>
+#include <math.h>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define endl '\n'
+#define ll long long int
+using namespace std;
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+    }
+};
