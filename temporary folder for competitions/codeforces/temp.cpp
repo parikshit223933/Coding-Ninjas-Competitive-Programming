d5943053f5d26b9fb8bc69d9a04e3abec4b82a90
parikshit singh <pk223933@gmail.com>
2020-06-16 20:13:10 +0530

    implemented

--- temporary folder for competitions/codeforces/temp.cpp
+++ temporary folder for competitions/codeforces/temp.cpp
@@ -14,9 +14,22 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define int long long int
+#define ll long long int
 using namespace std;
 int main()
 {
     fast;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string b;
+        cin >> b;
+        string a = "";
+        for (int i = 0; i < b.length(); i += 2)
+        {
+            a += b[i];
+        }
+        cout << a+b[b.length()-1] << endl;
+    }
 }
