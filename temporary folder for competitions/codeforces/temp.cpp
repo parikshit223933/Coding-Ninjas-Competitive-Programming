e796dd500771c1d61e2dedd9d2b27fb59560576e
parikshit singh <pk223933@gmail.com>
2020-06-18 22:26:26 +0530

    implemented

--- temporary folder for competitions/codeforces/temp.cpp
+++ temporary folder for competitions/codeforces/temp.cpp
@@ -14,12 +14,38 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
-
-
-
-int main()
+unordered_map<int, unordered_map<int,  pair<int, bool>>>dp;
+int operations(int a, int b, int n)
 {
-
+    if(a>n||b>n)
+    {
+        dp[a][b].second=true;
+        dp[a][b].first=0;
+        return 0;
+    }
+    if(a>b)
+    {
+        return 1+operations(a, a+b, n);
+    }
+    else
+    {
+        return 1+operations(a+b, b, n);
+    }
+    
+}
+ 
+int32_t main()
+{
+    fast;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        dp.clear();
+        int a, b, n;
+        cin>>a>>b>>n;
+        cout<<operations(a, b, n)<<endl;
+    }
 }
