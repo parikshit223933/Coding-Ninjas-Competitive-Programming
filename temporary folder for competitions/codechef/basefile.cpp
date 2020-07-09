04bacfa8d3434ca1976a54143364d5af11365ebd
parikshit singh <pk223933@gmail.com>
2020-07-10 01:49:16 +0530

    implemented

--- temporary folder for competitions/codechef/basefile.cpp
+++ temporary folder for competitions/codechef/basefile.cpp
@@ -14,9 +14,45 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
-int main()
+int solve(int *arr, int n)
+{
+    int count=0;
+    for(int i=0; i<n-1; i++)
+    {
+        int diff=abs(arr[i]-arr[i+1]);
+        int toBeAdded;
+        if(diff==0)
+        {
+            toBeAdded=0;
+        }
+        else if(diff==1)
+        {
+            toBeAdded=0;
+        }
+        else
+        {
+            toBeAdded=diff-1;
+        }
+        count+=toBeAdded;
+    }
+    return count;
+}
+int32_t main()
 {
     fast;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int *arr=new int [n];
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<solve(arr, n)<<endl;
+    }
 }
--- temporary folder for competitions/codeforces/temp.cpp
+++ temporary folder for competitions/codeforces/temp.cpp
@@ -18,28 +18,29 @@
 #define int long long int
 using namespace std;
 
-int mod(int a)
-{
-    if(a<0)
-    {
-        return -1*a;
-    }
-    return a;
-}
-
 bool solve(int a, int b, int n, int m)
 {
-    
-}
-
-bool solve2(int a, int b, int n, int m)
-{
-    /* case 1: n of first type of guests */
-
-    if(m<=min(a, b))
+    if (m > n)
     {
-        int rem=max(a, b)+(min(a, b)-m);
-        if(n<=rem)
+        if (m <= min(a, b))
+        {
+            if (n <= a + b - min(a, b) + m)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else if(m==n)
+    {
+        if(a+b>=m+n)
         {
             return true;
         }
@@ -47,30 +48,65 @@ bool solve2(int a, int b, int n, int m)
         {
             return false;
         }
+        
     }
     else
     {
-        return false;
+        if (max(a, b) - min(a, b) > n)
+        {
+            if (min(a, b) < m)
+            {
+                return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else if (max(a, b) - min(a, b) == n)
+        {
+            int temp = min(a, b);
+            if (m <= temp)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            int temp = min(a, b);
+            n -= max(a, b) - min(a, b);
+            if (temp + temp > m + n)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
-    
 }
 
 int32_t main()
 {
     fast;
     int t;
-    cin>>t;
-    while(t--)
+    cin >> t;
+    while (t--)
     {
         int a, b, n, m;
-        cin>>a>>b>>n>>m;
-        if(solve(a, b, n, m)||solve2(a, b, m, n))
+        cin >> a >> b >> n >> m;
+        if (solve(a, b, n, m))
         {
-            cout<<"Yes"<<endl;
+            cout << "Yes" << endl;
         }
         else
         {
-            cout<<"No"<<endl;
+            cout << "No" << endl;
         }
     }
 }
