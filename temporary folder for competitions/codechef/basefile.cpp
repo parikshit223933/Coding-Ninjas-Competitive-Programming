3258d0b4e7f85607208e9d2bc4b38de4c3a2b994
parikshit singh <pk223933@gmail.com>
2020-07-10 05:16:42 +0530

    implemented

--- temporary folder for competitions/codechef/basefile.cpp
+++ temporary folder for competitions/codechef/basefile.cpp
@@ -14,27 +14,106 @@
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);
 #define endl '\n'
-#define ll long long int
+#define int long long int
 using namespace std;
 
-struct point{
-	int x;
-	int y;
-};
-
-int main()
+int solve(int *a, int *b, int n)
 {
-	int t;
+	unordered_map<int, int>map;
+	int minimum=INT_MAX;
+	for(int i=0; i<n; i++)
+	{
+		map[a[i]]++;
+		map[b[i]]++;
+		minimum=min(minimum, a[i]);
+		minimum=min(minimum, b[i]);
+	}
+	bool stop=false;
+
+	unordered_map<int, int>correct;
+	for(auto iterator=map.begin(); iterator!=map.end(); iterator++)
+	{
+		if(iterator->second%2!=0)
+		{
+			stop=true;
+			break;
+		}
+		else
+		{
+			correct[(iterator->first)]=(iterator->second)/2;
+		}
+	}
+	if(stop)
+	{
+		return -1;
+	}
+
+	unordered_map<int, int>copyOfCorrect=correct;
+	vector<int>s1, s2;
+	for(int i=0; i<n; i++)
+	{
+		if(copyOfCorrect[a[i]])
+		{
+			copyOfCorrect[a[i]]--;
+		}
+		else
+		{
+			s1.push_back(a[i]);
+		}
+	}
+	copyOfCorrect.clear();
+	copyOfCorrect=correct;
+	for(int i=0; i<n; i++)
+	{
+		if(copyOfCorrect[b[i]])
+		{
+			copyOfCorrect[b[i]]--;
+		}
+		else
+		{
+			s2.push_back(b[i]);
+		}
+	}
+	
+	if(s1.size()==0)
+	{
+		return 0;
+	}
+	if(s1.size()!=s2.size())
+	{
+		return -1;
+	}
+
+
+	sort(s1.begin(), s1.end());
+	sort(s2.begin(), s2.end(), greater<int>());
+	int cost=0;
+	for(int i=0; i<s1.size(); i++)
+	{
+		cost+=min(2*minimum, min(s1[i], s2[i]));
+	}
+	return cost;
+}
+
+int32_t main()
+{
+    fast;
+    int t;
 	cin>>t;
 	while(t--)
 	{
 		int n;
 		cin>>n;
-		int new_n=4*n-1;
-		point *arr=new point[new_n];
-		for(int i=0; i<new_n; i++)
+		int *a=new int [n];
+		int *b=new int [n];
+		for(int i=0; i<n; i++)
 		{
-			cin>>arr[i].x>>arr[i].y;
-		} 
+			cin>>a[i];
+		}
+		for(int i=0; i<n; i++)
+		{
+			cin>>b[i];
+		}
+		cout<<solve(a, b, n)<<endl;
 	}
 }
--- temporary folder for competitions/codechef/temp.cpp
+++ temporary folder for competitions/codechef/temp.cpp
@@ -11,73 +11,97 @@
 #include <set>
 #include <unordered_set>
 #define fast                          \
-	ios_base::sync_with_stdio(false); \
-	cin.tie(NULL);
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
 #define endl '\n'
 #define ll long long int
 using namespace std;
 
+void test()
+{
+    ll n;///////////////
+    cin >> n;/////////////////
+    vector<ll> a, b;//////////////////
+    unordered_map<ll, ll> map;////////////
+    ll u;/////////////
+    ll mi = INT_MAX;/////////////
+    for (ll i = 0; i < n; i++)///////////////
+    {/////////////
+        cin >> u;//////////////
+        a.push_back(u);//////////////
+        map[a[i]]++;///////////////
+        mi = min(mi, a[i]);//////////////
+    }///////////////
+    for (ll i = 0; i < n; i++)///////////
+    {/////////////////
+        cin >> u;//////////////
+        b.push_back(u);//////////////
+        map[b[i]]++;/////////////
+        mi = min(mi, b[i]);////////////////////
+    }////////////
+    bool stop = false;////////////
+    unordered_map<ll, ll> corr;//////////
+    for (auto it = map.begin(); it != map.end(); it++)////////////
+    {////////////
+        if (it->second % 2 == 1)///////////
+        {/////////////
+            stop = true;/////////////
+            break;///////////
+        }//////////////
+        else//////////////////
+        {////////////////
+            corr[it->first] = it->second / 2;//////////////
+        }//////////////
+    }///////////////
+    if (stop)////////////
+    {//////////////////////////////////
+        cout << -1 << endl;//////////////////
+        return;/////////////////////////
+    }//////////////////////////////////
+    unordered_map<ll, ll> copy = corr;//////////////
+    vector<ll> s1, s2;///////////
+    for (ll i = 0; i < n; i++)//////////////
+    {///////////////////
+        if (copy[a[i]])///////////////
+            copy[a[i]]--;///////////////
+        else////////////////////
+            s1.push_back(a[i]);////////////
+    }///////////////
+////////////////////
+    copy = corr;//////////////////////
+    for (ll i = 0; i < n; i++)/////////////////
+    {///////////
+        if (copy[b[i]])///////////////
+            copy[b[i]]--;///////////
+        else/////////////////////
+            s2.push_back(b[i]);//////////////
+    }/////////////
+    if (s1.size() == 0)///
+    {/////////////////
+        cout << 0 << endl;///////////////
+        return;////////////
+    }/////////////////////
+    if (s1.size() != s2.size())//////////////////
+    {//////////////
+        cout << -1 << endl;////////////
+        return;/////////////////
+    }////////////
+    ll cost = 0;
+    sort(s1.begin(), s1.end());
+    sort(s2.begin(), s2.end(), greater<>());
+    for (ll i = 0; i < s1.size(); i++)
+    {
+        cost += min(2 * mi, min(s1[i], s2[i]));
+    }
+    cout << cost << endl;
+}
+
 int main()
 {
-	fast;
-	ll t;
-	cin >> t;
-	while (t--)
-	{
-		ll n;
-		ll x_coordinate;
-		ll y_coordinate;
-		ll k;
-		ll x=0;
-		ll y=0;
-
-		map<ll, ll> map1;
-		map<ll, ll> map2;
-
-		cin >> n;
-		ll new_n = (4 * n) - 1;
-		while (new_n--)
-		{
-			cin >> x >> y;
-			/* searching x */
-			if (map1.find(x) == map1.end())
-			{
-				map1[x]=1;
-			}
-			else
-			{
-				map1[x]++;
-			}
-			/* searching y */
-			if (map2.find(y) == map2.end())
-			{
-				map2[y]=1;
-			}
-			else
-			{
-				map2[y]++;
-			}
-		}
-
-		for (auto iterator = map1.begin(); iterator != map1.end(); iterator++)
-		{
-
-			if ((iterator->second) % 2 != 0)
-			{
-				x_coordinate = iterator->first;
-				break;
-			}
-		}
-		for (auto iterator = map2.begin(); iterator != map2.end(); iterator++)
-		{
-
-			if ((iterator->second) % 2 != 0)
-			{
-				y_coordinate = iterator->first;
-				break;
-			}
-		}
-		cout << x_coordinate << " " << y_coordinate << endl;
-	}
-	return 0;
+    fast;
+    int t;
+    cin >> t;
+    while (t--)
+        test();
+    return 0;
 }
