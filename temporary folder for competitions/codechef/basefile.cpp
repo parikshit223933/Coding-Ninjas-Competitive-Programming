b9e5a5cff435e6386e28d21942d0a6dfa48c64c0
parikshit singh <pk223933@gmail.com>
2020-07-10 04:13:33 +0530

    unordered map failed here and ordered map proved to be fast

--- temporary folder for competitions/codechef/basefile.cpp
+++ temporary folder for competitions/codechef/basefile.cpp
@@ -11,141 +11,30 @@
 #include <set>
 #include <unordered_set>
 #define fast                          \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(NULL);
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);
 #define endl '\n'
 #define ll long long int
 using namespace std;
 
-void printer(char **arr)
-{
-    int n = 8;
-    for (int i = n-1; i >=0; i--)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-bool checker(int i, int j)
-{
-    if (i >= 0 && i < 8 && j >= 0 && j < 8)
-    {
-        return true;
-    }
-    return false;
-}
-
-void configurer(char **arr)
-{
-    int n = 8;
-
-    bool **visited = new bool *[n];
-    for (int i = 0; i < n; i++)
-    {
-        visited[i] = new bool[n];
-        for (int j = 0; j < n; j++)
-        {
-            visited[i][j] = false;
-        }
-    }
-
-    for (int i = n - 1; i >= 0; i--)
-    {
-        for (int j = n - 1; j >= 0; j--)
-        {
-            if (arr[i][j] == 'X')
-            {
-                if (checker(i - 1, j - 1) && arr[i - 1][j - 1] != 'X' && !visited[i - 1][j - 1]) //top left
-                {
-                    return;
-                }
-                if (checker(i, j - 1) && arr[i][j - 1] != 'X' && !visited[i][j - 1]) //left
-                {
-                    return;
-                }
-                if (checker(i - 1, j) && arr[i - 1][j] != 'X' && !visited[i - 1][j]) //top
-                {
-                    return;
-                }
-                if (checker(i + 1, j - 1) && arr[i + 1][j - 1] != 'X' && !visited[i + 1][j - 1]) //bottom left
-                {
-                    return;
-                }
-                if (checker(i - 1, j + 1) && arr[i - 1][j + 1] != 'X' && !visited[i - 1][j + 1]) //top right
-                {
-                    return;
-                }
-                if (checker(i + 1, j + 1) && arr[i + 1][j + 1] != 'X' && !visited[i + 1][j + 1]) //bottom right
-                {
-                    return;
-                }
-                if (checker(i, j + 1) && arr[i][j + 1] != 'X' && !visited[i][j + 1]) //right
-                {
-                    return;
-                }
-                if (checker(i + 1, j) && arr[i + 1][j] != 'X' && !visited[i + 1][j]) //bottom
-                {
-                    return;
-                }
-                arr[i][j] = '.';
-                visited[i][j] = true;
-            }
-        }
-    }
-}
-
-void solve(int k)
-{
-    int n = 8;
-    char **arr = new char *[n];
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = new char[n];
-        for (int j = 0; j < n; j++)
-        {
-            arr[i][j] = 'X';
-        }
-    }
-    arr[0][0] = 'O';
-
-    k--;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            if (k == 0)
-            {
-                configurer(arr);
-                printer(arr);
-                return;
-            }
-
-            if (i == 0 && j == 0)
-            {
-                continue;
-            }
-
-            arr[i][j] = '.';
-            k--;
-        }
-    }
-    configurer(arr);
-    printer(arr);
-    return;
-}
+struct point{
+	int x;
+	int y;
+};
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int k;
-        cin >> k;
-        solve(k);
-    }
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int new_n=4*n-1;
+		point *arr=new point[new_n];
+		for(int i=0; i<new_n; i++)
+		{
+			cin>>arr[i].x>>arr[i].y;
+		} 
+	}
 }
--- temporary folder for competitions/codechef/temp.cpp
+++ temporary folder for competitions/codechef/temp.cpp
@@ -17,40 +17,67 @@
 #define ll long long int
 using namespace std;
 
-void solve()
+int main()
 {
-	int k;
-	int n = 7;
-	cin >> k;
-	k--;
-
-	cout << 'O';
-	for (int i = 0; i < 8; i++)
+	fast;
+	ll t;
+	cin >> t;
+	while (t--)
 	{
-		for (int j = 0; j < n; j++)
+		ll n;
+		ll x_coordinate;
+		ll y_coordinate;
+		ll k;
+		ll x=0;
+		ll y=0;
+
+		map<ll, ll> map1;
+		map<ll, ll> map2;
+
+		cin >> n;
+		ll new_n = (4 * n) - 1;
+		while (new_n--)
 		{
-			if (k > 0)
+			cin >> x >> y;
+			/* searching x */
+			if (map1.find(x) == map1.end())
 			{
-				cout << '.';
+				map1[x]=1;
 			}
 			else
 			{
-				cout << "X";
+				map1[x]++;
 			}
-			k--;
+			/* searching y */
+			if (map2.find(y) == map2.end())
+			{
+				map2[y]=1;
+			}
+			else
+			{
+				map2[y]++;
+			}
 		}
-		n = 8;
-		cout << endl;
-	}
-}
 
-int main()
-{
-	int t;
-	cin >> t;
+		for (auto iterator = map1.begin(); iterator != map1.end(); iterator++)
+		{
 
-	while (t--)
-	{
-		solve();
+			if ((iterator->second) % 2 != 0)
+			{
+				x_coordinate = iterator->first;
+				break;
+			}
+		}
+		for (auto iterator = map2.begin(); iterator != map2.end(); iterator++)
+		{
+
+			if ((iterator->second) % 2 != 0)
+			{
+				y_coordinate = iterator->first;
+				break;
+			}
+		}
+		cout << x_coordinate << " " << y_coordinate << endl;
 	}
+	return 0;
 }
