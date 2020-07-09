f213c7216dad719aa5a74dd455f569b9df2014e5
parikshit singh <pk223933@gmail.com>
2020-07-10 03:38:23 +0530

    Implemented

--- temporary folder for competitions/codechef/basefile.cpp
+++ temporary folder for competitions/codechef/basefile.cpp
@@ -14,65 +14,138 @@
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
 #define endl '\n'
-#define int long long int
+#define ll long long int
 using namespace std;
 
-inline int power(int n)
+void printer(char **arr)
 {
-    int sum;
-    for (sum = 0; n > 0; sum += n % 10, n /= 10);
-    return sum;
-}
-
-void solve(int *a, int *b, int n)
-{
-    int sumA=0;
-    int sumB=0;
-    for(int i=0; i<n; i++)
+    int n = 8;
+    for (int i = n-1; i >=0; i--)
     {
-        if(power(a[i])>power(b[i]))
+        for (int j = 0; j < n; j++)
         {
-            sumA+=1;
+            cout << arr[i][j] << " ";
         }
-        else if(power(a[i])<power(b[i]))
-        {
-            sumB+=1;
-        }
-        else
-        {
-            sumA+=1;
-            sumB+=1;
-        }
-    }
-    if(sumA>sumB)
-    {
-        cout<<0<<" "<<sumA<<endl;
-    }
-    else if(sumA<sumB)
-    {
-        cout<<1<<" "<<sumB<<endl;
-    }
-    else
-    {
-        cout<<2<<" "<<sumA<<endl;
+        cout << endl;
     }
 }
 
-int32_t main()
+bool checker(int i, int j)
 {
-    fast;
+    if (i >= 0 && i < 8 && j >= 0 && j < 8)
+    {
+        return true;
+    }
+    return false;
+}
+
+void configurer(char **arr)
+{
+    int n = 8;
+
+    bool **visited = new bool *[n];
+    for (int i = 0; i < n; i++)
+    {
+        visited[i] = new bool[n];
+        for (int j = 0; j < n; j++)
+        {
+            visited[i][j] = false;
+        }
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = n - 1; j >= 0; j--)
+        {
+            if (arr[i][j] == 'X')
+            {
+                if (checker(i - 1, j - 1) && arr[i - 1][j - 1] != 'X' && !visited[i - 1][j - 1]) //top left
+                {
+                    return;
+                }
+                if (checker(i, j - 1) && arr[i][j - 1] != 'X' && !visited[i][j - 1]) //left
+                {
+                    return;
+                }
+                if (checker(i - 1, j) && arr[i - 1][j] != 'X' && !visited[i - 1][j]) //top
+                {
+                    return;
+                }
+                if (checker(i + 1, j - 1) && arr[i + 1][j - 1] != 'X' && !visited[i + 1][j - 1]) //bottom left
+                {
+                    return;
+                }
+                if (checker(i - 1, j + 1) && arr[i - 1][j + 1] != 'X' && !visited[i - 1][j + 1]) //top right
+                {
+                    return;
+                }
+                if (checker(i + 1, j + 1) && arr[i + 1][j + 1] != 'X' && !visited[i + 1][j + 1]) //bottom right
+                {
+                    return;
+                }
+                if (checker(i, j + 1) && arr[i][j + 1] != 'X' && !visited[i][j + 1]) //right
+                {
+                    return;
+                }
+                if (checker(i + 1, j) && arr[i + 1][j] != 'X' && !visited[i + 1][j]) //bottom
+                {
+                    return;
+                }
+                arr[i][j] = '.';
+                visited[i][j] = true;
+            }
+        }
+    }
+}
+
+void solve(int k)
+{
+    int n = 8;
+    char **arr = new char *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new char[n];
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = 'X';
+        }
+    }
+    arr[0][0] = 'O';
+
+    k--;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (k == 0)
+            {
+                configurer(arr);
+                printer(arr);
+                return;
+            }
+
+            if (i == 0 && j == 0)
+            {
+                continue;
+            }
+
+            arr[i][j] = '.';
+            k--;
+        }
+    }
+    configurer(arr);
+    printer(arr);
+    return;
+}
+
+int main()
+{
     int t;
     cin >> t;
     while (t--)
     {
-        int n;
-        cin >> n;
-        int *a = new int[n];
-        int *b = new int[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i] >> b[i];
-        }
-        solve(a, b, n);
+        int k;
+        cin >> k;
+        solve(k);
     }
 }
--- temporary folder for competitions/codechef/temp.cpp
+++ temporary folder for competitions/codechef/temp.cpp
@@ -1,15 +1,56 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <iterator>
+#include <limits.h>
+#include <math.h>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#define fast                          \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);
+#define endl '\n'
+#define ll long long int
 using namespace std;
-long long int getSum(long long int n)
+
+void solve()
 {
-    long long int sum;
-    for (sum = 0; n > 0; sum += n % 10, n /= 10);
-    return sum;
+	int k;
+	int n = 7;
+	cin >> k;
+	k--;
+
+	cout << 'O';
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (k > 0)
+			{
+				cout << '.';
+			}
+			else
+			{
+				cout << "X";
+			}
+			k--;
+		}
+		n = 8;
+		cout << endl;
+	}
 }
 
 int main()
 {
-    long long int n;
-    cin>>n;
-    cout<<getSum(n)<<endl;
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		solve();
+	}
 }
