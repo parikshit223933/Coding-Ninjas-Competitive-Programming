72a819ba9abf7a92dea92ffcfaead8b676d325a0
parikshit singh <pk223933@gmail.com>
2020-07-16 23:06:12 +0530

    ninjas hire  test

---
+++ temporary folder for competitions/CN/temp.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <iostream>
+#include <utility>
+#include <string>
+#include <iterator>
+#include <stdlib.h>
+#include <limits.h>
+#include <math.h>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <queue>
+#define fast                          \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL);
+#define endl '\n'
+#define int long long int
+using namespace std;
+int main()
+{
+	
+}
