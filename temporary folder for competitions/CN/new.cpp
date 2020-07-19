5702f48e82abeb8596a70a695be1bfad288be6c5
parikshit singh <pk223933@gmail.com>
2020-07-19 20:41:22 +0530

    partially correct

--- temporary folder for competitions/CN/new.cpp
+++ temporary folder for competitions/CN/new.cpp
@@ -1,42 +1,6 @@
-#include <iostream>
-#include<limits.h>
-#include<map>
+#include<iostream>
 using namespace std;
-void solve(int one[], int two[], int k)
-{
-    // Hint: keys are one's indices, values are two's indices
-    map<int, int>idxMap;
-
-    for (int i = 0; i < k; i++)
-    {
-        int min = 1000000;
-        int oneIdx = -1;
-        int twoIdx = -1;
-
-        for (int j = 0; j < 3; j++)
-        {
-            if (idxMap[j] == 3)
-            {
-                continue;
-            }
-            if (one[j] + two[idxMap[j]] < min)
-            {
-                min = one[j] + two[idxMap[j]];
-                oneIdx = j;
-                twoIdx = idxMap[j];
-            }
-        }
-
-        idxMap[oneIdx]++;
-        cout << (one[oneIdx] + " " + two[twoIdx]);
-        cout << (", ");
-    }
-}
-
 int main()
 {
-    int one[] = {1, 7, 11};
-    int two[] = {2, 4, 6};
-    int k = 3;
-    solve(one, two, k);
+    
 }
--- temporary folder for competitions/CN/temp.cpp
+++ temporary folder for competitions/CN/temp.cpp
@@ -17,9 +17,56 @@
 	ios_base::sync_with_stdio(false); \
 	cin.tie(NULL);
 #define endl '\n'
-#define int long long int
+#define ll long long int
 using namespace std;
+void solve(string s)
+{
+	unordered_map<char, int>m;
+	for(int i=0; i<s.length(); i++)
+	{
+		m[s[i]]++;
+	};
+	char max_char=0;
+	int max_length=0;
+	for(auto i: m)
+	{
+		if(max_length<i.second)
+		{
+			max_length=i.second;
+			max_char=i.first;
+		}
+	}
+	if(max_length==1)
+	{
+		char lmc;
+		char mc='z';
+		for(int i=0; i<s.length(); i++)
+		{
+			if((int)s[i]<(int)mc)
+			{
+				lmc=s[i];
+			}
+		}
+		cout<<lmc<<endl;
+	}
+	else
+	{
+		for(int i=0; i<max_length; i++)
+		{
+			cout<<max_char;
+		}
+		cout<<endl;
+	}
+}
 int main()
 {
-	
+	fast;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		solve(s);
+	}
 }
