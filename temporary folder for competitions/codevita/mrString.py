4cfe6a34d012ec85ae6d494ff160aebdbd99b62f
parikshit <pk223933@gmail.com>
2020-08-16 06:54:40 +0530

    implemented by py

--- temporary folder for competitions/codevita/mrString.py
+++ temporary folder for competitions/codevita/mrString.py
@@ -1,18 +1,43 @@
-numbersss = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-numbercountofvowels = [2, 2, 1, 2, 2, 2, 1, 2, 2, 2]
+def printAnswers(numbersss, finalAnswer):
+    if finalAnswer <= 100:
+        print(numbersss[finalAnswer], end="")
+    else:
+        print("greater 100", end="")
+    return
 
-userInputforLength = int(input())
-arrayofnumbers = list(map(int, input().strip().split()))
-initialCount = 0
-for iteratorMain in range(userInputforLength):
-    for iteratorSecondary in str(arrayofnumbers[iteratorMain]):
-        initialCount = initialCount + numbercountofvowels[int(iteratorSecondary)]
-finalAnswer = 0
-for iteratorMain in range(userInputforLength-1):
-    for j in range(iteratorMain+1,userInputforLength):
-        if arrayofnumbers[iteratorMain] + arrayofnumbers[j] == initialCount:
-            finalAnswer += 1
-if finalAnswer <= 100:
-    print(numbersss[finalAnswer],end="")
-else:
-    print("greater 100",end="")
+
+def initializeArrays():
+    numbersss = ["zero", "one", "two", "three", "four",
+                 "five", "six", "seven", "eight", "nine"]
+    numbercountofvowels = [2, 2, 1, 2, 2, 2, 1, 2, 2, 2]
+    return (numbersss, numbercountofvowels)
+
+
+def initializeRequirements():
+    finalAnswer = 0
+    initialCount = 0
+    userInputforLength = int(input())
+    arrayofnumbers = list(map(int, input().strip().split()))
+    return (finalAnswer, initialCount, userInputforLength, arrayofnumbers)
+
+
+def main():
+
+    (numbersss, numbercountofvowels) = initializeArrays()
+    (finalAnswer, initialCount, userInputforLength,
+     arrayofnumbers) = initializeRequirements()
+
+    for iteratorMain in range(userInputforLength):
+        for iteratorSecondary in str(arrayofnumbers[iteratorMain]):
+            initialCount = initialCount + \
+                numbercountofvowels[int(iteratorSecondary)]
+
+    for iteratorMain in range(userInputforLength-1):
+        for j in range(iteratorMain+1, userInputforLength):
+            if arrayofnumbers[iteratorMain] + arrayofnumbers[j] == initialCount:
+                finalAnswer += 1
+
+    printAnswers(numbersss, finalAnswer)
+
+
+main()
